24862726c078d9894667fedeb4649fa517c8253e
George Millo <georgejulianmillo@gmail.com>
2015-12-31 12:22:30 +0100

    add better routes for signing in/up

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
 
+  devise_scope :user do
+    get :sign_in, to: "devise/sessions#new"
+    get :sign_up, to: "devise/registrations#new"
+  end
+
   root to: "application#root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
