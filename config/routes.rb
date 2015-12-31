69f1cf67c0b07178a96b3a76b8f458c97a1b93b2
George Millo <georgejulianmillo@gmail.com>
2015-12-31 13:03:21 +0100

    better routes (again) for user sessions, and add username to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :administrators
-  devise_for :users
+  devise_for :users, skip: :sessions
 
   devise_scope :user do
-    get :sign_in, to: "devise/sessions#new"
+    get    :sign_in, to: "devise/sessions#new",      as: :new_user_session
+    post   :sign_in, to: "devise/sessions#create",   as: :user_session
+    delete :sign_out, to: "devise/sessions#destroy", as: :destroy_user_session
     get :sign_up, to: "devise/registrations#new"
   end
 
