c137e2aab0639d5a69f3543b540f422a49d9c589
George Millo <georgejulianmillo@gmail.com>
2016-01-04 12:48:59 +0100

    fix broken user sign up form

--- Gemfile
+++ Gemfile
@@ -32,5 +32,9 @@ group :development do
   gem 'web-console', '~> 3.0'
 end
 
+group :test do
+  gem 'capybara', github: 'jnicklas/capybara'
+end
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,49 @@
+require "rails_helper"
+
+describe "signing up" do
+  subject { page }
+
+  before { visit new_user_registration_path }
+
+  it "has fields to create a new user" do
+    is_expected.to have_field :user_email
+    is_expected.to have_field :user_password
+    is_expected.to have_field :user_password_confirmation
+    is_expected.to have_field :user_name
+  end
+
+  describe "submitting the form" do
+    let(:submit_form) { click_button "Sign up" }
+
+    # The external HTTP calls to Stripe on the front and back ends are all
+    # stubbed out with the magic of stripe-ruby-mock.
+    describe "with valid user information" do
+      before do
+        fill_in :user_email, with: "testuser@example.com"
+        fill_in :user_password, with: "password123"
+        fill_in :user_password_confirmation, with: "password123"
+        fill_in :user_name, with: "Dave"
+      end
+
+      it "creates a new user" do
+        expect{ submit_form }.to change{User.count}.by(1)
+      end
+
+      describe "the created user" do
+        before { submit_form }
+
+        it "has the correct attributes" do
+          user = User.last
+          expect(user.email).to eq "testuser@example.com"
+          expect(user.name).to eq "Dave"
+        end
+      end
+    end
+
+    describe "with invalid user information" do
+      it "doesn't create a new user" do
+        expect{ submit_form }.not_to change{User.count}
+      end
+    end
+  end
+end
