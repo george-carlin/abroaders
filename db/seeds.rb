51ed18b571d20844906464f3efd604eb72b43d24
George Millo <georgejulianmillo@gmail.com>
2016-01-05 13:14:12 +0100

    update seeds file to add contact info

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,6 @@ ApplicationRecord.transaction do
   unless User.admin.any?
     %w[Erik AJ George].each do |name|
       User.admin.create!(
-        name:  name,
         email: "#{name.downcase}@abroaders.com",
         password:              "abroaders123",
         password_confirmation: "abroaders123",
@@ -21,13 +20,23 @@ ApplicationRecord.transaction do
 
   unless User.non_admin.any?
     50.times do
-      name = Faker::Name.name
-      User.create!(
+      name = Faker::Name.first_name
+      u = User.create!(
         email: "#{name.parameterize}@example.com",
-        name:  name,
         password:              "abroaders123",
         password_confirmation: "abroaders123"
       )
+      if rand > 0.3
+        u.create_contact_info!(
+          phone_number: Faker::PhoneNumber.phone_number,
+          first_name: name,
+          middle_names: (Faker::Name.first_name if rand > 0.1),
+          last_name: Faker::Name.last_name,
+          whatsapp: rand > 0.4,
+          text_message: rand > 0.1,
+          imessage: rand > 0.7
+        )
+      end
     end
   end
 end
