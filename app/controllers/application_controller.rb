0c829843f1c26296a00a09849d1f49339fee751b
George Millo <georgejulianmillo@gmail.com>
2016-01-12 18:17:35 +0100

    remove vestigial references to User.name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
   # Placeholder page for root path
   def root
     if current_user
@@ -15,12 +13,6 @@ class ApplicationController < ActionController::Base
         render "users/dashboard"
       end
     end
-  end
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
 end
