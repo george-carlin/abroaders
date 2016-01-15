10ec914273d532813529ab89de97982897446b07
George Millo <georgejulianmillo@gmail.com>
2016-01-15 17:29:52 +0100

    update/fix spending_info_pages_spec

--- spec/features/spending_info_pages_spec.rb
+++ spec/features/spending_info_pages_spec.rb
@@ -3,24 +3,16 @@ require "rails_helper"
 describe "spending info pages" do
   subject { page }
 
-  include_context "logged in"
+  include_context "logged in as new user"
+
+  before { user.create_contact_info!(attributes_for(:contact_info)) }
 
   describe "new page" do
     before do
-      extra_setup
       visit new_spending_info_path
     end
 
-    let(:extra_setup) { nil }
-
-    # context "when I already have provided my contact info" do
-    #   let(:extra_setup) { create(:contact_info, user: user) }
-    #   it "redirects to root" do
-    #     expect(current_path).to eq root_path
-    #   end
-    # end
-
-    it "has fields for contact info" do
+    it "has fields for spending info" do
       should have_field :spending_info_citizenship_us_citizen
       should have_field :spending_info_citizenship_us_permanent_resident
       should have_field :spending_info_citizenship_neither
@@ -42,7 +34,7 @@ describe "spending info pages" do
           fill_in :spending_info_spending_per_month_dollars, with: "1500"
         end
 
-        it "creates a new ContactInfo" do
+        it "saves the spending info" do
           expect(user.spending_info).not_to be_persisted
           submit_form
           expect(user.reload.spending_info).to be_persisted
