668862a089dc4e723dda6e01dd58a38337542366
George Millo <georgejulianmillo@gmail.com>
2016-01-21 15:03:05 +0100

    bug fix when clicking a checkbox on survey/cards

--- app/assets/javascripts/card_accounts.js
+++ app/assets/javascripts/card_accounts.js
@@ -9,9 +9,16 @@ window.ready(function () {
   });
 
 
-  $(".card-survey-checkbox").click(function () {
+  $(".card-survey-checkbox").click(function (e) {
     var $checkbox = $(this).find("input[type=checkbox]");
-    $checkbox.prop("checked", !$checkbox.prop("checked"));;
+    // The click handler will be called when the user clicks *anywhere* within
+    // the <div>, but if they have clicked specifically on the checkbox, we
+    // DON'T want to check/uncheck it via jQuery, because it's about to be
+    // checked/unchecked anyway. Without wrapping this in a if statement, the
+    // checkbox would be checked and then immediately unchecked.
+    if (e.target.nodeName !== "INPUT") {
+      $checkbox.prop("checked", !$checkbox.prop("checked"));;
+    }
   });
 
 });
