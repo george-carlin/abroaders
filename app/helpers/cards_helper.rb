3ac0314ef00850e82f212401c244959db5d3e671
George Millo <georgejulianmillo@gmail.com>
2016-01-21 14:55:16 +0100

    add card images

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -16,6 +16,18 @@ module CardsHelper
     options_for_enum_select(Card.types)
   end
 
+  def card_image_tag(card, size="180x114")
+    id  = card.identifier
+    dir = Rails.root.join("app", "assets", "images", "cards")
+    if File.exists?(dir.join("#{id}.png"))
+      image_tag "cards/#{id}.png", size: size
+    elsif File.exists?(dir.join("#{id}.jpg"))
+      image_tag "cards/#{id}.jpg", size: size
+    else
+      "Not found - #{id}"
+    end
+  end
+
   private
 
   def options_for_enum_select(enum)
