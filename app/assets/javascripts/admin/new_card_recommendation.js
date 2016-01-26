george-carlin/abroaders

commit fa27eb0a34f902a4b65c0ebbb297a8a95cbc45bf

admins can filter cards by b/p

app/assets/javascripts/admin/new_card_recommendation.js
@@ -3,4 +3,14 @@ $(document).ready(function () {
     // The table can't be sorted by the first column:
     headers: { 0: { sorter: false } }
   });
+
+  $('.card_bp_filter').click(function (e) {
+    var show  = this.checked,
+        value = this.dataset.value;
+    $("tr.admin_recommend_card").each(function (i, tr) {
+      if (value == tr.dataset.bp) {
+        tr.style.display = show ? "" : "none"
+      }
+    });
+  });
 });

app/assets/javascripts/components/admin/users/card_recommendations/CardRecommendationFilterCheckbox.js.jsx
@@ -1,0 +1,30 @@
+var CardRecommendationFilterCheckbox = React.createClass({
+
+  propTypes: {
+    filterType: React.PropTypes.string.isRequired,
+    value:      React.PropTypes.string.isRequired,
+    onChangeHandler: React.PropTypes.func.isRequired
+  },
+
+  render() {
+    var onChangeHandler = this.props.onChangeHandler.bind(
+      null,
+      this.props.filterType,
+      this.props.value
+    )
+
+    return (
+      <div className="checkbox">
+        <label>
+          <input
+            type="checkbox"
+            defaultChecked={true}
+            onChange={onChangeHandler}
+          />
+          &nbsp;{this.props.value.capitalize()}&nbsp;
+        </label>
+      </div>
+    )
+  }
+
+});

app/assets/javascripts/components/admin/users/card_recommendations/CardRecommendationFilters.js.jsx
@@ -1,0 +1,50 @@
+var CardRecommendationFilters = React.createClass({
+
+  propTypes: {
+    updateFilterCallback: React.PropTypes.func.isRequired,
+  },
+
+  render() {
+    return (
+      <div className="form-inline">
+        {["visa", "mastercard", "amex"].map(function (brand) {
+          return (
+            <CardRecommendationFilterCheckbox
+              key={"brand-" + brand}
+              filterType="hiddenBrands"
+              value={brand}
+              onChangeHandler={this.props.updateFilterCallback}
+            />
+          )
+        }.bind(this))}
+
+        &nbsp; | &nbsp;&nbsp;
+
+        {["business", "personal"].map(function (bp) {
+          return (
+            <CardRecommendationFilterCheckbox
+              key={"bp-" + bp}
+              filterType="hiddenBPs"
+              value={bp}
+              onChangeHandler={this.props.updateFilterCallback}
+            />
+          )
+        }.bind(this))}
+
+        &nbsp; | &nbsp;&nbsp;
+
+        <select id="card_bank_filter" className="form-control input-sm">
+          <option>All Banks</option>
+          <option>Barclays</option>
+          <option>Capital One</option>
+          <option>American Express</option>
+          <option>Chase</option>
+          <option>US Bank</option>
+          <option>Bank Of America</option>
+          <option>Citibank</option>
+        </select>
+      </div>
+    )
+  }
+
+});

app/helpers/cards_helper.rb
@@ -28,6 +28,20 @@ module CardsHelper
     end
   end
 
+  def card_bp_filter_check_box_tag(bp)
+    klass =  :card_bp_filter
+    id    =  :"#{klass}_#{bp}"
+    label_tag id do
+      check_box_tag(
+        id,
+        nil,
+        true,
+        class: klass,
+        data: { key: :bp, value: bp }
+      ) << raw("&nbsp;&nbsp#{bp.capitalize}")
+    end
+  end
+
   private
 
   def options_for_enum_select(enum)

app/views/admin/users/show.html.erb
@@ -52,12 +52,26 @@
 
 <%= form_for @card_recommendation, url: admin_user_card_recommendations_path(@user) do |f| %>
 
-  <%#= react_component "CardRecommendationForm", cards: serialize(@cards)  %>
-
   <div>
-    <!--
-    <CardRecommendationFilters updateFilterCallback={this._updateFilters} />
-    -->
+    <div class="form-inline">
+      <% Card.bps.keys.each do |bp| %>
+        <%= card_bp_filter_check_box_tag(bp) %>
+      <% end %>
+
+      &nbsp; | &nbsp;&nbsp;
+
+      <select id="card_bank_filter" class="form-control input-sm">
+        <option>All Banks</option>
+        <option>Barclays</option>
+        <option>Capital One</option>
+        <option>American Express</option>
+        <option>Chase</option>
+        <option>US Bank</option>
+        <option>Bank Of America</option>
+        <option>Citibank</option>
+      </select>
+    </div>
+
     <table class="table table-striped">
       <thead>
         <th></th>
@@ -82,7 +96,11 @@
       </thead>
       <tbody>
         <% @cards.each_with_index do |card, i| %>
-          <tr>
+          <tr
+            id="<%= dom_id(card, :admin_recommend) %>"
+            class="<%= dom_class(card, :admin_recommend) %>"
+            data-bp="<%= card.bp %>"
+          >
             <td><%= f.radio_button :card_id, card.id, checked: (i==0) %></td>
             <td><%= card.identifier %></td>
             <td><%= card.name %></td>

spec/features/admin/users/show_page_spec.rb
@@ -7,8 +7,13 @@ describe "admin section" do
     include_context "logged in as admin"
 
     before do
-      @cards = create_list(:card, 4)
-      @user  = create(:user)
+      @cards = [
+        @chase_business = create(:card, :business, bank: :chase),
+        @chase_personal = create(:card, :personal, bank: :chase),
+        @usb_business   = create(:card, :business, bank: :us_bank),
+        @usb_personal   = create(:card, :personal, bank: :us_bank)
+      ]
+      @user = create(:user)
       extra_setup
       visit admin_user_path(@user)
     end
@@ -53,27 +58,55 @@ describe "admin section" do
     it "has a form to recommend a new card" do
       within "form#new_card_account" do
         @cards.each do |card|
-          should have_selector card_radio_btn(card)
+          should have_recommendable_card(card)
+          within recommendable_card_selector(card) do
+            should have_selector card_radio_btn(card)
+          end
         end
       end
     end
 
-    it "has a dropdown to filter by bank" do
-      pending
-      is_expected.to have_selector "select#card_bank_filter"
-      within "select#card_bank_filter" do
-        is_expected.to have_text "All Banks"
-        is_expected.to have_text "Barclays"
-        is_expected.to have_text "Capital One"
-        is_expected.to have_text "American Express"
-        is_expected.to have_text "Chase"
-        is_expected.to have_text "US Bank"
-        is_expected.to have_text "Bank Of America"
-        is_expected.to have_text "Citibank"
+    describe "filters" do
+      def should_have_recommendable_cards(*cards)
+        cards.each { |card| should have_recommendable_card(card) }
+      end
+
+      def should_not_have_recommendable_cards(*cards)
+        cards.each { |card| should_not have_recommendable_card(card) }
+      end
+
+      describe "the cards", js: true do
+        specify "can be filtered by b/p" do
+          uncheck :card_bp_filter_business
+          save_screenshot "1.png"
+          should_have_recommendable_cards(@chase_personal, @usb_personal)
+          should_not_have_recommendable_cards(@chase_business, @usb_business)
+          uncheck :card_bp_filter_personal
+          should_not_have_recommendable_cards(*@cards)
+          check :card_bp_filter_business
+          should_have_recommendable_cards(@chase_business, @usb_business)
+          should_not_have_recommendable_cards(@chase_personal, @usb_personal)
+          check :card_bp_filter_personal
+          should_have_recommendable_cards(*@cards)
+        end
+      end
+
+      specify "there is a 'filter by bank' dropdown" do
+        pending
+        is_expected.to have_selector "select#card_bank_filter"
+        within "select#card_bank_filter" do
+          is_expected.to have_text "All Banks"
+          is_expected.to have_text "Barclays"
+          is_expected.to have_text "Capital One"
+          is_expected.to have_text "American Express"
+          is_expected.to have_text "Chase"
+          is_expected.to have_text "US Bank"
+          is_expected.to have_text "Bank Of America"
+          is_expected.to have_text "Citibank"
+        end
       end
     end
 
-    describe "filtering cards"
 
     describe "the card account status dropdown" do
       it "is initially hidden" do
@@ -152,6 +185,14 @@ describe "admin section" do
 
     def card_radio_btn(card)
       "input#card_account_card_id_#{card.id}"
+    end
+
+    def have_recommendable_card(card)
+      have_selector recommendable_card_selector(card)
+    end
+
+    def recommendable_card_selector(card)
+      "##{dom_id(card, :admin_recommend)}"
     end
 
   end

commit 5b842101d853d509e5d4387bbe1a8945020b8843

fix CSS styles broken by fa27eb0a

app/assets/javascripts/admin/new_card_recommendation.js
@@ -5,11 +5,21 @@ $(document).ready(function () {
   });
 
   $('.card_bp_filter').click(function (e) {
-    var show  = this.checked,
+    var hide  = !this.checked,
         value = this.dataset.value;
     $("tr.admin_recommend_card").each(function (i, tr) {
+      var $tr = $(tr);
+
       if (value == tr.dataset.bp) {
-        tr.style.display = show ? "" : "none"
+        if (hide) {
+          // Add a dummy element after this one so that the Bootstrap
+          // .table-striped classes don't get messed up. See
+          // http://stackoverflow.com/a/20580140/1603071
+          $tr.after('<tr></tr>').hide();
+        } else {
+          // Remove the dummy <tr> added above.
+          $tr.show().next().remove();
+        }
       }
     });
   });