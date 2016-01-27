31c5310dec6be24b1e37f7b1d693f34d0320e563
George Millo <georgejulianmillo@gmail.com>
2016-01-27 21:31:24 +0100

    add per-page <title>s

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 module ApplicationHelper
   include BootstrapOverrides::Overrides
+
+  def full_title(page_title)
+    base_title = "Abroaders"
+    page_title.empty? ? base_title : "#{page_title.strip} | #{base_title}"
+  end
+
 end
