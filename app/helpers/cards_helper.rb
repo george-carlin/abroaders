17d662a9bda71eeebe304c6cfaf81781375ecea5
George Millo <georgejulianmillo@gmail.com>
2016-01-29 17:05:51 +0100

    Reduce unecessary complexity in helpers
    We don't need all these extra helper methods that are just a thin
    wrapper around the private method 'options_for_enum_select'. Make that
    method public, and remove the wrappers.

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -4,18 +4,6 @@ module CardsHelper
     "$%.2f" % (card.annual_fee_cents / 100.0)
   end
 
-  def options_for_card_brand_select
-    options_for_enum_select(Card.brands)
-  end
-
-  def options_for_card_bp_select
-    options_for_enum_select(Card.bps)
-  end
-
-  def options_for_card_type_select
-    options_for_enum_select(Card.types)
-  end
-
   def card_image_tag(card, size="180x114")
     id  = card.identifier
     dir = Rails.root.join("app", "assets", "images", "cards")
@@ -54,14 +42,6 @@ module CardsHelper
         data: { key: :bank, value: bank_id }
       ) << raw("&nbsp;&nbsp#{BankName.new(bank_id).name}")
     end
-  end
-
-  private
-
-  def options_for_enum_select(enum)
-    options_for_select(enum.keys.each_with_object({}) do |key, hash|
-      hash[key.capitalize] = key
-    end)
   end
 
 end
