cfc06870d6e94054153a873b0ccb22037877f1c4
George Millo <georgejulianmillo@gmail.com>
2016-02-04 17:26:52 -0600

    add card_offer factory

--- spec/factories/card_offers.rb
+++ spec/factories/card_offers.rb
@@ -1,11 +1,17 @@
 FactoryGirl.define do
-  factory :card_offer do
-    card nil
-points_awarded 1
-spend 1
-cost 1
-days 1
-status 1
-  end
+  factory :card_offer, aliases: [:live_card_offer] do
+    card
+    sequence(:identifier) do |n|
+      letters = ('A'..'Z').to_a.shuffle.first(4).join
+      "%02d-#{letters}" % n
+    end
+    points_awarded { rand(20) * 5_000 }
+    spend { rand(10) * 500 }
+    cost { rand(20) * 5 }
+    days { [30, 60, 90, 90, 90, 90, 90, 90, 120].sample }
 
+    factory :expired_card_offer do
+      status :expired
+    end
+  end
 end
