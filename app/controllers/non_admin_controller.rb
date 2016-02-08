e22b61614471f6c34ca6f4b16bd381bdda89081a
George Millo <georgejulianmillo@gmail.com>
2016-02-08 13:34:08 -0600

    fix redirection for users who haven't completed the survey

--- app/controllers/non_admin_controller.rb
+++ app/controllers/non_admin_controller.rb
@@ -8,8 +8,12 @@ class NonAdminController < AuthenticatedController
   protected
 
   def redirect_to_survey_if_incomplete
-    if !current_user.info.try(:persisted?) && controller_name != "user_infos"
-      redirect_to survey_path and return
+    if current_user.has_completed_user_info_survey?
+      if !current_user.info.has_completed_card_survey?
+        redirect_to card_survey_path unless request.path == card_survey_path
+      end
+    elsif controller_name != "user_infos"
+      redirect_to survey_path unless request.path == survey_path
     end
   end
 end
--- spec/features/survey_redirection_spec.rb
+++ spec/features/survey_redirection_spec.rb
@@ -6,11 +6,15 @@ describe "initial surveys" do
   context "as a new user" do
     include_context "logged in as new user"
 
-    context "when I have not yet filled in the initial survey" do
+    context "who has not filled in the contact/spending info survey" do
       describe "trying to visit any page" do
         it "redirects to the survey" do
-          pending
-          [new_travel_plan_path, current_cards_path].each do |path|
+          [
+            new_travel_plan_path,
+            card_accounts_path,
+            card_survey_path,
+            travel_plans_path
+          ].each do |path|
             visit path
             expect(current_path).to eq survey_path
           end
@@ -22,6 +26,37 @@ describe "initial surveys" do
           visit root_path
           expect(current_path).to eq survey_path
         end
+      end
+    end
+
+    context "who has filled in the contact/spending info survey" do
+      before { create(:user_info, user: user); user.reload }
+
+      context "but not the cards survey" do
+        describe "trying to visit any page" do
+          it "redirects to the cards survey" do
+            [
+              new_travel_plan_path,
+              card_accounts_path,
+              survey_path,
+              travel_plans_path
+            ].each do |path|
+              visit path
+              expect(current_path).to eq card_survey_path
+            end
+          end
+
+          pending "it also redirects from root_path" do
+            # leave this until we've created a proper 'dashboard' or whatever for
+            # logged on users on the root path
+            visit root_path
+            expect(current_path).to eq card_survey_path
+          end
+        end
+      end
+
+      context "and the cards survey" do
+        pending # add this once we've added the next steps in the survey
       end
     end
   end
