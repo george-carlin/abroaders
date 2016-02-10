e513284a5b4a8bad66eecb54147c8375e3ed3d63
George Millo <georgejulianmillo@gmail.com>
2016-02-10 15:02:44 -0600

    fix survey redirection for when the survey is complete
    (previously it was crashing)

--- app/controllers/non_admin_controller.rb
+++ app/controllers/non_admin_controller.rb
@@ -10,15 +10,21 @@ class NonAdminController < AuthenticatedController
   def redirect_to_survey_if_incomplete
     # The path to the current stage of the survey that the user needs to
     # complete. nil if the user has completed all survey stages:
-    current_survey_path = ({
+    current_survey_path = nil
+
+    {
       # method to test if stage is complete =>
       #             path to redirect to if stage is not complete
       :has_completed_user_info_survey? => survey_user_info_path,
       :has_completed_cards_survey?     => survey_card_accounts_path,
       :has_completed_balances_survey?  => survey_balances_path,
-    }.detect do |method, path|
-      path if !current_user.send(method)
-    end)[1]
+    }.each do |method, path|
+      if !current_user.send(method)
+        current_survey_path = path
+        break
+      end
+    end
+
     if current_survey_path && current_survey_path != request.path
       redirect_to current_survey_path
     end
