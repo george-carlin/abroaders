e79463b0ba0faa130b7f4702079088ee4894f0a2
George Millo <georgejulianmillo@gmail.com>
2016-03-16 15:14:39 +0000

    move around order of db migrations
    Now they'll actually work when you're creating the DB from scratch

--- db/migrate/20160310104932_rename_users_to_accounts.rb
+++ db/migrate/20160310104932_rename_users_to_accounts.rb
@@ -3,13 +3,5 @@ class RenameUsersToAccounts < ActiveRecord::Migration[5.0]
     rename_table :users, :accounts
     rename_column :surveys, :user_id, :account_id
     rename_column :travel_plans, :user_id, :account_id
-    rename_column :card_accounts, :user_id, :passenger_id
-    rename_column :balances, :user_id, :passenger_id
-    remove_foreign_key "card_accounts", "accounts"
-    add_foreign_key "card_accounts", "passengers", column: "passenger_id",
-                                                   on_delete: :cascade
-    remove_foreign_key "balances", "accounts"
-    add_foreign_key "balances", "passengers", column: "passenger_id",
-                                              on_delete: :cascade
   end
 end
--- db/migrate/20160310114511_rename_surveys_to_passengers.rb
+++ db/migrate/20160310114511_rename_surveys_to_passengers.rb
@@ -7,5 +7,14 @@ class RenameSurveysToPassengers < ActiveRecord::Migration[5.0]
     # passenger.
     remove_index :passengers, :account_id
     add_index :passengers, [:account_id, :main], unique: true
+
+    rename_column :card_accounts, :user_id, :passenger_id
+    rename_column :balances, :user_id, :passenger_id
+    remove_foreign_key "card_accounts", "accounts"
+    add_foreign_key "card_accounts", "passengers", column: "passenger_id",
+                                                   on_delete: :cascade
+    remove_foreign_key "balances", "accounts"
+    add_foreign_key "balances", "passengers", column: "passenger_id",
+                                              on_delete: :cascade
   end
 end
