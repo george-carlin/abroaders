34531710c1aa106a6c32d14699a46fc081d979b3
George Millo <georgejulianmillo@gmail.com>
2016-03-18 13:54:37 +0000

    update passenger factory with account onboarding stage

--- spec/factories/passengers.rb
+++ spec/factories/passengers.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :passenger, aliases: [:main_passenger] do
-    account
+    association :account, factory: :account, onboarding_stage: "spending"
     first_name   { Faker::Name.first_name }
     last_name    { Faker::Name.last_name  }
     phone_number { Faker::PhoneNumber.phone_number }
@@ -15,11 +15,18 @@ FactoryGirl.define do
     end
 
     trait :with_spending_info do
+      association :account, factory: :account, onboarding_stage: "cards"
       spending_info
     end
     # Potential contribution to FactoryGirl, allow for traits to be aliased
     trait :with_spending do
+      association :account, factory: :account, onboarding_stage: "cards"
       spending_info
+    end
+
+    trait :onboarded do
+      with_spending_info
+      association :account, factory: :account, onboarding_stage: "onboarded"
     end
 
     factory :companion,                    traits: [:companion]
