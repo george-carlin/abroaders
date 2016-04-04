dd820972c2b3981127c0328c166fb976662c7c3a
George Millo <georgejulianmillo@gmail.com>
2016-04-04 14:53:00 +0200

    DRY CardOffer validations

--- app/models/card_offer.rb
+++ app/models/card_offer.rb
@@ -9,32 +9,17 @@ class CardOffer < ApplicationRecord
 
   # Validations
 
-  validates :cost,
-    presence: true,
-    numericality: {
-      greater_than_or_equal_to: 0,
-      less_than_or_equal_to: POSTGRESQL_MAX_INT_VALUE
-    }
-  validates :days,
-    presence: true,
-    numericality: {
-      greater_than_or_equal_to: 0,
-      less_than_or_equal_to: POSTGRESQL_MAX_INT_VALUE
-    }
-  validates :identifier, presence: true, uniqueness: true
-  validates :points_awarded,
-    presence: true,
-    numericality: {
-      greater_than_or_equal_to: 0,
-      less_than_or_equal_to: POSTGRESQL_MAX_INT_VALUE
-    }
-  validates :status, presence: true
-  validates :spend,
-    presence: true,
-    numericality: {
-      greater_than_or_equal_to: 0,
-      less_than_or_equal_to: POSTGRESQL_MAX_INT_VALUE
-    }
+  with_options presence: true do
+    validates :identifier, uniqueness: true
+    validates :status
+
+    with_options numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: POSTGRESQL_MAX_INT_VALUE } do
+      validates :cost
+      validates :days
+      validates :points_awarded
+      validates :spend
+    end
+  end
 
   # Associations
 
