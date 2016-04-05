89bed3bba2f02d364b073c3c5c1505175c60cea3
George Millo <georgejulianmillo@gmail.com>
2016-04-05 19:14:41 +0200

    uncomment migration code
    this shouldn't have been commented

--- db/migrate/20160404110917_rename_bank_to_bank_id.rb
+++ db/migrate/20160404110917_rename_bank_to_bank_id.rb
@@ -1,8 +1,8 @@
 class RenameBankToBankId < ActiveRecord::Migration[5.0]
   def change
-    #remove_column :cards, :bank, :integer, null: false
-    #remove_index :cards, column: :bank_id
-    #add_column    :cards, :bank_id, :integer, null: false, index: true
+    remove_column :cards, :bank, :integer, null: false
+    remove_index :cards, column: :bank_id
+    add_column    :cards, :bank_id, :integer, null: false, index: true
     add_index :cards, :bank_id
   end
 end
