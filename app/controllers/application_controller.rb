3668a36980811cd7e14063a9114a0c463d22e7c4
George Millo <georgejulianmillo@gmail.com>
2016-04-11 17:50:37 +0200

    remove old survey redirection stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,33 +21,4 @@ class ApplicationController < ActionController::Base
     head :ok
   end
 
-  protected
-
-  # This will only ever be called from ApplicationController#dashboard, or from
-  # NonAdminController and its subclasses, since it only applies to logged-in
-  # users who aren't Admins (i.e. who are Accounts). application#dashboard is
-  # the only path that a logged in Account might visit that doesn't live under
-  # NonAdminController (and it can't live under that controller because the
-  # path in question is root_path, which for obvious reasons may be visited by
-  # anybody)
-  def redirect_to_survey
-    {
-      # Value of account.onboarding_stage =>
-      #       the path to redirect to if they're at this stage
-      "travel_plans"            => survey_travel_plan_path,
-      "passengers"              => survey_passengers_path,
-      "spending"                => survey_spending_path,
-      "main_passenger_cards"    => survey_card_accounts_path(:main),
-      "companion_cards"         => survey_card_accounts_path(:companion),
-      "main_passenger_balances" => survey_balances_path(:main),
-      "companion_balances"      => survey_balances_path(:companion),
-      "readiness"               => survey_readiness_path
-    }.each do |stage, path|
-      if current_account.onboarding_stage == stage && request.path != path
-        redirect_to path and return true
-      end
-    end
-    false
-  end
-
 end
