3bad1d34079d34a57b674a3fe9fba81b9d26a171
George Millo <georgejulianmillo@gmail.com>
2016-04-15 16:37:23 +0200

    remove unused JS file

--- app/assets/javascripts/other/travel_plans.js
+++
@@ -1,8 +0,0 @@
-// TODO add me again once we've added the date selecter back to the form
-// $(document).ready(function () {
-//   $('#travel_plan_daterange_select').datepicker({
-//     weekStart: 1,
-//     startDate: new Date(),
-//     todayHighlight: true,
-//   });
-// });
