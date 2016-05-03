13094810ef49d8386dcfb41d29d8cd4218022e9b
George Millo <georgejulianmillo@gmail.com>
2016-05-03 18:35:54 +0200

    add present_each helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,22 @@ module ApplicationHelper
 
   # See http://nithinbekal.com/posts/rails-presenters/
   def present(model, presenter_class=nil)
-    klass = presenter_class || "#{model.class}Presenter".constantize
-    presenter = klass.new(model, self)
-    yield(presenter) if block_given?
+    presenter = get_presenter(model, presenter_class)
+    yield(presenter)
   end
+
+  def present_each(collection, presenter_class=nil, &block)
+    collection.each do |model|
+      presenter = get_presenter(model, presenter_class)
+      yield(presenter)
+    end
+  end
+
+  private
+
+  def get_presenter(model, klass=nil)
+    klass ||= "#{model.class}Presenter".constantize
+    klass.new(model, self)
+  end
+
 end
