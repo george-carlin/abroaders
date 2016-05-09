1e8d4896444ab5a9594ac9fb461589a2a0f88e04
George Millo <georgejulianmillo@gmail.com>
2016-05-09 14:55:23 +0200

    add CardAccountPresenter

---
+++ app/presenters/card_account_presenter.rb
@@ -0,0 +1,71 @@
+class CardAccountPresenter < ApplicationPresenter
+
+  def render(&block)
+    h.render "card_accounts/card_account", card_account: self, &block
+  end
+
+  def render_recommendation_actions
+    h.render "card_accounts/recommendation_actions", card_account: self
+  end
+
+  %i[opened_at closed_at].each do |meth|
+    define_method meth do
+      super().strftime("%b %Y")
+    end
+  end
+
+  def apply_btn
+    h.link_to(
+      "Apply",
+      h.apply_card_account_path(self),
+      id: "card_recommendation_#{id}_apply_btn",
+      class: "card_recommendation_apply_btn btn btn-primary btn-sm",
+      target: "_blank"
+    )
+  end
+
+  def decline_form(&block)
+    h.form_tag(
+      h.decline_card_account_path(self),
+      class: "decline_card_recommendation_form",
+      style: "display:none;",
+      &block
+    )
+  end
+
+  def decline_reason_field_tag
+    h.text_field_tag(
+      "card_account[decline_reason]",
+      "",
+      class: "input-sm card_account_decline_reason",
+      id: "card_account_#{id}_decline_reason",
+      placeholder: "Why don't you want to apply for this card?",
+    )
+  end
+
+  def decline_btn
+    h.button_tag(
+      "No Thanks",
+      class: "card_recommendation_decline_btn btn btn-default btn-sm",
+      id:    "card_recommendation_#{id}_decline_btn",
+    )
+  end
+
+  def cancel_decline_btn
+    h.button_tag(
+      "Cancel",
+      class: "card_recommendation_cancel_decline_btn btn btn-sm btn-default",
+      id:    "card_recommendation_#{id}_cancel_decline_btn",
+    )
+  end
+
+  def confirm_decline_btn
+    h.button_tag(
+      "Confirm",
+      class: "card_recommendation_confirm_decline_btn btn btn-sm btn-primary",
+      id: "card_recommendation_#{id}_confirm_decline_btn",
+    )
+  end
+
+
+end
