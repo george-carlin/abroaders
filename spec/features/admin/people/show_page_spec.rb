b98b01a1fc6b3b1b1758658f3684502465526ef0
George Millo <georgejulianmillo@gmail.com>
2016-05-11 11:39:11 +0200

    remove outdated specs

--- spec/features/admin/people/show_page_spec.rb
+++ spec/features/admin/people/show_page_spec.rb
@@ -2,9 +2,6 @@ require "rails_helper"
 
 describe "admin section" do
   describe "show user page" do
-    # This spec is totally out of date, and the admin/people#show page is
-    # unimportant and likely to change in the future anyway, so skipping
-    # the failing specs for now
     subject { page }
 
     include_context "logged in as admin"
@@ -47,75 +44,6 @@ describe "admin section" do
 
     it "has the person's name as the page header" do
       is_expected.to have_selector "h1", text: "Fred"
-    end
-
-    it "displays the person's info" do
-      pending
-      is_expected.to have_info "email", @person.email
-    end
-
-    context "when the person" do
-      context "has not yet added their spending info" do
-        it "says so" do
-          pending
-          is_expected.to have_content t("admin.people.show.no_spending")
-          is_expected.to have_no_content t("admin.people.show.not_onboarded")
-        end
-
-        include_examples "does not have recommend or assign links"
-      end
-
-      context "has added their spending info" do
-        let(:extra_setup) do
-          @person.create_spending_info!(
-            business_spending_usd: 1500,
-            credit_score: 678,
-            has_business: :with_ein,
-          )
-          if onboarded
-            pending
-            @account.update_attributes!(onboarding_stage: :onboarded)
-          end
-        end
-        let(:onboarded) { false }
-
-        it "displays the spending info" do
-          pending
-          is_expected.to have_info "credit-score", 678
-          is_expected.to have_info "personal-spending", "$2500"
-          is_expected.to have_info "business-spending", "$1500"
-        end
-
-        context "but the account is still not fully onboarded" do
-          it "says so" do
-            pending
-            is_expected.to have_no_content t("admin.people.show.no_spending")
-            is_expected.to have_content t("admin.people.show.not_onboarded")
-          end
-
-          include_examples "does not have recommend or assign links"
-        end
-
-        context "and the account is fully onboarded" do
-          let(:onboarded) { true }
-          it "has links to recommend or assign a card" do
-            is_expected.to have_link recommend_link_text,
-                href: new_admin_person_card_recommendation_path(@person)
-          end
-        end
-      end
-    end
-
-    def card_account_selector(account)
-      "#card_account_#{account.id}"
-    end
-
-    def card_radio_btn(card)
-      "input#card_account_card_id_#{card.id}"
-    end
-
-    def have_info(attr, value)
-      have_selector ".person-#{attr}", text: value
     end
 
   end
