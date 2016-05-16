e2605da2dd6fe2a9934344440ca71838b6b8c2c3
George Millo <georgejulianmillo@gmail.com>
2016-05-16 15:07:08 +0200

    standardise on @account as ivar in CardRecsCtrler

--- app/controllers/card_recommendations_controller.rb
+++ app/controllers/card_recommendations_controller.rb
@@ -1,25 +1,24 @@
 class CardRecommendationsController < CardAccountsController
 
   def apply
-    @recommendation = load_card_account
+    @account = load_card_account
 
     # Make sure this is the right type of card account:
-    redirect_to card_accounts_path and return unless @recommendation.recommendation?
+    redirect_to card_accounts_path and return unless @account.recommendation?
 
     # We can't know for sure if the user has actually applied; the most
     # we can do is note that they've visited this page and (hopefully)
     # been redirected to the bank's page
-    @recommendation.update_attributes!(clicked_at: Time.now)
-    @card = @recommendation.card
+    @account.update_attributes!(clicked_at: Time.now)
+    @card = @account.card
   end
 
   def decline
     raise "decline message must be present" unless decline_reason.present?
-    @recommendation = load_card_account
+    @account = load_card_account
 
-    # TODO replace with a policy object and 'applyable?'
-    if @recommendation.recommendation?
-      @recommendation.decline_with_reason!(decline_reason)
+    if @account.declinable?
+      @account.decline_with_reason!(decline_reason)
       flash[:success] = t("admin.people.card_accounts.you_have_declined")
       redirect_to card_accounts_path
     else
