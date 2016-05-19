296ea586867806acbd5bafcfa95384f1a04fcdb6
George Millo <georgejulianmillo@gmail.com>
2016-05-19 15:55:27 +0200

    include account email in notification subject line
    The emails need to have distinct subjects, or GMail will thread the
    conversations, which messes up Erik's Zapier task

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -2,7 +2,7 @@ class AccountMailer < ApplicationMailer
 
   def notify_admin_of_sign_up(account_id)
     @account = Account.find(account_id)
-    mail(to: ENV["ERIKS_EMAIL"], subject: "New sign up at Abroaders app")
+    mail(to: ENV["ERIKS_EMAIL"], subject: "New sign up at Abroaders app - #{@account.email}")
   end
 
 end
