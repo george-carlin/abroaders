george-carlin/abroaders

commit 9d1b62e786323f545a80b5e31b6159116d6c2df2

add Bank#attributes
Also convert Bank to use Virtus

app/models/bank.rb
@@ -5,8 +5,10 @@
 # an ActiveRecord model as far as Card is concerned (so we can get methods like
 # Card#bank, Bank.find(id).cards, etc)
 class Bank
+  include Virtus.model
 
-  attr_reader :id, :name
+  attribute :id,   Fixnum
+  attribute :name, String
 
   # Note that only odd numbers are used for card IDs. This is because each card
   # has a deterministically-generated unique identifier which starts with a
@@ -30,12 +32,12 @@ class Bank
     23 => "Wells Fargo",
   }
 
-  class << self
-    alias_method :find, :new
+  def self.find(id)
+    new(id: id)
   end
 
   def self.all
-    TABLE.map { |id, _| new(id) }
+    TABLE.map { |id, _| find(id) }
   end
 
   def self.first
@@ -74,11 +76,11 @@ class Bank
     end
   end
 
-  def initialize(id)
-    unless @name = TABLE[id.to_i]
+  def initialize(attrs={})
+    super
+    unless @name = TABLE[id]
       raise ActiveRecord::RecordNotFound, "Couldn't find #{self.class} with 'id'=#{id}"
     end
-    @id = id.to_i
   end
 
   def cards
@@ -93,13 +95,10 @@ class Bank
     id <=> other_bank.id
   end
 
-  # Hash equality. This lets us do things like Card.all.group_by(&:bank)
-  # hash must return a fixnum that is always the same for identical Banks:
-  def hash
-    Digest::MD5.new.hexdigest("#{id}#{name}")[0..15].to_i(16)
-  end
-  def eql?(other_bank)
-    self == other_bank && hash == other_bank.hash
+  def attributes
+    # Virtus's 'attributes' method returns a hash with symbol keys, but
+    # ActiveRecord::Base#attributes uses string keys. Keep things consistent:
+    super.stringify_keys
   end
 
   def to_param

app/models/card.rb
@@ -53,7 +53,7 @@ class Card < ApplicationRecord
     end
 
     def bank
-      @bank ||= Bank.new(bank_id)
+      @bank ||= Bank.find(bank_id)
     end
 
     def bank=(new_bank)

spec/models/bank_spec.rb
@@ -8,6 +8,23 @@ describe Bank do
     expect(result.id).to eq 1
   end
 
+  example ".find_by" do
+    bank = Bank.find_by(name: "Chase")
+    expect(bank.name).to eq "Chase"
+    expect(bank.id).to eq 1
+  end
+
+  example ".all" do
+    banks = Bank.all
+    names = [
+      "Chase", "Citibank", "Barclays", "American Express", "Capital One",
+      "Bank of America", "US Bank", "Discover", "Diners Club", "SunTrust",
+      "TD Bank", "Wells Fargo",
+    ]
+    expect(banks.length).to eq names.length
+    expect(banks.map(&:name)).to match_array(names)
+  end
+
   example "#name" do
     bank = Bank.find(1)
     expect(bank.name).to eq "Chase"
@@ -38,6 +55,11 @@ describe Bank do
     b2 = Bank.find(3)
     expect(b0 == b1).to be true
     expect(b0 == b2).to be false
+  end
+
+  example "#attributes" do
+    bank = Bank.find(1)
+    expect(bank.attributes).to eq({ "id" => 1, "name" => "Chase" })
   end
 
 end

commit 88a035d70fd2cb39f002478c9fff15c9e5035b87

bug fix on card survey
It was broken by 9d1b62e786323f545a

app/models/bank.rb
@@ -112,6 +112,15 @@ class Bank
     id <=> other_bank.id
   end
 
+  # Hash equality. This lets us do things like Card.all.group_by(&:bank)
+  # hash must return a fixnum that is always the same for identical Banks:
+  def hash
+    Digest::MD5.new.hexdigest("#{id}#{name}")[0..15].to_i(16)
+  end
+  def eql?(other_bank)
+    self == other_bank && hash == other_bank.hash
+  end
+
   def attributes
     # Virtus's 'attributes' method returns a hash with symbol keys, but
     # ActiveRecord::Base#attributes uses string keys. Keep things consistent:

spec/features/card_accounts/survey_spec.rb
@@ -23,6 +23,7 @@ describe "card accounts survey", :onboarding do
       create(:card, :personal, :mastercard, bank_id: chase.id, name: "Card 1"),
       create(:card, :business, :mastercard, bank_id: citi.id,  name: "Card 2"),
       create(:card, :personal, :visa,       bank_id: citi.id,  name: "Card 3"),
+      create(:card, :personal, :visa,       bank_id: citi.id,  name: "Card 4"),
     ]
     @hidden_card = create(:card, shown_on_survey: false)
 
@@ -125,6 +126,16 @@ describe "card accounts survey", :onboarding do
         is_expected.to have_selector "h2", text: header
         is_expected.to have_selector "##{bank.to_param}_cards"
         is_expected.to have_selector "##{bank}_#{type}_cards"
+      end
+    end
+  end
+
+  it "only has one 'group' per bank and b/p" do # bug fix
+    %w[chase citibank].each do |bank|
+      %w[personal business].each do |type|
+        expect(all("[id='#{bank.to_param}_cards']").length).to eq 1
+        expect(all("[id='#{bank.to_param}_personal_cards']").length).to eq 1
+        expect(all("[id='#{bank.to_param}_business_cards']").length).to eq 1
       end
     end
   end

spec/forms/cards_survey_spec.rb
@@ -1,0 +1,37 @@
+require "rails_helper"
+
+describe CardsSurvey do
+  let(:survey) { described_class.new }
+
+  describe "#each_section" do
+    it "yields cards grouped by bank, then by b/p" do
+      currency = create(:currency)
+      b0 = Bank.first
+      b1 = Bank.last
+
+      c0 = create(:card, :personal, bank: b0, currency: currency)
+      c1 = create(:card, :personal, bank: b0, currency: currency)
+      c2 = create(:card, :business, bank: b0, currency: currency)
+      c3 = create(:card, :business, bank: b0, currency: currency)
+      c4 = create(:card, :personal, bank: b1,   currency: currency)
+      c5 = create(:card, :personal, bank: b1,   currency: currency)
+      c6 = create(:card, :business, bank: b1,   currency: currency)
+      c7 = create(:card, :business, bank: b1,   currency: currency)
+
+      banks = []
+      bank_groups = {}
+      survey.each_section do |bank, cards_grouped_by_bp|
+        banks << bank
+        bank_groups[bank.name] = cards_grouped_by_bp
+      end
+
+      expect(banks).to match_array [b0, b1]
+
+      expect(bank_groups[b0.name]["personal"]).to match_array([c0,c1])
+      expect(bank_groups[b0.name]["business"]).to match_array([c2,c3])
+      expect(bank_groups[b1.name]["personal"]).to match_array([c4,c5])
+      expect(bank_groups[b1.name]["business"]).to match_array([c6,c7])
+    end
+  end
+
+end