938c35c5ba8da895f34609478923fe27b9e24b9b
George Millo <georgejulianmillo@gmail.com>
2016-05-27 15:07:33 +0200

    add safety check to card_accounts#source migration
    the #source column has been removed in a later commit, which means this
    migration will crash when running all migrations from scratch. So wrap
    the relevant lines in a conditional.

--- db/migrate/20160430225929_add_source_to_card_accounts.rb
+++ db/migrate/20160430225929_add_source_to_card_accounts.rb
@@ -10,14 +10,16 @@ class AddSourceToCardAccounts < ActiveRecord::Migration[5.0]
           # update_all has to take the direct integer values of the enum,
           # not the pretty string defined in the class
 
-          # Safety check:
-          unless CardAccount.sources["from_survey"] == 0 &&
-                  CardAccount.sources["recommendation"] == 1
-            raise "something's grone wrong, cap'n!"
-          end
+          if CardAccount.any?
+            # Safety check:
+            unless CardAccount.sources["from_survey"] == 0 &&
+                    CardAccount.sources["recommendation"] == 1
+              raise "something's grone wrong, cap'n!"
+            end
 
-          CardAccount.where(recommended_at: nil).update_all(source: 0) # from_survey
-          CardAccount.where.not(recommended_at: nil).update_all(source: 1) # recommendation
+            CardAccount.where(recommended_at: nil).update_all(source: 0) # from_survey
+            CardAccount.where.not(recommended_at: nil).update_all(source: 1) # recommendation
+          end
           change_column :card_accounts, :source, :integer, null: false
         end
       end
