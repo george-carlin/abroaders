700d6c9729d34272b746f7d25e8958efe858e24e
George Millo <georgejulianmillo@gmail.com>
2016-05-30 15:09:22 +0200

    allow ButtonGroup component to take extra classNames

--- app/assets/javascripts/components/ButtonGroup/ButtonGroup.js.jsx
+++ app/assets/javascripts/components/ButtonGroup/ButtonGroup.js.jsx
@@ -1,9 +1,21 @@
 const React = require("react");
+const _     = require("underscore");
 
 const ButtonGroup = React.createClass({
+  propTypes: {
+    className: React.PropTypes.string,
+  },
+
   render() {
+    const className = this.props.className || "";
+    const classes   = className.split(/\s+/);
+
+    if (!_.includes(classes, "btn-group")) {
+      classes.push("btn-group");
+    }
+
     return (
-      <div className="btn-group">
+      <div className={classes.join(" ")}>
         {this.props.children}
       </div>
     );
