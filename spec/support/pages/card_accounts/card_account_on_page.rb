2b347383ccfa296404d11771701c440513c21009
George Millo <georgejulianmillo@gmail.com>
2016-06-01 15:44:12 +0200

    Bug fix in card account on page
    update set_approved_at_to to handle the case where the target date isn't
    in the current month. Otherwise the spec which calls this method will
    fail whenever the spec is being run on one of the first 5 days of a
    month (because it tries to select the date 5.days.ago)

--- spec/support/pages/card_accounts/card_account_on_page.rb
+++ spec/support/pages/card_accounts/card_account_on_page.rb
@@ -30,9 +30,28 @@ class CardAccountOnPage < ModelOnPage
     has_no_link? "Apply", href: apply_card_recommendation_path(card_account)
   end
 
+  # NOTE: If we start adding other datepickers to the app, and you find
+  # yourself semi-duplicating this method elsewhere, it might be a better idea
+  # to extract a new Page Object called DatePickerOnPage
   def set_approved_at_to(date)
     find("#" << approved_at).click
-    day = date.strftime("%e").strip
+    day = date.day.to_s
+
+    # Prevent an infinite loop from occurring in the "until" block below
+    if date > Date.today
+      raise "error: approved at must be today or in the past"
+    end
+
+    # If the date we want to find isn't in the current month, go back through
+    # the datepicker months until we find it.
+    get_current_month_in_datepicker = -> { Date.parse(find(".datepicker-switch").text) }
+    target_month = date.change(day: 1).to_date # drop the time part of the date if it's there
+
+    # Both dates are a Date object where day of the month is the 1st:
+    until get_current_month_in_datepicker.call == target_month
+      # Go back a month
+      find(".datepicker .prev").click
+    end
     find(".datepicker .day:not(.old)", text: day).click
   end
 
