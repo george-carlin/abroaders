b3b98d3564a21bc17c6f81cd717edb921e637313
George Millo <georgejulianmillo@gmail.com>
2016-06-05 16:11:05 +0200

    restore the previously-deleted React component 'Row'

---
+++ app/assets/javascripts/components/Row/Row.js.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+
+const Row = React.createClass({
+  render() {
+    return (
+      <div className="row">
+        {this.props.children}
+      </div>
+    );
+  },
+});
+
+module.exports = Row;
