a1b760a52690c88e82251c2017377a2a18c09dc2
George Millo <georgejulianmillo@gmail.com>
2016-06-27 19:15:40 +0200

    speed up card survey feature spec
    1. move stuff into before(:all) as much as possible
    2. move the 'can't access' tests into a controller spec.
    
    This reduces the total time that the tests take by over 50% - from
    ~55 secs to ~25 secs

---
+++ spec/controllers/card_accounts_controller_spec.rb
@@ -0,0 +1,58 @@
+require "rails_helper"
+
+describe CardAccountsController do
+  include_context "account devise mapping"
+
+  let(:account) { create(:account) }
+
+  describe "GET #survey" do
+    let(:account) do
+      create(
+        :account,
+        :onboarded_travel_plans => onboarded_travel_plans,
+        :onboarded_type         => onboarded_type,
+      )
+    end
+    let(:person) { account.owner }
+
+    subject do
+      create(:spending_info, person: person) if onboarded_spending
+      person.update_attributes!(onboarded_cards: true) if onboarded_cards
+      person.eligible_to_apply! if eligible
+      sign_in account
+      get :survey, params: { person_id: person.id }
+    end
+
+    context "when I haven't completed the travel plans survey" do
+      let(:onboarded_travel_plans) { false }
+      it { is_expected.to redirect_to new_travel_plan_path }
+    end
+
+    let(:eligible)           { true }
+    let(:onboarded_cards)    { false }
+    let(:onboarded_spending) { true }
+    let(:onboarded_type)     { true }
+    let(:onboarded_travel_plans) { true }
+
+    context "when I haven't chosen an account type yet" do
+      let(:onboarded_type) { false }
+      it { is_expected.to redirect_to type_account_path }
+    end
+
+    context "when I'm not eligible to apply for cards" do
+      let(:eligible) { false }
+      it { is_expected.to redirect_to survey_person_balances_path(person) }
+    end
+
+    context "when I need to complete the spending survey" do
+      let(:onboarded_spending) { false }
+      it { is_expected.to redirect_to new_person_spending_info_path(person) }
+    end
+
+    context "when I've already completed this survey" do
+      let(:onboarded_cards) { true }
+      it { is_expected.to redirect_to survey_person_balances_path(person) }
+    end
+
+  end
+end
