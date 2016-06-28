e12ab01f78fb272a73a031ded0af65e50173f6ac
George Millo <georgejulianmillo@gmail.com>
2016-06-28 19:19:53 +0200

    add output_account_csv.rb

---
+++ lib/output_account_csv.rb
@@ -0,0 +1,78 @@
+# Copy and paste this shitty code into the rails console on Heroku to print
+# out some CSV data that Erik can use:
+
+headers = [
+  "email",
+  "own_recommended_at",
+  "own_seen_at",
+  "own_clicked_at",
+  "own_applied_at",
+  "own_declined_at",
+  "own_opened_at",
+  "own_denied_at",
+  "own_ready_at",
+  "com_recommended_at",
+  "com_seen_at",
+  "com_clicked_at",
+  "com_applied_at",
+  "com_declined_at",
+  "com_opened_at",
+  "com_denied_at",
+  "com_ready_at",
+  "signed_up_at",
+  "onboarded",
+]
+
+def cols_for_rec_and_person(rec, person)
+  date_format = "%D"
+
+  [
+    rec&.recommended_at&.strftime(date_format),
+    rec&.seen_at&.strftime(date_format),
+    rec&.clicked_at&.strftime(date_format),
+    rec&.applied_at&.strftime(date_format),
+    rec&.declined_at&.strftime(date_format),
+    rec&.opened_at&.strftime(date_format),
+    rec&.denied_at&.strftime(date_format),
+    person.readiness_status&.created_at&.strftime("%D")
+  ]
+end
+
+person_includes = [:card_accounts, :card_recommendations, :readiness_status, :spending_info, :eligibility]
+
+rows = Account.includes(
+  people:    person_includes,
+  owner:     person_includes,
+  companion: person_includes,
+).find_each.map do |account|
+  row = [ account.email ]
+
+  owner    = account.owner
+  last_rec = owner.card_recommendations.order(created_at: :asc).first
+
+  row = [ account.email ] + cols_for_rec_and_person(last_rec, owner)
+
+  if account.has_companion?
+    companion = account.companion
+    last_rec  = companion.card_recommendations.order(created_at: :asc).first
+
+    row += cols_for_rec_and_person(last_rec, companion)
+  else
+    row += [nil]*8
+  end
+
+  row += [
+    account.created_at.strftime("%D"),
+    account.onboarded?
+  ]
+end
+
+rows.unshift(headers)
+
+path = Rails.root.join("tmp", "account-dump-#{rand(10000)}.csv")
+
+CSV.open(path, "w") do |csv|
+  rows.each { |row| csv << row }
+end
+
+puts File.read(path)
