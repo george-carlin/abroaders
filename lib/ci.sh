d3780f23dcf09e298e9b8a6be029ba8d65af0681
George Millo <georgejulianmillo@gmail.com>
2016-07-11 12:23:59 +0200

    Update ci.sh
    1. Install Gems and NPM packages before running tests
    2. say whether the build passed or not

--- lib/ci.sh
+++ lib/ci.sh
@@ -1,3 +1,8 @@
+bundle install
+npm install
 rails db:environment:set db:drop db:create db:schema:load RAILS_ENV=test
 rspec -f p
-say "CI finished"
+if [ $? -eq 0 ]
+then say "CI finished - build successful"
+else say "CI finished - build failed"
+fi
