9642892c928a50bc3f3053a1d3ffe9576b67ae30
George Millo <georgejulianmillo@gmail.com>
2016-07-18 17:06:01 +0100

    refactor and simplify survey cards spec

--- spec/features/card_accounts/index_page/survey_cards_spec.rb
+++ spec/features/card_accounts/index_page/survey_cards_spec.rb
@@ -1,201 +1,143 @@
 require "rails_helper"
 
-describe "as a user viewing my cards - survey cards section" do
+describe "card accounts page survey cards section" do
   subject { page }
 
   include_context "logged in"
 
-  let(:me) { account.owner }
+  let(:me)        { account.owner }
   let(:companion) { account.companion }
 
-  before do
-    create(:companion, account: account) if has_companion
-    @cards = create_list(:card, 2)
-    extra_setup
+  before(:all) { @cards = create_list(:card, 2) }
+  let(:cards)  { @cards }
+
+  def visit_page
     visit card_accounts_path
   end
 
-  let(:extra_setup) { nil }
+  def create_companion!
+    create(:person, main: false, account: account)
+    account.reload
+  end
 
-  let(:have_survey_cards_header) { have_selector "h2", text: "Other Cards" }
+  let(:have_survey_cards_header)    { have_selector "h2", text: "Other Cards" }
   let(:have_no_survey_cards_header) { have_no_selector "h2", text: "Other Cards" }
 
-  let(:survey_cards_section)         { "#card_accounts_from_survey" }
-  let(:owner_survey_cards_section)    { "#owner_card_accounts_from_survey" }
+  let(:survey_cards_section)           { "#card_accounts_from_survey" }
+  let(:owner_survey_cards_section)     { "#owner_card_accounts_from_survey" }
   let(:companion_survey_cards_section) { "#companion_card_accounts_from_survey" }
 
-  shared_context "I added cards in survey" do
-    let(:extra_setup) do
-      @open_account   = create(:open_survey_card_account, person: me, card: @cards[0])
-      @closed_account = create(:closed_survey_card_account, person: me, card: @cards[1])
-    end
-
-    let(:open_account)   { CardAccountOnPage.new(@open_account, self) }
-    let(:closed_account) { CardAccountOnPage.new(@closed_account, self) }
+  example "no companion and no 'from survey' cards" do
+    visit_page
+    # it has no 'Other cards' section
+    expect(page).to have_no_survey_cards_header
+    expect(page).to have_no_selector survey_cards_section
+    expect(page).to have_no_content "has no other cards"
   end
 
-  shared_examples "lists owner cards" do
-    it "has a section for them" do
-      is_expected.to have_survey_cards_header
-      is_expected.to have_selector owner_survey_cards_section
+  example "no companion; I have 'from survey' cards" do
+    open_acc   = create(:open_survey_card_account,   person: me, card: cards[0])
+    closed_acc = create(:closed_survey_card_account, person: me, card: cards[1])
+
+    open_acc_on_page   = CardAccountOnPage.new(open_acc, self)
+    closed_acc_on_page = CardAccountOnPage.new(closed_acc, self)
+
+    visit_page
+
+    # has a section for them:
+    expect(page).to have_survey_cards_header
+    expect(page).to have_selector owner_survey_cards_section
+
+    # lists them and their info:
+    within owner_survey_cards_section do
+      expect(open_acc_on_page).to be_present
+      expect(closed_acc_on_page).to be_present
     end
 
-    it "lists them" do
-      within owner_survey_cards_section do
-        expect(open_account).to be_present
-        expect(closed_account).to be_present
-      end
+    expect(open_acc_on_page).to have_info_for_a_survey_card
+    expect(closed_acc_on_page).to have_info_for_a_survey_card
 
-      expect(open_account).to have_content "Card Name: #{@cards[0].name}"
-      expect(open_account).to have_content "Bank: #{@cards[0].bank_name}"
-      expect(open_account).to have_content "Open"
-      expect(open_account).to have_content @open_account.opened_at.strftime("%b %Y")
-      expect(open_account).to have_no_content "Closed"
-
-      expect(closed_account).to have_content "Card Name: #{@cards[1].name}"
-      expect(closed_account).to have_content "Bank: #{@cards[1].bank_name}"
-      expect(closed_account).to have_content "Closed"
-      expect(closed_account).to have_content @closed_account.opened_at.strftime("%b %Y")
-      expect(closed_account).to have_content @closed_account.closed_at.strftime("%b %Y")
-    end
-
-    it "doesn't have apply/decline btns for them" do
-      expect(open_account).to have_no_apply_btn
-      expect(open_account).to have_no_decline_btn
-      expect(closed_account).to have_no_apply_btn
-      expect(closed_account).to have_no_decline_btn
-    end
+    expect(page).to have_no_selector "h2", text: "#{me.first_name}'s cards"
   end
 
-  context "when I have no companion" do
-    let(:has_companion) { false }
-
-    context "and I didn't add any cards in the onboarding survey" do
-      before { raise if me.card_accounts.from_survey.any? } # Sanity check
-
-      it "doesn't have an 'Other Cards' section'" do
-        is_expected.to have_no_survey_cards_header
-        is_expected.to have_no_selector survey_cards_section
-        is_expected.to have_no_content "has no other cards"
-      end
-    end
-
-    context "and I added cards in the onboarding survey" do
-      include_context  "I added cards in survey"
-      include_examples "lists owner cards"
-
-      it "doesn't divide 'Other Cards' into main/companion sections" do
-        is_expected.to have_no_selector "h2", text: "#{me.first_name}'s cards"
-      end
-    end
+  example "companion; no-one has survey cards" do
+    create_companion!
+    visit_page
+    expect(page).to have_no_survey_cards_header
+    expect(page).to have_no_selector survey_cards_section
   end
 
-  context "when I have a companion" do
-    let(:has_companion) { true }
+  example "companion; only owner has survey cards" do
+    create_companion!
 
-    context "and neither of us added any cards in the onboarding survey" do
-      it "doesn't have an 'Other Cards' section" do
-        is_expected.to have_no_survey_cards_header
-        is_expected.to have_no_selector survey_cards_section
-      end
+    open_acc   = create(:open_survey_card_account,   person: me, card: cards[0])
+    closed_acc = create(:closed_survey_card_account, person: me, card: cards[1])
+
+    open_acc_on_page   = CardAccountOnPage.new(open_acc, self)
+    closed_acc_on_page = CardAccountOnPage.new(closed_acc, self)
+
+    visit_page
+
+    # lists them and their info:
+    within owner_survey_cards_section do
+      expect(open_acc_on_page).to be_present
+      expect(closed_acc_on_page).to be_present
     end
 
-    context "and only I added cards in the onboarding survey" do
-      include_context  "I added cards in survey"
-      include_examples "lists owner cards"
+    expect(page).to have_selector "h3", text: "#{companion.first_name}'s Cards"
+    expect(page).to have_content "#{companion.first_name} has no other cards"
+  end
 
-      it "notes that my companion didn't add any cards" do
-        is_expected.to have_selector "h3", text: "#{companion.first_name}'s Cards"
-        is_expected.to have_content "#{companion.first_name} has no other cards"
-      end
+  example "companion; only companion has survey cards" do
+    create_companion!
+    open_acc   = create(:open_survey_card_account, person: companion, card: cards[0])
+    closed_acc = create(:closed_survey_card_account, person: companion, card: cards[1])
+
+    open_acc_on_page   = CardAccountOnPage.new(open_acc, self)
+    closed_acc_on_page = CardAccountOnPage.new(closed_acc, self)
+
+    visit_page
+
+    expect(page).to have_selector "h3", text: "#{companion.first_name}'s Cards"
+    within companion_survey_cards_section do
+      expect(open_acc_on_page).to be_present
+      expect(closed_acc_on_page).to be_present
     end
 
-    context "and only my companion added cards in the onboarding survey" do
-      let(:extra_setup) do
-        @open_account   = create(:open_survey_card_account, person: companion, card: @cards[0])
-        @closed_account = create(:closed_survey_card_account, person: companion, card: @cards[1])
-      end
+    expect(open_acc_on_page).to have_info_for_a_survey_card
+    expect(closed_acc_on_page).to have_info_for_a_survey_card
 
-      let(:open_account)   { CardAccountOnPage.new(@open_account, self) }
-      let(:closed_account) { CardAccountOnPage.new(@closed_account, self) }
+    expect(page).to have_selector "h3", text: "#{me.first_name}'s Cards"
+    expect(page).to have_content "#{me.first_name} has no other cards"
+  end
 
-      it "has a section for them" do
-        is_expected.to have_survey_cards_header
-        is_expected.to have_selector survey_cards_section
-      end
+  example "companion; both people have survey cards" do
+    m_open   = create(:open_survey_card_account,   person: me, card: cards[0])
+    m_closed = create(:closed_survey_card_account, person: me, card: cards[1])
+    create_companion!
+    p_open   = create(:open_survey_card_account,   person: companion, card: cards[0])
+    p_closed = create(:closed_survey_card_account, person: companion, card: cards[1])
 
-      it "lists them" do
-        within companion_survey_cards_section do
-          expect(open_account).to be_present
-          expect(closed_account).to be_present
-        end
+    m_open   = CardAccountOnPage.new(m_open,   self)
+    m_closed = CardAccountOnPage.new(m_closed, self)
+    p_open   = CardAccountOnPage.new(p_open,   self)
+    p_closed = CardAccountOnPage.new(p_closed, self)
 
-        expect(open_account).to have_content "Card Name: #{@cards[0].name}"
-        expect(open_account).to have_content "Bank: #{@cards[0].bank_name}"
-        expect(open_account).to have_content "Open"
-        expect(open_account).to have_content @open_account.opened_at.strftime("%b %Y")
-        expect(open_account).to have_no_content "Closed"
+    visit_page
 
-        expect(closed_account).to have_content "Card Name: #{@cards[1].name}"
-        expect(closed_account).to have_content "Bank: #{@cards[1].bank_name}"
-        expect(closed_account).to have_content "Closed"
-        expect(closed_account).to have_content @closed_account.opened_at.strftime("%b %Y")
-        expect(closed_account).to have_content @closed_account.closed_at.strftime("%b %Y")
-      end
-
-      it "doesn't have apply/decline btns for them" do
-        expect(open_account).to have_no_apply_btn
-        expect(open_account).to have_no_decline_btn
-        expect(closed_account).to have_no_apply_btn
-        expect(closed_account).to have_no_decline_btn
-      end
-
-      it "notes that I didn't add any cards" do
-        is_expected.to have_selector "h3", text: "#{me.first_name}'s Cards"
-        is_expected.to have_content "#{me.first_name} has no other cards"
-      end
+    within owner_survey_cards_section do
+      expect(m_open).to be_present
+      expect(m_closed).to be_present
     end
 
-    context "and my companion and I both added cards" do
-      let(:extra_setup) do
-        @m_open   = create(:open_survey_card_account,   person: me, card: @cards[0])
-        @m_closed = create(:closed_survey_card_account, person: me, card: @cards[1])
-        @p_open   = create(:open_survey_card_account,   person: companion, card: @cards[0])
-        @p_closed = create(:closed_survey_card_account, person: companion, card: @cards[1])
-      end
-
-      let(:m_open)   { CardAccountOnPage.new(@m_open,   self) }
-      let(:m_closed) { CardAccountOnPage.new(@m_closed, self) }
-      let(:p_open)   { CardAccountOnPage.new(@p_open,   self) }
-      let(:p_closed) { CardAccountOnPage.new(@p_closed, self) }
-
-      it "lists them all" do
-        within owner_survey_cards_section do
-          expect(m_open).to be_present
-          expect(m_closed).to be_present
-        end
-
-        within companion_survey_cards_section do
-          expect(p_open).to be_present
-          expect(p_closed).to be_present
-        end
-
-        [m_open, p_open].each do |account|
-          expect(account).to have_content "Card Name: #{@cards[0].name}"
-          expect(account).to have_content "Bank: #{@cards[0].bank_name}"
-          expect(account).to have_content "Open"
-          expect(account).to have_content account.model.opened_at.strftime("%b %Y")
-          expect(account).to have_no_content "Closed"
-        end
-
-        [m_closed, p_closed].each do |account|
-          expect(account).to have_content "Card Name: #{@cards[1].name}"
-          expect(account).to have_content "Bank: #{@cards[1].bank_name}"
-          expect(account).to have_content "Closed"
-          expect(account).to have_content account.model.opened_at.strftime("%b %Y")
-          expect(account).to have_content account.model.closed_at.strftime("%b %Y")
-        end
-      end
+    within companion_survey_cards_section do
+      expect(p_open).to be_present
+      expect(p_closed).to be_present
     end
+
+    expect(m_open).to have_info_for_a_survey_card
+    expect(m_closed).to have_info_for_a_survey_card
+    expect(p_open).to have_info_for_a_survey_card
+    expect(p_closed).to have_info_for_a_survey_card
   end
 end
--- spec/support/pages/card_accounts/card_account_on_page.rb
+++ spec/support/pages/card_accounts/card_account_on_page.rb
@@ -28,4 +28,32 @@ class CardAccountOnPage < RecordOnPage
 
   # =================================================
 
+  def has_info_for_a_survey_card?
+    has_basic_info = has_content?("Card Name: #{card.name}") &&
+                      has_content?("Bank: #{card.bank_name}") &&
+                      has_content?("Open") &&
+                      has_content?(card_account.opened_at.strftime("%b %Y")) &&
+                      has_no_apply_or_decline_btns?
+
+    if card_account.closed_at.present?
+      has_basic_info && 
+        has_content?("Closed") &&
+        has_content?(card_account.closed_at.strftime("%b %Y"))
+    else
+      has_basic_info && has_no_content?("Closed")
+    end
+  end
+
+  def has_apply_and_decline_btns?
+    has_apply_btn? && has_decline_btn?
+  end
+
+  def has_no_apply_or_decline_btns?
+    has_no_apply_btn? && has_no_decline_btn?
+  end
+
+  private
+
+  delegate :card, to: :card_account
+
 end
