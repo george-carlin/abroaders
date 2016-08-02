481e8b704083c4cd0cf8d3a1bbbcf9ab4fb8cf9b
George Millo <georgejulianmillo@gmail.com>
2016-08-02 14:45:12 +0200

    visual changes to card rec modal countdown

--- app/assets/javascripts/other/dashboard.js
+++ app/assets/javascripts/other/dashboard.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
      $('[data-countdown]').each(function() {
            var $this = $(this), finalDate = $(this).data('countdown');
            $this.countdown(finalDate, function(event) {
-                $this.html(event.strftime('%D Days, %H Hours, %M Minutes'));
+                $this.html(event.strftime('%D days, %H hours, %M minutes, %S seconds'));
                });
          });
 });
