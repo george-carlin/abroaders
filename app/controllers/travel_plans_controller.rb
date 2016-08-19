be9415745cb1360d0eb8de6a098169020992163b
George Millo <georgejulianmillo@gmail.com>
2016-08-19 17:21:35 +0200

    add comment
    --skip-ci

--- app/controllers/travel_plans_controller.rb
+++ app/controllers/travel_plans_controller.rb
@@ -61,6 +61,7 @@ class TravelPlansController < AuthenticatedUserController
   def load_countries
     countries = Destination.country.order("name ASC").to_a
 
+    # Move the U.S. and related 'countries' to the top of the list
     if ha = countries.detect { |c| c.name == "Hawaii" }
       countries.delete(ha)
       countries.unshift(ha)
