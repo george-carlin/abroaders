d73241cc5c792e23448e35077162a803af4dd214
George Millo <georgejulianmillo@gmail.com>
2016-09-02 14:04:05 +0200

    add comment
    --skip-ci

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -1,3 +1,10 @@
+# These emails are sent to an email address where they're then detected by
+# IfThisThenThat and used to trigger bunch of different APIs, set up by Erik.
+# We're doing it this way because it's quicker and easier than writing the Ruby
+# to trigger the APIs directly, and it means that Erik can easily tweak the
+# IFTTT script without having to use up developer time. Eventually we'll want
+# to update things so our app triggers the APIs directly, but it's low
+# priority.
 class AccountMailer < ApplicationMailer
 
   def notify_admin_of_sign_up(account_id)
