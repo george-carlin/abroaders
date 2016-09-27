34e95b8fc7d6a3ea9450a1a2491bc076f0246b2a
George Millo <georgejulianmillo@gmail.com>
2016-09-27 12:32:19 +0100

    fix bad indentation

--- app/helpers/admin_area/offers_helper.rb
+++ app/helpers/admin_area/offers_helper.rb
@@ -2,10 +2,10 @@ module AdminArea
   module OffersHelper
     def options_for_offer_condition_select(offer)
       options_for_select(
-          Offer.conditions.each_with_object({}) do |(key, _), hash|
-            hash[t("activerecord.attributes.offer.conditions.#{key}")] = key
-          end,
-          offer.condition
+        Offer.conditions.each_with_object({}) do |(key, _), hash|
+          hash[t("activerecord.attributes.offer.conditions.#{key}")] = key
+        end,
+        offer.condition
       )
     end
 
