59cab43650329bc4d66a136bed977f0f70a9a91f
George Millo <georgejulianmillo@gmail.com>
2016-10-06 00:51:34 +0200

    simplify account type form
    eligibility, phone number and spending have been/will be split into
    different survey pages

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -9,39 +9,16 @@ class AccountsController < AuthenticatedUserController
     @owner       = current_account.owner
   end
 
-  def create_solo_account
-    @solo_account = SoloAccountForm.new(solo_account_params)
-    @solo_account.account = current_account
-    # The front-end should prevent invalid data from being submitted. If they
-    # bypass the JS, fuck 'em.
-    @solo_account.save!
-    track_account_type_intercom_event!
-    redirect_to current_account.onboarding_survey.current_page.path
-  end
-
-  def create_couples_account
-    @couples_account = CouplesAccountForm.new(couples_account_params)
-    @couples_account.account = current_account
-    # The front-end should prevent invalid data from being submitted. If they
-    # bypass the JS, fuck 'em.
-    @couples_account.save!
-    track_account_type_intercom_event!
+  def submit_type
+    form = AccountTypeForm.new(account: current_account)
+    form.update!(account_type_params)
+    track_intercom_event("obs_account_type")
     redirect_to current_account.onboarding_survey.current_page.path
   end
 
   private
 
-  def solo_account_params
-    params.require(:solo_account).permit(:monthly_spending_usd, :eligible, :phone_number)
-  end
-
-  def couples_account_params
-    params.require(:couples_account).permit(
-      :monthly_spending_usd, :companion_first_name, :eligibility, :phone_number,
-    )
-  end
-
-  def track_account_type_intercom_event!
-    track_intercom_event("obs_account_type")
+  def account_type_params
+    params.require(:account).permit(:type, :companion_first_name)
   end
 end
--- app/forms/account_type_form.rb
+++ app/forms/account_type_form.rb
@@ -1,2 +1,27 @@
 class AccountTypeForm < ApplicationForm
+  attribute :account,              Account
+  attribute :type,                 String # 'solo' or 'couples'
+  attribute :companion_first_name, String
+
+  validates :companion_first_name,
+            presence: { if: :couples? },
+            absence: { if: :solo? }
+  validates :type, inclusion: { in: %w[solo couples] }
+
+  private
+
+  def persist!
+    if couples?
+      account.create_companion!(first_name: companion_first_name)
+    end
+    account.onboarding_survey.choose_account_type!
+  end
+
+  def solo?
+    type == 'solo'
+  end
+
+  def couples?
+    type == 'couples'
+  end
 end
