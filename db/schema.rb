8205538b4580fc3af30d233861d5f297123c18cd
George Millo <georgejulianmillo@gmail.com>
2016-10-11 19:12:21 +0100

    add accounts.onboarding_state DB column

--- db/schema.rb
+++ db/schema.rb
@@ -10,31 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160929214946) do
+ActiveRecord::Schema.define(version: 20161011141750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "accounts", force: :cascade do |t|
-    t.string   "email",                      default: "",    null: false
-    t.string   "encrypted_password",         default: "",    null: false
+    t.string   "email",                      default: "",              null: false
+    t.string   "encrypted_password",         default: "",              null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",              default: 0,     null: false
+    t.integer  "sign_in_count",              default: 0,               null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                 null: false
-    t.datetime "updated_at",                                 null: false
+    t.datetime "created_at",                                           null: false
+    t.datetime "updated_at",                                           null: false
     t.integer  "monthly_spending_usd"
-    t.boolean  "onboarded_type",             default: false, null: false
-    t.boolean  "onboarded_travel_plans",     default: false, null: false
-    t.integer  "unseen_notifications_count", default: 0,     null: false
+    t.integer  "unseen_notifications_count", default: 0,               null: false
     t.string   "phone_number"
-    t.boolean  "onboarded_home_airports",    default: false, null: false
+    t.string   "onboarding_state",           default: "home_airports", null: false
     t.index ["email"], name: "index_accounts_on_email", unique: true, using: :btree
+    t.index ["onboarding_state"], name: "index_accounts_on_onboarding_state", using: :btree
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true, using: :btree
   end
 
@@ -202,8 +201,6 @@ ActiveRecord::Schema.define(version: 20160929214946) do
     t.datetime "created_at",                              null: false
     t.datetime "updated_at",                              null: false
     t.boolean  "main",                    default: true,  null: false
-    t.boolean  "onboarded_cards",         default: false, null: false
-    t.boolean  "onboarded_balances",      default: false, null: false
     t.string   "award_wallet_email"
     t.datetime "last_recommendations_at"
     t.boolean  "eligible"
