3ca0839c5e57bbe973f730dadd9201f52e47fe3f
George Millo <georgejulianmillo@gmail.com>
2016-10-14 13:53:40 +0100

    Ignore jsx-no-bind ESLint errors

--- app/assets/javascripts/components/AccountTypeForm/SoloForm/Eligibility.js.jsx
+++ app/assets/javascripts/components/AccountTypeForm/SoloForm/Eligibility.js.jsx
@@ -1,3 +1,4 @@
+/* eslint react/jsx-no-bind: 0 */
 import React from "react";
 
 import Radio     from "../../core/Radio";
--- app/assets/javascripts/components/CardApplicationSurvey/ApplyOrDeclineBtns.js.jsx
+++ app/assets/javascripts/components/CardApplicationSurvey/ApplyOrDeclineBtns.js.jsx
@@ -1,3 +1,4 @@
+/* eslint react/jsx-no-bind: 0 */
 import React from "react";
 
 import Button       from "../core/Button";
