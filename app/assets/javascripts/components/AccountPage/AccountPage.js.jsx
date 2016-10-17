512cb1b173eb1262c16c8ffec9e30671323eec2b
Boris Shatalov <shatalov.bs@gmail.com>
2016-10-17 19:05:25 +0600

    write AccountPage component as a pure function

--- app/assets/javascripts/components/AccountPage/AccountPage.js.jsx
+++ app/assets/javascripts/components/AccountPage/AccountPage.js.jsx
@@ -6,56 +6,57 @@ import AccountTopInfo from "./AccountTopInfo";
 import HomeAirportsList from "./HomeAirportsList";
 import TravelPlansList from "./TravelPlansList";
 
-const AccountPage = React.createClass({
-  propTypes: {
-    alliances: React.PropTypes.array.isRequired,
-    account: React.PropTypes.object.isRequired,
-  },
+const AccountPage = (_props) => {
+  const props      = Object.assign({}, _props);
+  const account    = props.account;
 
-  render() {
-    const account = this.props.account;
+  return (
+    <div>
+      <AccountTopInfo
+        account={props.account}
+      />
 
-    return (
-      <div>
-        <AccountTopInfo
-          account={this.props.account}
-        />
-
-       {/*
-         hack to create some margin at the top of the page so the fixed position
-         'AccountInfo' hpanel doesn't overlap the start of the content
+      {/*
+       hack to create some margin at the top of the page so the fixed position
+       'AccountInfo' hpanel doesn't overlap the start of the content
        */}
-        <div style={{height: "180px"}}></div>
+      <div style={{height: "180px"}}></div>
 
-        <div className="hpanel">
-          <div className="panel-body">
-            <Row>
-              <BalancesTable
-                account={account}
-                alliances={this.props.alliances}
+      <div className="hpanel">
+        <div className="panel-body">
+          <Row>
+            <BalancesTable
+              account={account}
+              alliances={props.alliances}
+            />
+
+            <div className="col-xs-12 col-md-6">
+              <HomeAirportsList
+                homeAirports={account.homeAirports}
               />
 
-              <div className="col-xs-12 col-md-6">
-                <HomeAirportsList
-                  homeAirports={account.homeAirports}
-                />
-
-                {(() => {
-                  if (account.travelPlans.length > 0) {
-                    return (
-                      <TravelPlansList
-                        travelPlans={account.travelPlans}
-                      />
-                    );
-                  }
-                })()}
-              </div>
-            </Row>
-          </div>
+              {(() => {
+                if (account.travelPlans.length > 0) {
+                  return (
+                    <TravelPlansList
+                      travelPlans={account.travelPlans}
+                    />
+                  );
+                }
+              })()}
+            </div>
+          </Row>
         </div>
       </div>
-    );
-  },
-});
+    </div>
+  );
+};
+
+AccountPage.propTypes = Object.assign(
+  {
+    alliances: React.PropTypes.array.isRequired,
+    account: React.PropTypes.object.isRequired,
+  }
+);
 
 module.exports = AccountPage;
