97bbcbe2e43454c9823fbcbfdbae84cc8436e037
George Millo <georgejulianmillo@gmail.com>
2016-10-18 17:08:23 +0100

    add rubocop and eslint to bin/ci.sh

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,8 +1,27 @@
 bundle install
 npm install
+
+rubocop # automatically uses config file in .rubocop.yml
+if [ $? -ne 0 ]
+then
+  say "CI finished - build failed"
+  exit 1
+fi
+
+eslint --ext ".js,.jsx" app/assets
+if [ $? -ne 0 ]
+then
+  say "CI finished - build failed"
+  exit 1
+fi
+
 rails db:environment:set db:drop db:create db:schema:load RAILS_ENV=test
 rspec -f d
-if [ $? -eq 0 ]
-then say "CI finished - build successful"
-else say "CI finished - build failed"
+if [ $? -ne 0 ]
+then
+  say "CI finished - build failed"
+  exit 1
 fi
+
+say "CI finished - build successful"
+exit 0
