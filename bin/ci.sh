c3abba63c177047a78b2f79181d85d647922af54
George Millo <georgejulianmillo@gmail.com>
2016-10-19 15:26:50 +0100

    update CI scripts

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,3 +1,26 @@
+# Run the full test suite locally.
+#
+# How I use this: 
+#
+# cd ..
+# git clone abroaders ci
+# # edit config for 'ci' repo so that it uses a different DB to the main repo
+# # set up ENV vars for ci repo too if necessary
+#
+# Now I have a second copy of the codebase in the same dir as my main copy.
+# When I want to run all the tests I open a new Terminal window and enter:
+#
+# cd ../ci
+# git pull
+# gco branch-to-test
+# bin/ci.sh
+#
+# Now all my tests will run locally, and I can go back to my main repo and
+# continue to work and edit without worrying that anything I do will interfere
+# with the test run (and vica versa).
+#
+# Run bin/specs.sh to only run RSpec and skip ESLint/Rubocop
+
 bundle install
 npm install
 
