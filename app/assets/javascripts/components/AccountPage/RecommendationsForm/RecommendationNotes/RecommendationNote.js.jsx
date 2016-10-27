3c2250d62ad1bfc0aedd996a86251406fed2cf29
Boris Shatalov <shatalov.bs@gmail.com>
2016-10-27 12:26:33 +0600

    safe html for recommendation notes

--- app/assets/javascripts/components/AccountPage/RecommendationsForm/RecommendationNotes/RecommendationNote.js.jsx
+++ app/assets/javascripts/components/AccountPage/RecommendationsForm/RecommendationNotes/RecommendationNote.js.jsx
@@ -1,26 +1,25 @@
 import React from "react";
 
-const RecommendationNote = (_props) => {
-  const props = Object.assign({}, _props);
-  const recNote = props.recommendationNote;
-
-  return (
-    <div className="row">
-      <div className="col-xs-2">
-        <b>{recNote.createdAt + ":"}</b>
-      </div>
-
-      <div className="col-xs-10">
-        {recNote.content}
-      </div>
-    </div>
-  );
-};
-
-RecommendationNote.propTypes = Object.assign(
-  {
+const RecommendationNote = React.createClass({
+  propTypes: {
     recommendationNote: React.PropTypes.object.isRequired,
-  }
-);
+  },
+
+  componentDidMount() {
+    this.refs.noteContent.innerHTML = this.props.recommendationNote.content;
+  },
+
+  render() {
+    return (
+      <div className="row">
+        <div className="col-xs-2">
+          <b>{this.props.recommendationNote.createdAt + ":"}</b>
+        </div>
+
+        <div className="col-xs-10 note-content" ref="noteContent"></div>
+      </div>
+    );
+  },
+});
 
 export default RecommendationNote;
--- app/serializers/recommendation_note_serializer.rb
+++ app/serializers/recommendation_note_serializer.rb
@@ -1,5 +1,11 @@
 class RecommendationNoteSerializer < ApplicationSerializer
+  include ActionView::Helpers
+
   attributes :id, :content, :created_at
+
+  def content
+    auto_link(simple_format(object.content))
+  end
 
   def created_at
     object.created_at.strftime("%F %r")
