029e5db4c86eaab6b494e3f6380a0c89a01901ca
George Millo <georgejulianmillo@gmail.com>
2016-10-29 14:07:12 +0100

    add FB post-signup tracking code

--- app/controllers/home_airports_controller.rb
+++ app/controllers/home_airports_controller.rb
@@ -3,6 +3,13 @@ class HomeAirportsController < AuthenticatedUserController
 
   def survey
     @account = current_account
+    # We need to run a Javascript snippet immediately after a user signs up -
+    # but we need to make sure it doesn't get run again if they e.g. refresh
+    # the page. This hacky solution will do for now.
+    # (see also app/views/shared/third_party_scripts/_fb_tracking_pixel)
+    # Use a randomly-generated cookie name to obfuscate what it's for if the
+    # user looks at their cookies.
+    @output_fb_signup_code = !cookies[:cbd50008665cc7269327074d2778d9a6]
     @survey = HomeAirportsSurvey.new(account: @account)
   end
 
