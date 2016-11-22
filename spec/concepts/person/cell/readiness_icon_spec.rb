69c38b6d25d9fda89040799641845b110fdb62e7
George Millo <georgejulianmillo@gmail.com>
2016-11-22 18:53:48 +0100

    extract Person::Cell::ReadinessIcon

---
+++ spec/concepts/person/cell/readiness_icon_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe Person::Cell::ReadinessIcon do
+  let(:person) { Struct.new(:ready?, :eligible?).new(r, el) }
+  subject(:cell) { described_class.(person).() }
+
+  context 'when person is ready' do
+    let(:r)  { true }
+    let(:el) { true }
+    it { is_expected.to eq '(R)' }
+  end
+
+  context 'when person is eligible but not ready' do
+    let(:r)  { false }
+    let(:el) { true }
+    it { is_expected.to eq '(E)' }
+  end
+
+  context 'when person is ineligible' do
+    let(:r)  { false }
+    let(:el) { false }
+    it { is_expected.to eq '' }
+  end
+end
