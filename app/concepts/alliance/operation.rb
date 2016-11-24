6a7c3395697f294ae477f7877d902e0d20f96f67
George Millo <georgejulianmillo@gmail.com>
2016-11-24 08:46:46 +0000

    add Alliance::Create operation

---
+++ app/concepts/alliance/operation.rb
@@ -0,0 +1,30 @@
+class Alliance < ApplicationRecord
+  class Create < Trailblazer::Operation
+    include Model
+    model Alliance, :create
+
+    contract do
+      feature Reform::Form::Coercion
+
+      property :name,  type: Types::Stripped::String
+      property :order
+
+      validation do
+        required(:name).filled { str? }
+        optional(:order).maybe
+      end
+    end
+
+    def process(params)
+      validate params[:alliance] do |f|
+        f.sync
+        f.model.order ||= begin
+          # TODO not sure this belongs in the 'process' method. Also this isn't
+          # ideal as it doesn't use the next *unfilled* value.
+          (Alliance.maximum(:order) || 0) + 1
+        end
+        f.model.save
+      end
+    end
+  end
+end
