066693a2392f39bfcccdb565593494707348a3cf
George Millo <georgejulianmillo@gmail.com>
2016-11-24 14:53:21 +0100

    add bin/copy_production_db_to_local.sh

---
+++ bin/copy_production_db_to_local.sh
@@ -0,0 +1,4 @@
+heroku pg:backups capture --app abroaders
+curl -o latest.dump `heroku pg:backups public-url`
+# If you're trying to use this file and you're not George, you'll probably need to change this line:
+pg_restore --verbose --clean --no-acl --no-owner -h localhost -U george -d abroaders_development latest.dump
