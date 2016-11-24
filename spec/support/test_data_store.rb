1a0cdccf9d3556729806b875eb2376619d8be697
George Millo <georgejulianmillo@gmail.com>
2016-11-24 14:46:56 +0100

    use ApplicationRecord.class_eval in TestDataStore
    otherwise tests can fail if the 'real' ApplicationRecord
    file doesn't get autoloaded first

--- spec/support/test_data_store.rb
+++ spec/support/test_data_store.rb
@@ -20,7 +20,7 @@ class TestDataStore
   end
 end
 
-class ApplicationRecord < ActiveRecord::Base
+ApplicationRecord.class_eval do
   cattr_accessor :__storing_on
   after_create :__store_test_data, if: :__storing_on
 
