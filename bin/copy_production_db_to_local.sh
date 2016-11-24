063cde4bfc3e066c3a75885526ad8139678c7900
George Millo <georgejulianmillo@gmail.com>
2016-11-24 14:58:14 +0100

    set db environment in bin/copy_production_db_to_local

--- bin/copy_production_db_to_local.sh
+++ bin/copy_production_db_to_local.sh
@@ -2,3 +2,4 @@ heroku pg:backups capture --app abroaders
 curl -o latest.dump `heroku pg:backups public-url`
 # If you're trying to use this file and you're not George, you'll probably need to change this line:
 pg_restore --verbose --clean --no-acl --no-owner -h localhost -U george -d abroaders_development latest.dump
+rake db:environment:set RAILS_ENV=development
