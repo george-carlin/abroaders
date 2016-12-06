fa9322366ede22cc959f37d43eafec10dc5f21d9
George Millo <georgejulianmillo@gmail.com>
2016-12-06 19:32:59 +0000

    extract Recommendation products table cell

---
+++ app/concepts/recommendation/cell/admin/new/products_table/row.rb
@@ -0,0 +1,31 @@
+class Recommendation::Cell::Admin::New::ProductsTable::Row < ::Product::Cell
+  include ActionView::Helpers::RecordTagHelper
+
+  private
+
+  def tr_tag(&block)
+    content_tag(
+      :tr,
+      id: html_id,
+      class: html_classes,
+      data: data,
+      &block
+    )
+  end
+
+  def data
+    {
+      bp:       model.bp,
+      bank:     model.bank_id,
+      currency: model.currency_id,
+    }
+  end
+
+  def html_id
+    dom_id(model, :admin_recommend)
+  end
+
+  def html_classes
+    dom_class(model, :admin_recommend)
+  end
+end
