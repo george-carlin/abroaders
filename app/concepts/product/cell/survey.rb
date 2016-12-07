f5cb433ed6ca9e9233b9dee598e1cbf91fcbb511
George Millo <georgejulianmillo@gmail.com>
2016-12-07 19:20:58 +0000

    extract Product::Cell::Survey::Product
    closes #79

---
+++ app/concepts/product/cell/survey.rb
@@ -0,0 +1,3 @@
+class Product::Cell::Survey < Trailblazer::Cell
+  autoload :Product, 'product/cell/survey/product'
+end
---
+++ spec/concepts/product/cell/survey/product_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Product::Cell::Survey::Product, type: :view do
+  let(:product) { create(:product, name: 'Sapphire', network: :visa) }
+  let(:cell)    { described_class.(product, context: CELL_CONTEXT).show }
+
+  describe 'card name' do
+    context 'for a business card' do
+      before { product.update!(bp: :business) }
+
+      it 'has the format "<product name>, business, <network>"' do
+        expect(cell).to have_content 'Sapphire business Visa'
+      end
+    end
+
+    context 'for a personal card' do
+      before { product.update!(bp: :personal) }
+
+      it 'has the format "<card name>, <network>"' do
+        expect(cell).to have_content 'Sapphire Visa'
+      end
+    end
+  end
+end
