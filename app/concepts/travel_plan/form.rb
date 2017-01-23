562382e59ee8bf83d06c253f97d0b2765f7cb95b
George Millo <georgejulianmillo@gmail.com>
2017-01-23 18:50:23 +0000

    add TravelPlan::Form (WIP)
    part of the switch from our own custom abstractions to Trailblazer and
    Reform

---
+++ app/concepts/travel_plan/form.rb
@@ -0,0 +1,158 @@
+class TravelPlan < ApplicationRecord
+  class Form < Reform::Form
+    feature Reform::Form::Coercion
+
+    AIRPORT_REGEX  = /\(([A-Z]{3})\)\s*\z/
+    # An AirportString is a string in the format "Airport Name (XXX)" where XXX
+    # is an IATA code. E.g. "London Heathrow (LHR)". On the front-end,
+    # typeahead.js will fill the HTML <input> with a string in this format
+    # which then gets submitted to the server. It's our job at the back-end to
+    # make sense of the string and figure out which airport they mean.
+    #
+    # Previously we were trying to do more on the frontend by having the JS
+    # also update a hidden field with the airport's ID, and submitting that ID
+    # to the server directly. Turns out that this approach didn't work in some
+    # edge cases, so the current 'least bad' solution is to submit the full
+    # string and let the server figure out the airport ID for itself.
+    AirportString = Types::Strict::String.constrained(format: AIRPORT_REGEX)
+
+    property :from, type: AirportString, virtual: true
+    property :to,   type: AirportString, virtual: true
+    # It's not possible to submit an invalid type through the normal form, so let it blow up:
+    property :type, type: Types::Strict::String.enum('single', 'return'), default: "return"
+    property :no_of_passengers,        type: Types::Form::Int, default: 1
+    property :accepts_economy,         type: Types::Form::Bool, default: false
+    property :accepts_premium_economy, type: Types::Form::Bool, default: false
+    property :accepts_business_class,  type: Types::Form::Bool, default: false
+    property :accepts_first_class,     type: Types::Form::Bool, default: false
+    # Call these '*_date' rather than '*_on' (which are the names of the
+    # underlying DB column) so that we get friendly error messages
+    property :depart_on, type: Types::Form::AmericanDate
+    property :return_on, type: Types::Form::AmericanDate
+    property :further_information, type: Types::Stripped::String.optional
+
+    def sync
+      super
+      model.flights.build(
+        from: Airport.find_by_code!(AIRPORT_REGEX.match(from)[1]),
+        to:   Airport.find_by_code!(AIRPORT_REGEX.match(to)[1]),
+      )
+      model
+    end
+
+    def self.model_name
+      TravelPlan.model_name
+    end
+
+    def self.types
+      ::TravelPlan.types.slice("single", "return")
+    end
+
+    def depart_on_str
+      if depart_on.is_a?(Date)
+        depart_on.strftime(Types::Form::AmericanDate.meta(:format))
+      elsif depart_on.nil?
+        ""
+      else
+        depart_on
+      end
+    end
+
+    def return_on_str
+      if return_on.is_a?(Date)
+        return_on.strftime(Types::Form::AmericanDate.meta(:format))
+      elsif return_on.nil?
+        ""
+      else
+        return_on
+      end
+    end
+
+    def owner_name(suffix = false)
+      suffix = suffix ? "r" : ""
+      "you#{suffix}"
+    end
+
+    validation do
+      # TODO convert to use dry-validation
+      validates :depart_on, presence: true
+      validates :from, presence: true
+      validates(
+        :no_of_passengers,
+        numericality: {
+          greater_than_or_equal_to: 1,
+          less_than_or_equal_to: TravelPlan::MAX_PASSENGERS,
+          # avoid a duplicative error message when blank:
+          allow_blank: true,
+        },
+        presence: true,
+      )
+      validates :to, presence: true
+
+      validates :return_on, presence: { if: :return? }, absence: { if: :single? }
+      validates :further_information, length: { maximum: 500 }
+      validate :depart_on_is_in_the_future
+      validate :return_on_is_in_the_future
+      validate :return_is_later_than_or_equal_to_departure
+      # Not bothering to validate that 'from' or 'to' a) follow the expected
+      # format or b) correspond to real airports in our DB. In both cases they
+      # shouldn't be able to submit data like this unless they've been
+      # tinkering with the HTML form, in which case we can just let it blow up
+      # and don'to bother displaying pretty validation errors to them.
+    end
+
+    private
+
+    def depart_on_is_in_the_future
+      if depart_on.is_a?(Date)
+        if depart_on < Time.zone.today
+          errors.add(:depart_on, "can't be in the past")
+        end
+      end
+    end
+
+    def return_on_is_in_the_future
+      if return_on.is_a?(Date)
+        if return_on < Time.zone.today
+          errors.add(:return_on, "can't be in the past")
+        end
+      end
+    end
+
+    def return_is_later_than_or_equal_to_departure
+      if return_on.is_a?(Date)
+        if depart_on.is_a?(Date) && return_on < depart_on
+          errors.add(:return_on, "can't be earlier than departure date")
+        end
+      end
+    end
+
+    def travel_plan_attributes
+      {
+        acceptable_classes:   acceptable_classes,
+        depart_on:            depart_on,
+        further_information:  (further_information.strip if further_information.present?),
+        no_of_passengers:     no_of_passengers,
+        return_on:            return_on,
+        type:                 type,
+      }
+    end
+
+    def acceptable_classes
+      [
+        (:economy         if accepts_economy?),
+        (:premium_economy if accepts_premium_economy?),
+        (:business_class  if accepts_business_class?),
+        (:first_class     if accepts_first_class?),
+      ].compact
+    end
+
+    def single?
+      type == "single"
+    end
+
+    def return?
+      type == "return"
+    end
+  end
+end
---
+++ spec/concepts/travel_plan/form_spec.rb
@@ -0,0 +1,144 @@
+require "rails_helper"
+
+RSpec.describe TravelPlan::Form, type: :model do
+  let(:account) { Account.new }
+  let(:form) { new_form }
+  subject { form }
+
+  def new_form
+    described_class.new(account.travel_plans.new)
+  end
+
+  def validate(attrs)
+    new_form.validate(attrs)
+  end
+
+  def errors_for(attrs={})
+    form = new_form
+    form.validate(attrs)
+    form.errors
+  end
+
+  it { is_expected.to validate_presence_of(:depart_on) }
+  it { is_expected.to validate_presence_of(:no_of_passengers) }
+
+  it 'from and to must be present' do
+    errors_when_blank = errors_for()
+    expect(errors_when_blank[:from]).to eq ["can't be blank"]
+    expect(errors_when_blank[:to]).to eq ["can't be blank"]
+  end
+
+  describe "#no_of_passengers" do
+    it 'must be >= 1, <= 20' do
+      def errors_for_no(no)
+        errors_for(no_of_passengers: no)[:no_of_passengers]
+      end
+
+      expect(errors_for_no(0)).to eq ['must be greater than or equal to 1']
+      expect(errors_for_no(1)).to be_empty
+      expect(errors_for_no(20)).to be_empty
+      expect(errors_for_no(21)).to eq ['must be less than or equal to 20']
+    end
+  end
+
+  it 'further_information is <= 500 chars' do
+    def errors_for_length(l)
+      errors_for(further_information: ('a' * l))[:further_information]
+    end
+
+    expect(errors_for_length(500)).to be_empty
+    expect(errors_for_length(501)).to eq ['is too long (maximum is 500 characters)']
+  end
+
+  specify "depart_on must be present and not in the past" do
+    def errors_for_date(date)
+      errors_for(depart_on: date)[:depart_on]
+    end
+
+    expect(errors_for_date(nil)).to eq ["can't be blank"]
+    form.depart_on = Time.zone.yesterday
+    expect(errors_for_date(Time.zone.yesterday)).to eq ["can't be in the past"]
+    expect(errors_for_date(Time.zone.today)).to be_empty
+  end
+
+  specify 'return_on must be blank when type is single' do
+    form.type = 'single'
+    expect(form).to validate_absence_of(:return_on)
+  end
+
+  context "when type is return" do
+    specify "return date must be present and not in the past" do
+      def errors_for_date(date)
+        errors_for(return_on: date, type: 'return')[:return_on]
+      end
+
+      expect(errors_for_date(nil)).to include "can't be blank"
+      expect(errors_for_date(Time.zone.yesterday)).to include "can't be in the past"
+      expect(errors_for_date(Time.zone.today)).to be_empty
+    end
+
+    specify "return date must be >= departure" do
+      def errors_for_dates(dep, ret)
+        errors_for(depart_on: dep, return_on: ret, type: 'return')[:return_on]
+      end
+
+      tomorrow = Time.zone.tomorrow
+      today    = Time.zone.today
+      expect(errors_for_dates(tomorrow, today)).to eq ["can't be earlier than departure date"]
+      expect(errors_for_dates(tomorrow, tomorrow)).to be_empty
+    end
+  end
+
+  describe 'saving' do
+    let(:airport_0) { create(:airport) }
+    let(:airport_1) { create(:airport) }
+
+    it 'creates a new travel plan with flights' do
+      account = create(:account, :onboarded)
+      form    = described_class.new(account.travel_plans.new)
+      expect(
+        form.validate(
+          from: "#{airport_0.name} (#{airport_0.code})",
+          to: "#{airport_1.name} (#{airport_1.code})",
+          type: 'return',
+          no_of_passengers: 5,
+          accepts_economy: true,
+          accepts_premium_economy: true,
+          accepts_business_class: true,
+          accepts_first_class: true,
+          depart_on: '05/08/2025',
+          return_on: '02/03/2026',
+          further_information: 'blah blah blah',
+        )
+      ).to be true
+      expect(form.save).to be true
+      tp = form.model
+      expect(tp.flights[0].from).to eq airport_0
+      expect(tp.flights[0].to).to eq airport_1
+      expect(tp.type).to eq 'return'
+      expect(tp.no_of_passengers).to eq 5
+      expect(tp.accepts_economy).to be true
+      expect(tp.accepts_premium_economy).to be true
+      expect(tp.accepts_business_class).to be true
+      expect(tp.accepts_first_class).to be true
+      expect(tp.depart_on).to eq Date.new(2025, 5, 8)
+      expect(tp.return_on).to eq Date.new(2026, 2, 3)
+      expect(tp.further_information).to eq 'blah blah blah'
+    end
+
+    # TODO move to an operation called TravelPlan::Onboard
+    skip "when account is not onboarded" do
+      it "updates the account's onboarding state" do
+        account = create(:account, onboarding_state: :travel_plan)
+        form.account = account
+        form.from = airport_0.full_name
+        form.to   = airport_1.full_name
+        form.no_of_passengers = 1
+        form.depart_on   = Time.zone.today
+        expect { form.save! }.to change { account.travel_plans.count }.by(1)
+        account.reload
+        expect(account.onboarding_state).to eq "account_type"
+      end
+    end
+  end
+end
