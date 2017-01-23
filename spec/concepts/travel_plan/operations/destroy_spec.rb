f459c791ad64f54cfe00eaa081485b481f9d733f
George Millo <georgejulianmillo@gmail.com>
2017-01-23 14:47:44 +0000

    add TravelPlan::Operations::Destroy

---
+++ spec/concepts/travel_plan/operations/destroy_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe TravelPlan::Operations::Destroy do
+  let(:travel_plan) { create(:travel_plan) }
+  let(:account) { travel_plan.account }
+  let(:op) { described_class }
+
+  it 'destroys the travel plan' do
+    result = op.({ id: travel_plan.id }, 'current_account' => account)
+    expect(result.success?).to be true
+    expect(TravelPlan.find_by(id: travel_plan.id)).to be nil
+  end
+
+  it "can't destroy someone else's travel plan" do
+    other_account = create(:account)
+    expect do
+      op.({ id: travel_plan.id }, 'current_account' => other_account)
+    end.to raise_error ActiveRecord::RecordNotFound
+  end
+end
