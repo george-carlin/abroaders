2aecbec421aaeebf8f9d055a14ded013c4e25905
George Millo <georgejulianmillo@gmail.com>
2017-01-23 14:47:44 +0000

    add TravelPlan::Operations::Destroy

---
+++ app/concepts/travel_plan/operations/destroy.rb
@@ -0,0 +1,18 @@
+class TravelPlan < ApplicationRecord
+  module Operations
+    class Destroy < Trailblazer::Operation
+      step :setup_model!
+      step :destroy_travel_plan!
+
+      private
+
+      def setup_model!(opts, params:, current_account:, **)
+        opts['model'] = current_account.travel_plans.find(params[:id])
+      end
+
+      def destroy_travel_plan!(opts, model:, **)
+        model.destroy!
+      end
+    end
+  end
+end
