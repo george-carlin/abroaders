9e687424a7ccec963e4703b9c1da8358d942b071
George Millo <georgejulianmillo@gmail.com>
2017-01-27 13:05:29 -0600

    remove old unused code

--- app/controllers/admin_area/travel_plans_controller.rb
+++ app/controllers/admin_area/travel_plans_controller.rb
@@ -1,20 +1,11 @@
 module AdminArea
   class TravelPlansController < AdminController
     def edit
-      render(text: "temporarily disabled") && return
-      # @travel_plan = AdminArea::EditTravelPlanForm.find(params[:id])
-      # render "travel_plans/edit"
+      render(text: 'editing travel plans by admins is not yet implemented. Go moan at George until he gets it down') && return
     end
 
     def update
-      render(text: "temporarily disabled") && return
-      # @travel_plan = AdminArea::EditTravelPlanForm.find(params[:id])
-      # if @travel_plan.update_attributes(travel_plan_params)
-      #   flash[:success] = "Travel plan for #{@travel_plan.owner_name} has been updated"
-      #   redirect_to admin_person_path(@travel_plan.owner)
-      # else
-      #   render "travel_plans/edit"
-      # end
+      render(text: 'editing travel plans by admins is not yet implemented. Go moan at George until he gets it down') && return
     end
   end
 end
