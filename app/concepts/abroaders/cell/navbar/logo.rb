958430213a1696faf241094e991ea71a2ca89703
George Millo <georgejulianmillo@gmail.com>
2017-01-29 16:42:59 -0600

    Abroaders::Cell is a module, not a class

--- app/concepts/abroaders/cell/navbar/logo.rb
+++ app/concepts/abroaders/cell/navbar/logo.rb
@@ -1,17 +1,19 @@
-class Abroaders::Cell < Trailblazer::Cell
-  class Navbar::Logo < Trailblazer::Cell
-    private
+module Abroaders
+  module Cell
+    class Navbar::Logo < Trailblazer::Cell
+      private
 
-    def html_classes
-      admin? ? 'admin-navbar' : ''
-    end
+      def html_classes
+        admin? ? 'admin-navbar' : ''
+      end
 
-    def text
-      raw("Abroaders#{' <small>(Admin)</small>' if admin?}")
-    end
+      def text
+        raw("Abroaders#{' <small>(Admin)</small>' if admin?}")
+      end
 
-    def admin?
-      model.is_a?(Admin)
+      def admin?
+        model.is_a?(Admin)
+      end
     end
   end
 end
