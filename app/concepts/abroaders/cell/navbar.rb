44a86d0c1720774a7b67121b4747e58a6d621779
George Millo <georgejulianmillo@gmail.com>
2017-01-29 16:42:59 -0600

    Abroaders::Cell is a module, not a class

--- app/concepts/abroaders/cell/navbar.rb
+++ app/concepts/abroaders/cell/navbar.rb
@@ -1,139 +1,141 @@
-class Abroaders::Cell < Trailblazer::Cell
-  class Navbar < Trailblazer::Cell
-    include ::Cell::Builder
+module Abroaders
+  module Cell
+    class Navbar < Trailblazer::Cell
+      include ::Cell::Builder
 
-    builds do |user|
-      case user
-      when Account then AccountNavbar
-      when Admin   then AdminNavbar
-      when nil then SignedOutNavbar
-      end
-    end
-
-    def show
-      root_path
-
-      render view: 'navbar' # use the same ERB file even in subclasses
-    end
-
-    private
-
-    def bars
-      if sidebar?
-        '<div class="header-link hide-menu"><i class="fa fa-bars"></i></div>'
-      else
-        ''
-      end
-    end
-
-    def search_form
-      ''
-    end
-
-    def pad_logo?
-      !sidebar?
-    end
-
-    def logo
-      cell(Logo)
-    end
-
-    def username
-      ''
-    end
-
-    def notifications
-      ''
-    end
-
-    class SignedInNavbar < self
-      def username
-        content_tag :li, model.email, class: :text
-      end
-
-      def links
-        content_tag :li do
-          link_to sign_out_path, method: :delete, id: 'sign_out_link' do
-            raw('<i class="pe-7s-upload pe-rotate-90"></i>')
-          end
+      builds do |user|
+        case user
+        when Account then AccountNavbar
+        when Admin   then AdminNavbar
+        when nil then SignedOutNavbar
         end
       end
 
-      def mobile_links
-        content_tag :li do
-          link_to('Sign out', sign_out_path, method: :delete)
-        end
-      end
-    end
+      def show
+        root_path
 
-    class AccountNavbar < SignedInNavbar
-      private
-
-      def sidebar?
-        model.onboarded?
+        render view: 'navbar' # use the same ERB file even in subclasses
       end
 
-      def notifications
-        model.onboarded? ? cell(Notification::Cell::List, model) : super
-      end
-
-      def sign_out_path
-        destroy_account_session_path
-      end
-    end
-
-    class AdminNavbar < SignedInNavbar
-      private
-
-      def search_form
-        cell(AdminArea::Account::Cell::SearchForm)
-      end
-
-      def sidebar?
-        true
-      end
-
-      def sign_out_path
-        destroy_admin_session_path
-      end
-
-      def logo
-        cell(Logo, model)
-      end
-    end
-
-    class SignedOutNavbar < self
       private
 
       def bars
+        if sidebar?
+          '<div class="header-link hide-menu"><i class="fa fa-bars"></i></div>'
+        else
+          ''
+        end
+      end
+
+      def search_form
         ''
       end
 
-      def sidebar?
-        false
+      def pad_logo?
+        !sidebar?
       end
 
-      def links
-        raw_links.map do |text, href|
+      def logo
+        cell(Logo)
+      end
+
+      def username
+        ''
+      end
+
+      def notifications
+        ''
+      end
+
+      class SignedInNavbar < self
+        def username
+          content_tag :li, model.email, class: :text
+        end
+
+        def links
           content_tag :li do
-            link_to text, href, class: 'text'
+            link_to sign_out_path, method: :delete, id: 'sign_out_link' do
+              raw('<i class="pe-7s-upload pe-rotate-90"></i>')
+            end
           end
-        end.join
-      end
+        end
 
-      def mobile_links
-        raw_links.map do |text, href|
+        def mobile_links
           content_tag :li do
-            link_to text, href
+            link_to('Sign out', sign_out_path, method: :delete)
           end
-        end.join
+        end
       end
 
-      def raw_links
-        [
-          ['Sign in', new_account_session_path],
-          ['Sign up', new_account_registration_path],
-        ]
+      class AccountNavbar < SignedInNavbar
+        private
+
+        def sidebar?
+          model.onboarded?
+        end
+
+        def notifications
+          model.onboarded? ? cell(Notification::Cell::List, model) : super
+        end
+
+        def sign_out_path
+          destroy_account_session_path
+        end
+      end
+
+      class AdminNavbar < SignedInNavbar
+        private
+
+        def search_form
+          cell(AdminArea::Account::Cell::SearchForm)
+        end
+
+        def sidebar?
+          true
+        end
+
+        def sign_out_path
+          destroy_admin_session_path
+        end
+
+        def logo
+          cell(Logo, model)
+        end
+      end
+
+      class SignedOutNavbar < self
+        private
+
+        def bars
+          ''
+        end
+
+        def sidebar?
+          false
+        end
+
+        def links
+          raw_links.map do |text, href|
+            content_tag :li do
+              link_to text, href, class: 'text'
+            end
+          end.join
+        end
+
+        def mobile_links
+          raw_links.map do |text, href|
+            content_tag :li do
+              link_to text, href
+            end
+          end.join
+        end
+
+        def raw_links
+          [
+            ['Sign in', new_account_session_path],
+            ['Sign up', new_account_registration_path],
+          ]
+        end
       end
     end
   end
--- app/concepts/abroaders/cell/sidebar.rb
+++ app/concepts/abroaders/cell/sidebar.rb
@@ -1,55 +1,57 @@
-class Abroaders::Cell < Trailblazer::Cell
-  class Sidebar < Trailblazer::Cell
-    include ::Cell::Builder
+module Abroaders
+  module Cell
+    class Sidebar < Trailblazer::Cell
+      include ::Cell::Builder
 
-    builds do |model|
-      case model
-      when Account then self
-      when Admin   then AdminSidebar
-      end
-    end
-
-    def container(&block)
-      content_tag :aside, id: :menu do
-        content_tag :div, id: :navigation do
-          content_tag :ul, id: 'side-menu', class: :nav, &block
-        end
-      end
-    end
-
-    def link(text, href, controller_class = nil)
-      active = if controller_class
-                 controller.is_a?(controller_class)
-               else
-                 request.path == href
-               end
-      content_tag :li, class: ('active' if active) do
-        link_to href do
-          content_tag :span, text, class: 'nav-label'
-        end
-      end
-    end
-
-    def nested_links(title, links, controller_class = nil)
-      cell(NestedLinks, nil, title: title, links: links, controller_class: controller_class)
-    end
-
-    class AdminSidebar < self
-    end
-
-    class NestedLinks < self
-      private
-
-      def active
-        if options[:controller_class]
-          controller.is_a?(options[:controller_class])
-        else
-          options[:links].any? { |_, href| href == request.path }
+      builds do |model|
+        case model
+        when Account then self
+        when Admin   then AdminSidebar
         end
       end
 
-      def links
-        options[:links].map { |text, href| link(text, href) }.join
+      def container(&block)
+        content_tag :aside, id: :menu do
+          content_tag :div, id: :navigation do
+            content_tag :ul, id: 'side-menu', class: :nav, &block
+          end
+        end
+      end
+
+      def link(text, href, controller_class = nil)
+        active = if controller_class
+                   controller.is_a?(controller_class)
+                 else
+                   request.path == href
+                 end
+        content_tag :li, class: ('active' if active) do
+          link_to href do
+            content_tag :span, text, class: 'nav-label'
+          end
+        end
+      end
+
+      def nested_links(title, links, controller_class = nil)
+        cell(NestedLinks, nil, title: title, links: links, controller_class: controller_class)
+      end
+
+      class AdminSidebar < self
+      end
+
+      class NestedLinks < self
+        private
+
+        def active
+          if options[:controller_class]
+            controller.is_a?(options[:controller_class])
+          else
+            options[:links].any? { |_, href| href == request.path }
+          end
+        end
+
+        def links
+          options[:links].map { |text, href| link(text, href) }.join
+        end
       end
     end
   end
