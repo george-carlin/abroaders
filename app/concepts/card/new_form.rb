dfd1858f25359b407010ef42bb8998fea68c8fcf
George Millo <georgejulianmillo@gmail.com>
2017-01-30 20:44:03 -0600

    couples accounts can add cards post-onboarding

--- app/concepts/card/new_form.rb
+++ app/concepts/card/new_form.rb
@@ -2,5 +2,6 @@ class Card < ApplicationRecord
   # Like the 'edit' form, except they can also specify the product ID
   class NewForm < ::Card::Form
     property :product_id, type: ::Types::Form::Int
+    property :person_id,  type: ::Types::Form::Int
   end
 end
