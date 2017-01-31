16cea52ce8b7519da8f7e30702a537045b26b257
George Millo <georgejulianmillo@gmail.com>
2017-01-30 19:29:32 -0600

    updates + specs for CardProduct::Cell::FullName
    addition: network_in_brackets option
    
    minor breaking changes:
    
    1. only hide 'American Express' as the network if the bank name is
    American Express *and is being displayed*
    2. don't include anything about the network when the network is unknown

--- app/concepts/card_product/cell/full_name.rb
+++ app/concepts/card_product/cell/full_name.rb
@@ -2,8 +2,13 @@ class CardProduct < ApplicationRecord
   module Cell
     # Takes a card product and returns a pretty name for it
     #
-    # option: 'with_bank'. default false. if true, include the bank's name in
-    # the result.
+    # options:
+    #   with_bank: default false. if true, result includes bank's name. If
+    #     bank name is American Express, don't include it (because the network
+    #     will also be AmEx, so the words 'American Express' will already be
+    #     included in the name.
+    #   network_in_brackets: default false. Wrap the name of the network
+    #     in brackets, e.g. 'Chase Sapphire (Visa)'
     class FullName < Trailblazer::Cell
       property :bank
       property :bp
@@ -16,14 +21,23 @@ class CardProduct < ApplicationRecord
         parts.unshift(bank_name) if options[:with_bank]
         parts.push('business') if bp == 'business'
         # Amex will already be displayed as the bank name, so don't be redundant
-        parts.push(network) unless bank_name == 'American Express'
-        parts.join(" ")
+        parts.push(network) unless exclude_network?
+        parts.join(' ')
       end
 
       private
 
       def network
-        cell(Network, model)
+        name = cell(Network, model)
+        if options[:network_in_brackets]
+          "(#{name})"
+        else
+          name
+        end
+      end
+
+      def exclude_network?
+        model.network == 'unknown_network' || (bank_name == 'American Express' && options[:with_bank])
       end
     end
   end
