7f0e951bea7ee521f2454abeecfecbddd6062fad
George Millo <georgejulianmillo@gmail.com>
2017-02-01 16:11:56 -0600

    extract ValidationErrorsAlert::ActiveModel cell
    This should be removed eventually because we should use dry-v for all
    contracts.
    
    Note that this is currently only being used in one place; most error
    alerts are still using the model_validation_errors partial, but that
    should be removed and replaced with this cell in all instances. Also
    note that the cell isn't an exact replacement for the partial because it
    doesn't have the 'full' option and doesn't let you specify a custom
    model name.

--- app/concepts/abroaders/cell/validation_errors_alert.rb
+++ app/concepts/abroaders/cell/validation_errors_alert.rb
@@ -4,8 +4,11 @@ module Abroaders
     # that it hasn't been validated yet), the cell will return an empty string.
     # Else, it will return a Bootstrap 'danger' alert containing info about the
     # errors.
+    #
+    # NOTE: the contract must use dry-validation for validation, not
+    # ActiveModel. For ActiveModel errors use ValidationErrorsAlert::ActiveModel
     class ValidationErrorsAlert < Trailblazer::Cell
-      alias form model
+      alias contract model
 
       def show
         return '' if error_messages.empty?
@@ -14,8 +17,41 @@ module Abroaders
 
       private
 
+      # will return a Hash in the format { key: ['err1', 'err2'] }.
       def error_messages
-        form.errors.messages
+        contract.errors.messages
+      end
+
+      # reform will drop support for ActiveModel eventually, so we should start
+      # thinking about converting all AM forms to use dry-v.
+      class ActiveModel < Trailblazer::Cell
+        alias contract model
+
+        def show
+          return '' if errors.empty?
+          cell(ErrorAlert, nil, content: render)
+        end
+
+        private
+
+        def intro
+          result = 'There '
+          result << if errors.count > 1
+                      "were #{errors.count} errors"
+                    else
+                      'was an error'
+                    end
+          result << " and the #{model_name} could not be saved:"
+          result
+        end
+
+        def errors
+          contract.errors
+        end
+
+        def model_name
+          contract.model.model_name.human.downcase
+        end
       end
     end
   end
