0deb6a34076a99b6e10ca340d7afc797f686860d
George Millo <georgejulianmillo@gmail.com>
2017-02-02 22:57:03 -0600

    remove OfferPresenter and AdminArea::OfferPresenter

---
+++ app/concepts/partner/cell/short_name.rb
@@ -0,0 +1,24 @@
+module Partner
+  module Cell
+    # takes one of the following strings:
+    #
+    # 'card_ratings'
+    # 'credit_cards'
+    # 'award_wallet'
+    # 'card_benefit'
+    #
+    # ... and just returns an abbreviation. If we ever upgrade Partner
+    # to a full DB-backed model, this cell might become more complicated
+    class ShortName < Trailblazer::Cell
+      def show
+        case model
+        when 'card_ratings' then 'CR'
+        when 'credit_cards' then 'CC'
+        when 'award_wallet' then 'AW'
+        when 'card_benefit' then 'CB'
+        else '-'
+        end
+      end
+    end
+  end
+end
