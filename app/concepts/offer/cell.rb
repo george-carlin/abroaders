bb12aff236a420663dd9d03613f49247dff2d1b3
George Millo <georgejulianmillo@gmail.com>
2017-02-02 22:20:32 -0600

    deprecate all the things!

--- app/concepts/offer/cell.rb
+++ app/concepts/offer/cell.rb
@@ -8,13 +8,7 @@ class Offer::Cell < Trailblazer::Cell
     super
   end
 
-  property :cost
   property :days
-  property :points_awarded
-  property :spend
-
-  include ::Cell::Builder
-  include ::ActionView::Helpers::NumberHelper
 
   def cost
     warn "#{self.class}#cost is deprecated. Use Offer::Cell::Cost instead"
@@ -34,35 +28,14 @@ class Offer::Cell < Trailblazer::Cell
     warn "Offer::Cell#description is deprecated"
     line = caller.select { |l| l.include?(Rails.root.to_s) }[1].split(':')[0..1].join(':')
     warn "Called from #{line}"
-    case model.condition
-    when "on_minimum_spend"
-      "Spend #{spend} within #{days} days to receive a bonus of "\
-        "#{points_awarded} #{currency_name} points"
-    when "on_approval"
-      "#{points_awarded} #{currency_name} points awarded upon a successful application for this card."
-    when "on_first_purchase"
-      "#{points_awarded} #{currency_name} points awarded upon making your first purchase using this card."
-    else raise "this should never happen"
-    end
+    Offer::Cell::Description.(model).()
   end
 
   def points_awarded
-    number_with_delimiter super
+    Offer::Cell::PointsAwarded.(model).()
   end
 
   def spend
-    number_to_currency super
-  end
-
-  private
-
-  def abbreviated_points
-    # Show points and spend as multiples of 1000, but don't print the decimal
-    # point if it's an exact multiple:
-    (offer.points_awarded / 1000.0).to_s.sub(/\.0+\z/, '')
-  end
-
-  def abbreviated_spend
-    (offer.spend / 1000.0).to_s.sub(/\.0+\z/, '')
+    Offer::Cell::Spend.(model).()
   end
 end
