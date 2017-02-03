95dd0a6b6af1824988f29282cb7328a44556de46
George Millo <georgejulianmillo@gmail.com>
2017-02-03 17:53:48 -0600

    balance/cell/list - format file correctly
    Cell should be a module, not a class, and nested modules/classes belong
    on new lines

--- app/concepts/balance/cell/list.rb
+++ app/concepts/balance/cell/list.rb
@@ -1,30 +1,32 @@
-class Balance::Cell < Trailblazer::Cell
-  class List < Trailblazer::Cell
-    include ActionView::Helpers::RecordTagHelper
+class Balance < Balance.superclass
+  module Cell
+    class List < Trailblazer::Cell
+      include ActionView::Helpers::RecordTagHelper
 
-    def show
-      content_tag :ul, class: 'person-balances' do
-        cell(ListItem, collection: model)
+      def show
+        content_tag :ul, class: 'person-balances' do
+          cell(ListItem, collection: model)
+        end
       end
     end
-  end
 
-  class ListItem < Trailblazer::Cell
-    include ActionView::Helpers::NumberHelper
+    class ListItem < Trailblazer::Cell
+      include ActionView::Helpers::NumberHelper
 
-    property :currency
-    property :value
+      property :currency
+      property :value
 
-    private
+      private
 
-    def html_id
-      dom_id(currency)
-    end
+      def html_id
+        dom_id(currency)
+      end
 
-    delegate :name, to: :currency, prefix: true
+      delegate :name, to: :currency, prefix: true
 
-    def value
-      number_with_delimiter(super)
+      def value
+        number_with_delimiter(super)
+      end
     end
   end
 end
