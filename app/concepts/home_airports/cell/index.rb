97152d2dc143a9a0200c940050f724b480d1dc95
George Millo <georgejulianmillo@gmail.com>
2017-02-03 20:15:20 -0600

    update home airports index page UI

--- app/concepts/home_airports/cell/index.rb
+++ app/concepts/home_airports/cell/index.rb
@@ -3,6 +3,16 @@ module HomeAirports
     # model = a collection of Airports
     class Index < Trailblazer::Cell
       alias airports model
+
+      private
+
+      def link_to_edit_home_airports
+        link_to(
+          'Edit airports',
+          edit_home_airports_path,
+          class: 'btn btn-primary',
+        )
+      end
     end
   end
 end
