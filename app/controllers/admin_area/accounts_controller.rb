02d2803b654c5b65b47a1cb00d41a941273d5124
George Millo <georgejulianmillo@gmail.com>
2017-03-21 18:02:08 -0300

    make admin/accounts#index and #show use operations

--- app/controllers/admin_area/accounts_controller.rb
+++ app/controllers/admin_area/accounts_controller.rb
@@ -2,25 +2,13 @@ module AdminArea
   class AccountsController < AdminController
     # GET /admin/accounts
     def index
-      person_assocs = [:spending_info]
-      @accounts = Account.includes(
-        :phone_number,
-        people: person_assocs,
-        owner: person_assocs,
-        companion: person_assocs,
-      ).order("email ASC")
-      render cell(Accounts::Cell::Index, @accounts, page: params[:page])
+      run Accounts::Operation::Index
+      render cell(Accounts::Cell::Index, result['accounts'], page: params[:page])
     end
 
     # GET /admin/accounts/1
     def show
-      @account = Account.find(params[:id])
-      @cards   = @account.cards.select(&:persisted?)
-      @recommendation = @account.cards.new
-      # Use @account.cards here instead of @cards because
-      # the latter is an Array, not a Relation (because of
-      # `.select(&:persisted?)`)
-      @products = CardProduct.where.not(id: @account.cards.select(:product_id))
+      run Accounts::Operation::Show
     end
 
     def search
