d3ec2c275da53a445b8a19c9fd17045bf86fc7eb
George Millo <georgejulianmillo@gmail.com>
2017-03-27 16:17:36 -0300

    AW: link to business.AW.com account list on admin/people#show

--- app/concepts/integrations/award_wallet/links.rb
+++ app/concepts/integrations/award_wallet/links.rb
@@ -4,6 +4,10 @@ module Integrations
       def edit_account_on_award_wallet_path(aw_account)
         "https://awardwallet.com/account/edit/#{aw_account.award_wallet_id}"
       end
+
+      def admin_award_wallet_account_list_url(aw_user)
+        "https://business.awardwallet.com/account/list#/?agentId=#{aw_user.agent_id}"
+      end
     end
   end
 end
