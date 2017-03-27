20652aba19ae27edfffb72b439c136742350055c
George Millo <georgejulianmillo@gmail.com>
2017-03-27 13:03:40 -0300

    add LoyaltyAccount

---
+++ app/models/loyalty_account.rb
@@ -0,0 +1,51 @@
+require 'dry-struct'
+require 'active_support/core_ext/hash/keys'
+
+require 'types'
+
+# Wrapper class that presents a common interface for both Balances and
+# AwardWalletAccounts in places where they are used more-or-less
+# interchangeably, e.g. when listing a person's loyalty accounts on
+# Balances#index
+#
+# Use LoyaltyAccount.build to create a LoyaltyAccount from a Balances or an
+# AwardWalletAccount, using the correct subclass of LoyaltyAccount
+class LoyaltyAccount < Dry::Struct
+  attribute :id, Types::Strict::Int
+  attribute :person_id, Types::Strict::Int
+  attribute :currency_name, Types::Strict::String
+  attribute :balance_raw, Types::Strict::Int
+  attribute :updated_at, Types::Strict::DateTime
+
+  def self.build(balance)
+    return AwardWallet.build(balance) if balance.is_a?(AwardWalletAccount)
+
+    attrs = balance.attributes.symbolize_keys.slice(:id, :person_id)
+    attrs[:balance_raw]   = balance.value
+    attrs[:currency_name] = balance.currency.name
+    attrs[:updated_at] = balance.updated_at.to_datetime
+    new(attrs)
+  end
+
+  def source
+    'abroaders'
+  end
+
+  class AwardWallet < self
+    attribute :award_wallet_id, Types::Strict::Int
+    attribute :login, Types::Strict::String
+
+    def self.build(awa)
+      attrs = awa.attributes.symbolize_keys.slice(:id, :balance_raw, :updated_at, :login)
+      attrs[:award_wallet_id] = awa.aw_id
+      attrs[:currency_name]   = awa.display_name
+      attrs[:person_id]       = awa.award_wallet_owner.person.id
+      attrs[:updated_at]      = awa.updated_at.to_datetime
+      new(attrs)
+    end
+
+    def source
+      'award_wallet'
+    end
+  end
+end
---
+++ spec/models/loyalty_account_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe LoyaltyAccount do
+  describe '.build' do
+    let(:updated_at) { Time.new(2016, 12, 31, 12, 55, 32) }
+
+    context 'for a Balance' do
+      let(:balance) do
+        Balance.new(
+          id: 1,
+          person_id: 2,
+          updated_at: updated_at,
+          value: 12345,
+          currency: Currency.new(name: 'American Airlines'),
+        )
+      end
+
+      it '' do
+        acc = described_class.build(balance)
+        expect(acc.id).to eq 1
+        expect(acc.person_id).to eq 2
+        expect(acc.updated_at).to eq updated_at
+        expect(acc.balance_raw).to eq 12345
+        expect(acc.currency_name).to eq 'American Airlines'
+        expect(acc.source).to eq 'abroaders'
+      end
+    end
+
+    context 'for an AwardWalletAccount' do
+      let(:awa) do
+        ::AwardWalletAccount.new(
+          id: 1,
+          aw_id: 555,
+          balance_raw: 12345,
+          display_name: 'American Airlines',
+          login: 'GeorgeMillo',
+          award_wallet_owner: AwardWalletOwner.new(person: Person.new(id: 2)),
+          updated_at: updated_at,
+        )
+      end
+
+      it '' do
+        acc = described_class.build(awa)
+        expect(acc.id).to eq 1
+        expect(acc.person_id).to eq 2
+        expect(acc.updated_at).to eq updated_at
+        expect(acc.balance_raw).to eq 12345
+        expect(acc.currency_name).to eq 'American Airlines'
+        expect(acc.source).to eq 'award_wallet'
+        expect(acc.login).to eq 'GeorgeMillo'
+      end
+    end
+  end
+end
