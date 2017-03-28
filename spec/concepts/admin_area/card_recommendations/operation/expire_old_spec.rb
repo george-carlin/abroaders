801dca9b7b10aa8e2dfd2011284f38bfa6afe39f
George Millo <georgejulianmillo@gmail.com>
2017-03-28 16:18:01 -0300

    refactor card rec ExpireOld spec

--- spec/concepts/admin_area/card_recommendations/operation/expire_old_spec.rb
+++ spec/concepts/admin_area/card_recommendations/operation/expire_old_spec.rb
@@ -26,22 +26,17 @@ module AdminArea
         result['model'].tap { |rec| rec.update!(attrs) }
       end
 
-      to_not_expire = [
-        # recommended recently:
-        create_rec(recommended_at: keep),
-        # recommended recently and seen:
-        create_rec(recommended_at: keep, seen_at: now),
-        # recommended recently, seen, and clicked
-        create_rec(recommended_at: keep, seen_at: now, clicked_at: now),
-        # recommended before cutoff point, but clicked
-        create_rec(recommended_at: lose, seen_at: now, clicked_at: now),
-        # recommended before cutoff point, but declined
-        create_rec(recommended_at: lose, seen_at: now, declined_at: now, decline_reason: 'X'),
-        # recommended before cutoff point, but applied
-        create_rec(recommended_at: lose, seen_at: now, applied_at: now),
-        # recommended before cutoff point, but pulled:
-        create_rec(recommended_at: lose, pulled_at: now),
-      ]
+      # in these variable names, 'new' means they were recommended more
+      # recently that the cutoff point and 'old' means that they're older
+
+      # recs which shouldn't have their expiry date set:
+      new      = create_rec(recommended_at: keep)
+      seen     = create_rec(recommended_at: keep, seen_at: now)
+      clicked  = create_rec(recommended_at: keep, seen_at: now, clicked_at: now)
+      old_and_clicked  = create_rec(recommended_at: lose, seen_at: now, clicked_at: now)
+      old_and_declined = create_rec(recommended_at: lose, seen_at: now, declined_at: now, decline_reason: 'X')
+      old_and_applied = create_rec(recommended_at: lose, seen_at: now, applied_at: now)
+      old_and_pulled = create_rec(recommended_at: lose, pulled_at: now)
 
       # accounts that were already expired shouldn't have their expired_at date
       # changed:
@@ -54,25 +49,28 @@ module AdminArea
       # rounded:
       already_expired.reload
 
-      to_expire = [
-        # recommended > 15 days ago:
-        create_rec(recommended_at: lose),
-        # cards that have only been *seen* should still expire:
-        create_rec(recommended_at: lose, seen_at: now),
-      ]
+      # to expire:
+      # recommended > 15 days ago:
+      old = create_rec(recommended_at: lose)
+      # cards that have only been *seen* should still expire:
+      old_and_seen = create_rec(recommended_at: lose, seen_at: now)
 
       expect do
         op.()
         already_expired.reload
       end.not_to change { already_expired.expired_at }
 
-      to_not_expire.each(&:reload)
+      expect(new.reload.expired_at).to be nil
+      expect(seen.reload.expired_at).to be nil
+      expect(clicked.reload.expired_at).to be nil
+      expect(old_and_clicked.reload.expired_at).to be nil
+      expect(old_and_declined.reload.expired_at).to be nil
+      expect(old_and_applied.reload.expired_at).to be nil
+      expect(old_and_pulled.reload.expired_at).to be nil
 
-      expect(to_not_expire.map(&:expired_at).compact).to eq []
-
-      to_expire.each do |rec|
-        expect(rec.reload.expired_at).to be_within(5.seconds).of(Time.zone.now)
-      end
+      expect(old.reload.expired_at).to be_within(5.seconds).of(Time.zone.now)
+      # cards that have only been *seen* should still expire:
+      expect(old_and_seen.reload.expired_at).to be_within(5.seconds).of(Time.zone.now)
     end
   end
 end
