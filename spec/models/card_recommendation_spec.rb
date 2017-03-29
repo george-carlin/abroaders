26f838c81d37b8d4db8772c65f996ca00cee7a79
George Millo <georgejulianmillo@gmail.com>
2017-03-29 14:17:07 -0300

    fix Rubocop errors

--- spec/models/card_recommendation_spec.rb
+++ spec/models/card_recommendation_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe CardRecommendation do
   example '#pull!' do
     card = create_card
     card.update!(recommended_at: Time.now)
-    rec  = described_class.new(card)
+    rec = described_class.new(card)
     rec.pull!
     expect(card.reload.pulled_at).to be_within(5.seconds).of(Time.now)
   end
