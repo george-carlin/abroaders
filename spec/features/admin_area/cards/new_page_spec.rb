1f7b6ebff788eac9ba1e9de81d58d2ffaa328d40
George Millo <georgejulianmillo@gmail.com>
2017-03-31 22:56:13 -0300

    remove nested module on admin new card page spec

--- spec/features/admin_area/cards/new_page_spec.rb
+++ spec/features/admin_area/cards/new_page_spec.rb
@@ -1,55 +1,53 @@
 require 'rails_helper'
 
-module AdminArea
-  RSpec.describe 'new card page' do
-    include_context 'logged in as admin'
+RSpec.describe 'new card page' do
+  include_context 'logged in as admin'
 
-    let(:account) { create(:account, :onboarded) }
-    let(:person)  { account.owner }
-    let!(:product) { create(:card_product) }
+  let(:account) { create(:account, :onboarded) }
+  let(:person)  { account.owner }
+  let!(:product) { create(:card_product) }
 
-    before { visit new_admin_person_card_path(person) }
+  before { visit new_admin_person_card_path(person) }
 
-    example 'creating a card' do
-      expect(page).to have_field :card_product_id
-      expect(page).to have_field :card_opened_on_1i
-      expect(page).to have_field :card_opened_on_2i
-      expect(page).to have_field :card_closed
+  example 'creating a card' do
+    expect(page).to have_field :card_product_id
+    expect(page).to have_field :card_opened_on_1i
+    expect(page).to have_field :card_opened_on_2i
+    expect(page).to have_field :card_closed
 
-      expect do
-        click_button 'Save'
-      end.to change { person.cards.count }.by(1)
+    expect do
+      click_button 'Save'
+    end.to change { person.cards.count }.by(1)
 
-      expect(current_path).to eq admin_person_path(person)
-    end
+    expect(current_path).to eq admin_person_path(person)
+  end
 
-    example 'creating a closed card' do
-      check :card_closed
+  example 'creating a closed card' do
+    check :card_closed
 
-      expect do
-        click_button 'Save'
-      end.to change { person.cards.count }.by(1)
+    expect do
+      click_button 'Save'
+    end.to change { person.cards.count }.by(1)
 
-      expect(person.cards.last).to be_closed
+    expect(person.cards.last).to be_closed
 
-      expect(current_path).to eq admin_person_path(person)
-    end
+    expect(current_path).to eq admin_person_path(person)
+  end
 
-    example 'invalid card creation', :js do
-      check :card_closed
-      # closed before opened:
-      select (Date.today.year - 1).to_s, from: :card_closed_on_1i
-      expect do
-        click_button 'Save'
-      end.not_to change { person.cards.count }
+  example 'invalid card creation', :js do
+    check :card_closed
+    # closed before opened:
+    select (Date.today.year - 1).to_s, from: :card_closed_on_1i
+    expect do
+      click_button 'Save'
+    end.not_to change { person.cards.count }
 
-      # still shows form, including with the 'closed' inputs visible:
-      expect(page).to have_field :card_product_id
-      expect(page).to have_field :card_opened_on_1i
-      expect(page).to have_field :card_opened_on_2i
-      expect(page).to have_field :card_closed, checked: true
-      expect(page).to have_field :card_closed_on_1i
-      expect(page).to have_field :card_closed_on_2i
-    end
+    # still shows form, including with the 'closed' inputs visible:
+    expect(page).to have_field :card_product_id
+    expect(page).to have_field :card_opened_on_1i
+    expect(page).to have_field :card_opened_on_2i
+    expect(page).to have_field :card_closed, checked: true
+    expect(page).to have_field :card_closed_on_1i
+    expect(page).to have_field :card_closed_on_2i
   end
 end
