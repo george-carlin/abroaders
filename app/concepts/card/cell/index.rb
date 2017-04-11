3d05854ef3afbf224d443e2a5d0078b3d89ab9e3
George Millo <georgejulianmillo@gmail.com>
2017-04-11 11:58:14 -0300

    split card index card rec cell into separate file

--- app/concepts/card/cell/index.rb
+++ app/concepts/card/cell/index.rb
@@ -24,51 +24,6 @@ class Card < Card.superclass
         return '' if recommendation_note.nil?
         cell(CardRecommendation::Cell::Note, recommendation_note)
       end
-
-      # @!method self.call(account, options = {})
-      class CardRecommendations < Abroaders::Cell::Base
-        property :people
-
-        def show
-          content_tag :div, id: 'card_recommendations' do
-            cell(ForPerson, collection: people)
-          end
-        end
-
-        # @!method self.call(person, options = {})
-        class ForPerson < Abroaders::Cell::Base
-          include Escaped
-
-          property :account
-          property :first_name
-          property :type
-          property :unresolved_card_recommendations
-
-          def show
-            content_tag :div, id: "#{type}_card_recommendations" do
-              header + recommendations
-            end
-          end
-
-          private
-
-          def header
-            return '' unless account.couples?
-            "<h3>#{first_name}'s Recommendations</h3>"
-          end
-
-          def recommendations
-            if unresolved_card_recommendations.any?
-              cell(
-                CardRecommendation::Cell::UnresolvedRec,
-                collection: unresolved_card_recommendations,
-              ).join('<hr>') { |c| c }
-            else
-              "No recommendations for #{first_name}"
-            end
-          end
-        end
-      end
     end
   end
 end
---
+++ app/concepts/card/cell/index/card_recommendations.rb
@@ -0,0 +1,50 @@
+class Card < Card.superclass
+  module Cell
+    class Index < Index.superclass
+      # @!method self.call(account, options = {})
+      class CardRecommendations < Abroaders::Cell::Base
+        property :people
+
+        def show
+          content_tag :div, id: 'card_recommendations' do
+            cell(ForPerson, collection: people)
+          end
+        end
+
+        # @!method self.call(person, options = {})
+        class ForPerson < Abroaders::Cell::Base
+          include Escaped
+
+          property :account
+          property :first_name
+          property :type
+          property :unresolved_card_recommendations
+
+          def show
+            content_tag :div, id: "#{type}_card_recommendations" do
+              header + recommendations
+            end
+          end
+
+          private
+
+          def header
+            return '' unless account.couples?
+            "<h3>#{first_name}'s Recommendations</h3>"
+          end
+
+          def recommendations
+            if unresolved_card_recommendations.any?
+              cell(
+                CardRecommendation::Cell::UnresolvedRec,
+                collection: unresolved_card_recommendations,
+              ).join('<hr>') { |c| c }
+            else
+              "No recommendations for #{first_name}"
+            end
+          end
+        end
+      end
+    end
+  end
+end
