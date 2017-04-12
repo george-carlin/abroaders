846eaeca73c0f9c9f9a5910c18dcb19e927a5974
George Millo <georgejulianmillo@gmail.com>
2017-04-12 11:04:32 -0300

    add specs and more docs for Transaction module

--- app/lib/abroaders/operation/transaction.rb
+++ app/lib/abroaders/operation/transaction.rb
@@ -1,6 +1,6 @@
 module Abroaders
   module Operation
-    # sample usage:
+    # Sample usage:
     #
     #   class MyOp < Trailblazer::Operation
     #     extend Abroaders::Operation::Transaction
@@ -17,6 +17,44 @@ module Abroaders
     #     end)
     #   end
     #
+    # Remember that the 'Wrap' step returns the value of the proc that's passed
+    # to it, rather than the block. So if the inner steps fail,
+    # ApplicationRecord.transaction will still return a truthy value and so the
+    # 'Wrap' step will pass. This appears to be a deliberate design decision on
+    # the part of Trailblazer:
+    # http://trailblazer.to/gems/operation/2.0/api.html#wrap
+    #
+    # So the following operation will always succeed:
+    #
+    #   class MyOp < Trailblazer::Operation
+    #     step wrap_in_transaction {
+    #       step :fail
+    #     }
+    #
+    #     def fail
+    #       false
+    #     end
+    #   end
+    #
+    # The workaround is to add a nested `failure` step that raises
+    # ActiveRecord::Rollback:
+    #
+    #   class MyOp < Trailblazer::Operation
+    #     step wrap_in_transaction {
+    #       step :fail
+    #       failure :rollback
+    #     }
+    #
+    #     def fail
+    #       false
+    #     end
+    #
+    #     def rollback
+    #       raise ActiveRecord::Rollback
+    #     end
+    #   end
+    #
+    # Not sure if this is the 'correct' way to do this, but it works.
     module Transaction
       def wrap_in_transaction(&steps)
         Wrap(->(*, &block) { ApplicationRecord.transaction(&block) }, &steps)
---
+++ spec/lib/abroaders/transaction_spec.rb
@@ -0,0 +1,73 @@
+require 'rails_helper'
+
+RSpec.describe Abroaders::Operation::Transaction do
+  class MyOp < Trailblazer::Operation
+    extend Abroaders::Operation::Transaction
+
+    step wrap_in_transaction {
+      step :create_account_0
+      step :raise_error
+      step :create_account_1
+      step :dont_pass
+      failure :rollback
+    }
+    failure :log_transaction_failed
+
+    private
+
+    def create_account_0(params:, **)
+      Account.create!(
+        email: "account#{params[:i] * 2}@example.com",
+        password: 'abroaders123',
+        password_confirmation: 'abroaders123',
+      )
+    end
+
+    def raise_error(params:, **)
+      raise ActiveRecord::Rollback if params[:will_raise]
+      true
+    end
+
+    def create_account_1(params:, **)
+      Account.create!(
+        email: "account#{(params[:i] * 2) + 1}@example.com",
+        password: 'abroaders123',
+        password_confirmation: 'abroaders123',
+      )
+    end
+
+    def dont_pass(params:, **)
+      !params[:will_fail]
+    end
+
+    def rollback(*)
+      raise ActiveRecord::Rollback
+    end
+
+    def log_transaction_failed(opts)
+      opts['error'] = 'transaction failed'
+    end
+  end
+
+  let(:op) { MyOp }
+
+  it 'wraps steps in a transaction' do
+    expect do
+      result = op.(i: 0)
+      expect(result.success?).to be true
+      expect(result['error']).to be nil
+    end.to change { Account.count }.by(2)
+
+    expect do
+      result = op.(i: 1, will_raise: true)
+      expect(result.success?).to be false
+      expect(result['error']).to eq 'transaction failed'
+    end.not_to change { Account.count }
+  end
+
+  it 'fails if you rollback' do
+    result = op.(i: 0, will_fail: true)
+    expect(result.success?).to be false
+    expect(result['error']).to eq 'transaction failed'
+  end
+end
