george-carlin/abroaders

commit cf03bbd1c7be08f3e222a638c1111761dbcd3123

remove a bunch of TODO/FIXME comments

app/concepts/phone_number/operation/onboard.rb
@@ -5,13 +5,14 @@ class PhoneNumber < PhoneNumber.superclass
     # Add a phone number to the account as part of the onboarding
     # survey
     class Onboard < Trailblazer::Operation
+      extend Abroaders::Operation::Transaction
+
       step :validate_account_onboarding_state!
       failure :log_invalid_onboarding_state!
-      # Wrapping the steps in a transaction seems to always return true? FIXME
-      # step Wrap ->(*, &block) { ApplicationRecord.transaction { block.call } } {
-      step Nested(PhoneNumber::Operation::Create)
-      step :update_onboarding_state!
-      # }
+      step wrap_in_transaction {
+        step Nested(PhoneNumber::Operation::Create)
+        step :update_onboarding_state!
+      }
 
       private
 

app/assets/stylesheets/custom.css.scss
@@ -229,7 +229,6 @@ footer.footer {
 
 // SPENDING INFO
 
-// TODO move me to under the dir of the relevant cell
 .has-ein {
   position: relative;
   bottom: 1px;

app/concepts/admin_area/card_accounts/new.rb
@@ -8,7 +8,7 @@ module AdminArea
       step :setup_model!
       step Contract::Build()
 
-      # This is display logic, it belongs in a cell, not an operation. FIXME
+      # This is display logic, it belongs in a cell, not an operation. REFACTORME
       def self.product_options
         CardProduct.all.map do |product|
           [CardProducts::Cell::Identifier.(product).(), product.id]

app/concepts/admin_area/people/cell/spending_info.rb
@@ -1,7 +1,7 @@
 module AdminArea
   module People
     module Cell
-      # TODO this cell sucks
+      # this cell sucks
       #
       # @!method self.call(model, opts = {})
       #   @param [Person] model

app/concepts/integrations/award_wallet/api_client.rb
@@ -9,7 +9,7 @@ module Integrations
     #
     #   https://business.awardwallet.com/api/account
     #
-    # Makes queries to the API, gets JSON responses, logs that JSON (TODO - not
+    # Makes queries to the API, gets JSON responses, logs that JSON (not
     # yet implemented), and returns the response parsed into a Hash. (The JSON
     # from the API uses camelCased key names, but this class will convert them
     # into underscored keys.

app/controllers/balances_controller.rb
@@ -10,7 +10,6 @@ class BalancesController < AuthenticatedUserController
   # GET /people/:person_id/balances/new
   def new
     run Balance::Operation::New
-    # TODO TRB convert view to cell
   end
 
   # POST /people/:person_id/balances
@@ -19,7 +18,6 @@ class BalancesController < AuthenticatedUserController
       flash[:success] = 'Created balance!'
       return redirect_to balances_path
     end
-    # TODO TRB convert view to cell
     render 'new'
   end
 

app/controllers/card_accounts_controller.rb
@@ -5,8 +5,6 @@ class CardAccountsController < AuthenticatedUserController
       render cell(CardAccount::Cell::New, result)
     else
       run CardAccount::New::SelectProduct
-      # TODO use new style, pass result to the cell directly
-      collection = result['collection']
       render cell(CardAccount::Cell::New::SelectProduct, collection, banks: result['banks'])
     end
   end

app/controllers/concerns/onboarding.rb
@@ -32,7 +32,7 @@ module Onboarding
   end
 
   module ClassMethods
-    # TODO - if you call this more than once in the same controller, things
+    # if you call this more than once in the same controller, things
     # break. So right now there's no way to specify that some actions in the
     # same controller are revisitable but others aren't. (See the commit that
     # added this comment)

commit edd9e98c8301b787435f55f87a5abe2ef12f52b5

make ops with transactions handle failure properly
See the docs for Abroaders::Transaction

This also fixed the phone number Onboard operation, which had been
broken by cf03bbd1c7be08f3e222a638c1111761dbcd3123.

app/concepts/integrations/award_wallet/user/refresh.rb
@@ -25,6 +25,7 @@ module Integrations
           step :update_user
           step :update_accounts
           step :delete_old_accounts_and_owners
+          failure :rollback
         }
 
         private
@@ -56,6 +57,10 @@ module Integrations
 
           model.award_wallet_owners.where.not(name: owner_names).destroy_all
           model.award_wallet_accounts.where.not(aw_id: account_ids).destroy_all
+        end
+
+        def rollback(*)
+          raise ActiveRecord::Rollback
         end
 
         class Job < ApplicationJob

app/concepts/phone_number/operation/onboard.rb
@@ -10,6 +10,7 @@ class PhoneNumber < PhoneNumber.superclass
       step Wrap(Abroaders::Transaction) {
         step Nested(PhoneNumber::Operation::Create)
         step :update_onboarding_state!
+        failure :rollback
       }
 
       private
@@ -24,6 +25,10 @@ class PhoneNumber < PhoneNumber.superclass
 
       def log_invalid_onboarding_state!(_options)
         result['errors'] = 'account in invalid onboarding state'
+      end
+
+      def rollback(*)
+        raise ActiveRecord::Rollback
       end
     end
   end