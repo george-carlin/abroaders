76ef092c5272b01c37306872c715c296ec0f2719
George Millo <georgejulianmillo@gmail.com>
2017-04-15 13:44:43 -0300

    use 'card_account' instead of 'card' for admin ops

--- app/concepts/admin_area/card_accounts/update.rb
+++ app/concepts/admin_area/card_accounts/update.rb
@@ -2,7 +2,7 @@ module AdminArea
   module CardAccounts
     class Update < Trailblazer::Operation
       step Nested(Edit)
-      step Contract::Validate(key: :card)
+      step Contract::Validate(key: :card_account)
       step Contract::Persist()
     end
   end
--- spec/concepts/admin_area/card_accounts/create_spec.rb
+++ spec/concepts/admin_area/card_accounts/create_spec.rb
@@ -5,23 +5,23 @@ RSpec.describe AdminArea::CardAccounts::Create do
   let(:product) { create(:card_product) }
   let(:op)      { described_class }
 
-  example 'creating a card' do
+  example 'success - creating an open card account' do
     result = op.(
       person_id: person.id,
-      card: { product_id: product.id, opened_on: Date.today },
+      card_account: { product_id: product.id, opened_on: Date.today },
     )
     expect(result.success?).to be true
 
-    card = result['model']
-    expect(card.product).to eq product
-    expect(card.closed_on).to be nil
-    expect(card.opened_on).to eq Date.today
+    card_account = result['model']
+    expect(card_account.product).to eq product
+    expect(card_account.closed_on).to be nil
+    expect(card_account.opened_on).to eq Date.today
   end
 
-  example 'creating a closed card' do
+  example 'success - creating a closed card account' do
     result = op.(
       person_id: person.id,
-      card: {
+      card_account: {
         closed: true,
         closed_on: Date.today,
         opened_on: Date.today - 2,
@@ -30,24 +30,24 @@ RSpec.describe AdminArea::CardAccounts::Create do
     )
     expect(result.success?).to be true
 
-    card = result['model']
-    expect(card.product).to eq product
-    expect(card.opened_on).to eq(Date.today - 2)
-    expect(card.closed_on).to eq Date.today
+    card_account = result['model']
+    expect(card_account.product).to eq product
+    expect(card_account.opened_on).to eq(Date.today - 2)
+    expect(card_account.closed_on).to eq Date.today
   end
 
-  example 'invalid save - opened in future' do
+  example 'failure - invalid save - opened in future' do
     result = op.(
       person_id: person.id,
-      card: { product_id: product.id, opened_on: (Date.today + 1) },
+      card_account: { product_id: product.id, opened_on: (Date.today + 1) },
     )
     expect(result.success?).to be false
   end
 
-  example 'invalid save - closed before opened' do
+  example 'failure - invalid save - closed before opened' do
     result = op.(
       person_id: person.id,
-      card: {
+      card_account: {
         closed: true,
         closed_on: (Date.today - 2),
         opened_on: Date.today,
