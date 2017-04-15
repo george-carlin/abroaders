809f847c8321c76e74afda20dfaebadd2173df2c
George Millo <georgejulianmillo@gmail.com>
2017-04-15 14:25:21 -0300

    remove unecessary params from CA::Create spec
    product_id doesn't do anything when you pass it in as a param nested
    under the 'card_account' key. (I think it did do something originally
    but I've removed it.) So stop passing in the pointless param, and get
    rid of the fancy `params.merge` stuff because it just makes the specs
    harder to follow.

--- spec/concepts/card_account/create_spec.rb
+++ spec/concepts/card_account/create_spec.rb
@@ -6,11 +6,12 @@ RSpec.describe CardAccount::Create do
   let(:product) { create(:card_product) }
   let(:op) { described_class }
 
-  let(:params) { { product_id: product.id } }
-
   example 'creating a card account - no person specified' do
     result = op.(
-      params.merge(card_account: { product_id: product.id, opened_on: Date.today }),
+      {
+        product_id: product.id,
+        card_account: { opened_on: Date.today },
+      },
       'account' => account,
     )
     expect(result.success?).to be true
@@ -25,14 +26,11 @@ RSpec.describe CardAccount::Create do
   example 'creating a card account - specifying a person-id' do
     companion = create(:companion, account: account)
     result = op.(
-      params.merge(
+      {
         person_id: companion.id,
-        card_account: {
-          opened_on: Date.today,
-          closed: false,
-          product_id: product.id,
-        },
-      ),
+        product_id: product.id,
+        card_account: { opened_on: Date.today, closed: false },
+      },
       'account' => account,
     )
     expect(result.success?).to be true
@@ -42,13 +40,14 @@ RSpec.describe CardAccount::Create do
 
   example 'creating a closed card account' do
     result = op.(
-      params.merge(
+      {
+        product_id: product.id,
         card_account: {
           closed: true,
           closed_on: Date.today,
           opened_on: Date.yesterday,
         },
-      ),
+      },
       'account' => account,
     )
     expect(result.success?).to be true
@@ -63,14 +62,20 @@ RSpec.describe CardAccount::Create do
   it 'posts to a Zapier webhook' do
     expect(ZapierWebhooks::CardAccount::Created).to receive(:enqueue).with(kind_of(Card))
     op.(
-      params.merge(card_account: { product_id: product.id, opened_on: Date.today }),
+      {
+        product_id: product.id,
+        card_account: { opened_on: Date.today },
+      },
       'account' => account,
     )
   end
 
   example 'invalid save - opened in future' do
     result = op.(
-      params.merge(card_account: { opened_on: Date.tomorrow, product_id: product.id }),
+      {
+        product_id: product.id,
+        card_account: { opened_on: Date.tomorrow },
+      },
       'account' => account,
     )
     expect(result.success?).to be false
@@ -78,13 +83,14 @@ RSpec.describe CardAccount::Create do
 
   example 'invalid save - closed before opened' do
     result = op.(
-      params.merge(
+      {
+        product_id: product.id,
         card_account: {
           closed: true,
           closed_on: Date.today - 10,
           opened_on: Date.today,
         },
-      ),
+      },
       'account' => account,
     )
     expect(result.success?).to be false
@@ -94,14 +100,11 @@ RSpec.describe CardAccount::Create do
     other_account = create(:account)
     expect do
       op.(
-        params.merge(
+        {
           person_id: other_account.owner.id,
-          card_account: {
-            opened_on: Date.today,
-            closed: false,
-            product_id: product.id,
-          },
-        ),
+          product_id: product.id,
+          card_account: { opened_on: Date.today, closed: false },
+        },
         'account' => account,
       )
     end.to raise_error(ActiveRecord::RecordNotFound)
