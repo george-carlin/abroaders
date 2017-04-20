6132bef1b0bdfc7d3a5111f0e3159bde7f3b81f7
George Millo <georgejulianmillo@gmail.com>
2017-04-20 18:06:27 -0500

    remove validations from Currency
    validations belong in form objects, not in models - and since there's no
    way to create/edit a currency through a form anyway, there's no need to
    replace these with a form object

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -32,10 +32,6 @@ class Currency < ApplicationRecord
 
   # Validations
 
-  validates :name, presence: true, uniqueness: true
-  validates :award_wallet_id, presence: true, uniqueness: true
-  validates :type, inclusion: { in: TYPES }
-
   # Associations
 
   def alliance
