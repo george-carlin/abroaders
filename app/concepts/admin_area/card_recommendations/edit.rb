fe1edd71fe3bda026dd09b43524e6d6632ef6229
George Millo <georgejulianmillo@gmail.com>
2017-04-20 17:46:57 -0500

    add docs for AA::CardRecommenations::Edit

--- app/concepts/admin_area/card_recommendations/edit.rb
+++ app/concepts/admin_area/card_recommendations/edit.rb
@@ -1,6 +1,13 @@
 module AdminArea
   module CardRecommendations
-    # Find a CardRecommenation by its ID, and prepare to edit it
+    # Find a CardRecommenation by its ID, and prepare to edit it.
+    #
+    # This page is very crude and basic, but we don't need a super-complicated
+    # thing for now - we just needed to throw something up quickly. The form
+    # object provides some basic validations, but there are too many
+    # permutations of possible values for it to be worth going overboard making
+    # sure we cover every possible way data might be 'invalid'. The onus is on
+    # the admins to make sure they don't save nonsensical data.
     class Edit < Trailblazer::Operation
       extend Contract::DSL
       contract Form
