d60eccb553ec45dd53685f70d1e70a2e8a24072f
George Millo <georgejulianmillo@gmail.com>
2017-04-21 18:05:58 -0500

    a/p#s: refactor filter JS
    We can remove the toggleOne function because it's only being called
    once. The fact that it's a separate function is a leftover from when it
    was also possible to filter by currencies.

--- app/assets/javascripts/other/admin/card_recommendations.js
+++ app/assets/javascripts/other/admin/card_recommendations.js
@@ -1,7 +1,22 @@
 /* eslint no-param-reassign: 0 */
 
 $(document).ready(function () {
-  function filterTable() {
+  // Hide and show the person's card accounts, and the recommendable offers,
+  // when the admin changes the filter options.
+  //
+  // This is split into two parts. First, when the admin changes a filter
+  // option (e.g. checks/unchecks a checkbox), the JS will update the *other*
+  // fields in the filter options form accordingly - e.g. by turning on the
+  // 'toggle all' checkbox if they manually check every other box individually.
+  //
+  // Once the form fields have been updated, the function
+  // updateFilteredElements is called, which shows/hides the card accounts and
+  // offers based on the current state of the form fields.
+  //
+  // This separates the display logic for the form from the display logic for
+  // the cards/offers, with only a minimal coupling between these two concerns.
+
+  function updateFilteredElements() {
     var checkedBPs, checkedBanks;
 
     checkedBPs = $(".card_bp_filter:checked").map(function (i, cb) {
@@ -37,45 +52,34 @@ $(document).ready(function () {
     });
   }
 
-  function toggleOne(checkboxes, toggleElem) {
-    // Show/hide the toggle all button:
-    // If *all* other boxes are also checked:
-    var allAreChecked = true;
-    checkboxes.each(function (i, checkbox) {
-      if (checkbox.checked) {
-        return true;
-      } else {
-        allAreChecked = false;
-        return false; // Return false to break out of the each loop early.
-      }
-    });
-    toggleElem.prop("checked", allAreChecked);
 
-    filterTable();
-  }
-
-  var $cardBankFilterCheckboxes = $('.card_bank_filter');
-  $cardBankFilterCheckboxes.click(function () {
-    toggleOne($cardBankFilterCheckboxes, $("#card_bank_filter_all"));
+  // the checkboxes for each bank
+  var $bankFilterCBs = $('.card_bank_filter');
+  var $toggleAllBanksCB = $("#card_bank_filter_all");
+  $bankFilterCBs.click(function () {
+    // Show the toggle all button iff *all* banks are checked:
+    var all = $bankFilterCBs.length === $bankFilterCBs.filter(':checked').length;
+    $toggleAllBanksCB.prop("checked", all);
+    updateFilteredElements();
   });
 
-  $("#card_bank_filter_all").click(function () {
+  $toggleAllBanksCB.click(function () {
     var toggleAllCB = this;
-    $cardBankFilterCheckboxes.each(function (i, cb) {
+    $bankFilterCBs.each(function (i, cb) {
       cb.checked = toggleAllCB.checked;
     });
-    filterTable();
+    updateFilteredElements();
   });
 
   $('.card_bp_filter').click(function (e) {
-    filterTable();
+    updateFilteredElements();
   });
 
   $('.card_bank_only_filter').click(function (e) {
     var bankId = this.dataset.value;
-    $("#card_bank_filter_all").prop('checked', false);
-    $(".card_bank_filter").prop('checked', false);
+    $toggleAllBanksCB.prop('checked', false);
+    $bankFilterCBs.prop('checked', false);
     $("#card_bank_filter_" + bankId).prop('checked', true);
-    filterTable();
+    updateFilteredElements();
   });
 });
