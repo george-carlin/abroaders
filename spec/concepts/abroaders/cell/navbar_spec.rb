26fba0e0fa49653a8a6d6c7faa68811413a90f85
George Millo <georgejulianmillo@gmail.com>
2017-04-24 17:43:21 -0500

    fix outdated specs for navbar cell
    simplify them too

--- spec/concepts/abroaders/cell/navbar_spec.rb
+++ spec/concepts/abroaders/cell/navbar_spec.rb
@@ -3,54 +3,41 @@ require 'cells_helper'
 RSpec.describe Abroaders::Cell::Navbar do
   controller ApplicationController
 
-  let(:rendered) { show(user) }
+  example 'not signed in' do
+    rendered = show(nil)
 
-  context 'when not signed in' do
-    subject(:user) { nil }
+    # it 'has links to sign in/up' do
+    expect(rendered).to have_link 'Sign up', href: new_account_registration_path
+    expect(rendered).to have_link 'Sign in', href: new_account_session_path
 
-    it 'has links to sign in/up' do
-      expect(rendered).to have_link 'Sign up', href: new_account_registration_path
-      expect(rendered).to have_link 'Sign in', href: new_account_session_path
-    end
+    # it 'shows the regular logo' do
+    expect(rendered).to have_selector '#logo:not(.admin-navbar)'
 
-    it 'shows the regular logo' do
-      expect(rendered).to have_selector '#logo:not(.admin-navbar)', text: /Abroaders\s*\z/
-    end
-
-    it 'has no search bar for accounts' do
-      expect(rendered).not_to have_selector '#admin_accounts_search_bar'
-    end
+    # it 'has no search bar for accounts' do
+    expect(rendered).not_to have_selector '#admin_accounts_search_bar'
   end
 
-  context 'when signed in as account' do
-    let(:user) { Account.new(email: 'admin@example.com') }
+  example 'when signed in as account' do
+    user = Account.new(email: 'admin@example.com')
+    rendered = show(user)
 
-    it 'has a link to sign out' do
-      expect(rendered).to have_link('', href: destroy_account_session_path)
-    end
-
-    it 'shows the regular logo' do
-      expect(rendered).to have_selector '#logo:not(.admin-navbar)', text: /Abroaders\s*\z/
-    end
-
-    it 'has no search bar for accounts' do
-      expect(rendered).not_to have_selector '#admin_accounts_search_bar'
-    end
+    # it 'has a link to sign out' do
+    expect(rendered).to have_link('', href: destroy_account_session_path)
+    # it 'shows the regular logo' do
+    expect(rendered).to have_selector '#logo:not(.admin-navbar)'
+    # it 'has no search bar for accounts' do
+    expect(rendered).not_to have_selector '#admin_accounts_search_bar'
   end
 
-  context 'when signed in as admin' do
-    let(:user) { Admin.new(email: 'admin@example.com') }
+  example 'when signed in as admin' do
+    user = Admin.new(email: 'admin@example.com')
+    rendered = show(user)
 
-    it 'has a link to sign out' do
-      expect(rendered).to have_link('', href: destroy_admin_session_path)
-    end
-
-    it 'shows the admin logo' do
-      expect(rendered).to have_selector '#logo.admin-navbar', text: 'Abroaders (Admin)'
-    end
-
-    it 'has a search bar for accounts' do
-      expect(rendered).to have_selector '#admin_accounts_search_bar'
-    end
+    # it 'has a link to sign out' do
+    expect(rendered).to have_link('', href: destroy_admin_session_path)
+    # it 'shows the admin logo' do
+    expect(rendered).to have_selector '#logo.admin-navbar'
+    # it 'has a search bar for accounts' do
+    expect(rendered).to have_selector '#admin_accounts_search_bar'
   end
 end
