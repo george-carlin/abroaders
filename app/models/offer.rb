bc7d6e35f5c97616bd995503609d41887dca746e
George Millo <georgejulianmillo@gmail.com>
2017-05-02 20:18:55 -0500

    add more documentation for Offer conditions

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -4,17 +4,8 @@ require 'types'
 # particular card product. For example, "spend $1000 on this card in your first
 # 3 months and receive 50,000 free points with Airline X".
 #
-# There are different ways you can get the bonus, as noted in the 'condition'
-# attribute:
-#
-# on_approval:       points awarded as soon as approved for card
-# on_first_purchase: points awarded once you make 1st purchase with card
-# on_minimum_spend:  points awarded if you spend $X within Y days
-#
-# It's also possible that a user will tell us that they have a particular card
-# but doesn't tell us which offer they used to sign up with, in which case
-# the condition will be 'unknown'. If it's a card that we recommended to them,
-# however, we will always know the condition.
+# There are different ways you can get the bonus, as noted in the 'Conditions'
+# type (see inline comments).
 #
 # Whether or not the other attributes will be present depends on the condition:
 #
@@ -30,19 +21,15 @@ require 'types'
 # 'link' is the link to the page where people can sign up for the cards.  NOTE:
 # any links from our app to the card application page MUST be nofollowed, for
 # compliance reasons.
-#
 class Offer < ApplicationRecord
-  # Attributes
-
-  # What does the customer have to do to receive the points?
+  # When are the points awarded?
   Conditions = Types::Strict::String.enum(
-    'on_approval',       # points awarded as soon as approved for card
-    'on_first_purchase', # points awarded once you make 1st purchase with card
-    'on_minimum_spend',  # points awarded if you spend $X within Y days
-    'unknown', # we don't know what offer the person signed up with
+    'on_approval',       # as soon as approved for card
+    'on_first_purchase', # once you make 1st purchase with card
+    'on_minimum_spend',  # if you spend $X within Y days
   ).freeze
 
-  # Which of our affilite partners is this offer for?
+  # Which of our affiliate partners provides this offer, if any?
   Partners = Types::Strict::String.enum(
     'award_wallet',
     'card_benefit',
