800a92e77b140ad1339df864ae2ad4270197bfa3
George Millo <georgejulianmillo@gmail.com>
2017-05-08 16:12:41 +0100

    fix weird autoloader bug (see comment)

--- app/concepts/admin_area/card_products/cell/option_tags.rb
+++ app/concepts/admin_area/card_products/cell/option_tags.rb
@@ -1,6 +1,23 @@
 module AdminArea
   module CardProducts
-    class OptionTags < Abroaders::Cell::Base
+    # For some reason, if this cell inherits from Abroaders::Cell::Base then
+    # the following bug occurs:
+    #
+    # 1. Start the server
+    # 2. Visit any page
+    # 3. Change the contents of a random file (as far as I can tell any file
+    #    will cause the error, but if you're struggling then one specific file
+    #    I tested with is Balance::Cell::Index.
+    # 4. Refresh the page twice (not once!)
+    # 5. You'll get the error 'superclass mismatch for class OptionTags'
+    #
+    # Looks like something funky is going on with the Rails autoloader. No idea
+    # why it's only happening for this class, but it's not worth investigating
+    # for now. Just use Trailblazer::Cell as the superclass instead, since
+    # OptionTags doesn't use any features from A::C::B.
+    class OptionTags < Trailblazer::Cell
+      include ActionView::Helpers::FormOptionsHelper
+
       def show
         products = CardProduct.all.map do |product|
           [CardProduct::Cell::FullName.(product, with_bank: true).(), product.id]
