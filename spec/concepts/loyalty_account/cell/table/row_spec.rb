b73c80454a9543a73e5655ef993e139c791d7779
George Millo <georgejulianmillo@gmail.com>
2017-05-08 15:25:45 +0100

    add spec for LA table row cell for AW accounts

--- spec/concepts/loyalty_account/cell/table/row_spec.rb
+++ spec/concepts/loyalty_account/cell/table/row_spec.rb
@@ -8,14 +8,35 @@ RSpec.describe LoyaltyAccount::Cell::Table::Row do
   let(:balance) { Balance.new(id: 1, person: person, value: 1234, currency: currency, updated_at: 5.minutes.ago) }
   let(:la) { LoyaltyAccount.build(balance) }
 
-  it '' do
+  it 'for an Abroaders balance' do
     rendered = show(la)
     expect(rendered).to have_content 'Sterling'
     expect(rendered).to have_content '1,234'
+    expect(rendered).to have_button 'Edit'
+    expect(rendered).to have_link 'Delete'
   end
 
   it 'avoids XSS attacks' do
     balance.currency.name = '<hacker>'
     expect(show(la).to_s).to include('&lt;hacker&gt;')
+  end
+
+  it 'for an AwardWallet account' do
+    owner = AwardWalletOwner.new(person: person)
+    awa = AwardWalletAccount.new(
+      id: 1,
+      aw_id: 234,
+      award_wallet_owner: owner,
+      balance_raw: 4321,
+      display_name: 'My currency',
+      login: 'GeorgeMillo',
+      updated_at: 5.minutes.ago,
+    )
+    account = LoyaltyAccount.build(awa)
+
+    rendered = show(account)
+    expect(rendered).to have_content 'My currency'
+    expect(rendered).to have_content '4,321'
+    expect(rendered).to have_link 'Edit'
   end
 end
