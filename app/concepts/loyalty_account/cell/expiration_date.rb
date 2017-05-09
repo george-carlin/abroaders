11c1d731de5578c2521210e14232c7a42668c402
George Millo <georgejulianmillo@gmail.com>
2017-05-09 11:06:14 +0100

    add docs/comments for LoyaltyAccount exp. date

--- app/concepts/loyalty_account/cell/expiration_date.rb
+++ app/concepts/loyalty_account/cell/expiration_date.rb
@@ -1,5 +1,13 @@
 class LoyaltyAccount < LoyaltyAccount.superclass
   module Cell
+    # Returns a <span> with information about when the loyalty account expires.
+    # If the loyalty account has no expiry date, then the text will say
+    # 'Unknown'. If there's no expiry date, or the expiry date is in the past
+    # or the very near future, then the <span> will also contain a warning
+    # icon.
+    #
+    # @!method self.call(loyalty_account, options = {})
+    #   @param loyalty_account [LoyaltyAccount]
     class ExpirationDate < ::Abroaders::Cell::Base
       property :expiration_date
 
@@ -25,6 +33,13 @@ class LoyaltyAccount < LoyaltyAccount.superclass
       def text
         return 'Unknown' if expiration_date.nil?
         distance_of_time_from_now = (expiration_date.to_time - Time.now).abs
+
+        # We can't use time_ago_in_words for everything, because it's too
+        # precise, e.g. it returns things like "5 minutes" and "about an hour".
+        # But we don't need to care about expiry time with that much precision;
+        # if it expires within 1 day of the present moment, simply stating the
+        # day is precise enough.
+
         if distance_of_time_from_now < 1.day
           'Today'
         elsif distance_of_time_from_now < 2.days
