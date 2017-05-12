74da601ebd201989d4cd6bac705f9653af98f646
George Millo <georgejulianmillo@gmail.com>
2017-05-12 09:43:14 +0100

    update layout cell

--- app/concepts/abroaders/cell/layout.rb
+++ app/concepts/abroaders/cell/layout.rb
@@ -1,26 +1,46 @@
 module Abroaders
   module Cell
-    # Placeholder class until we convert the whole layout to a cell
+    # Placeholder class until application.html.erb and basic.html.erb are
+    # fully extracted to cells
     class Layout < Abroaders::Cell::Base
+      option :title
+
+      # once the view is fully converted to a cell, everything below this
+      # line should be made private:
+
+      def head
+        cell(Head, nil, title: title)
+      end
+
+      def navbar
+        cell(Navbar, current_user)
+      end
+
+      def noscript
+        cell(Noscript)
+      end
+
+      def sidebar
+        @sidebar ||= cell(Sidebar, current_user)
+      end
+
+      def sidebar?
+        sidebar.show?
+      end
+
+      private
+
+      def current_user
+        current_account || current_admin
+      end
+
       class Head < Abroaders::Cell::Base
         include ActionView::Helpers::CsrfHelper
         include ::Cell::Helper::AssetHelper
 
         BASE_TITLE = "Abroaders".freeze
 
-        def head
-          cell(Head)
-        end
-
-        def sidebar
-          cell(Sidebar, model) if sidebar?
-        end
-
         option :title
-
-        def stylesheet_link_tag(*args)
-          super
-        end
 
         private
 
