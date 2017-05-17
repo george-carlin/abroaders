f206a22941608195bbd479e8d7df168f59e96460
George Millo <georgejulianmillo@gmail.com>
2017-05-17 12:55:05 +0100

    s/btn-facebook/btn-connect/

--- app/concepts/sessions/cell/facebook_connect_button.rb
+++ app/concepts/sessions/cell/facebook_connect_button.rb
@@ -2,7 +2,9 @@ module Sessions
   module Cell
     class FacebookConnectButton < Abroaders::Cell::Base
       def show(btn_text)
-        link_to '/auth/facebook', class: 'btn btn-facebook btn-block' do
+        # Don't use a class name that includes 'facebook' or 'fb' or we risk
+        # the button being hidden by some users' ad blockers.
+        link_to '/auth/facebook', class: 'btn btn-connect btn-block' do
           "<i class='fa fa-facebook-square'> </i> #{btn_text}"
         end
       end
