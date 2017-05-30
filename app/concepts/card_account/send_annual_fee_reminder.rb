5a24b7acdbaaaae770df106aa0036068cc34ea15
George Millo <georgejulianmillo@gmail.com>
2017-05-30 08:07:28 +0200

    send annual fee notification

---
+++ app/concepts/card_account/send_annual_fee_reminder.rb
@@ -0,0 +1,18 @@
+class CardAccount < CardAccount.superclass
+  class SendAnnualFeeReminder
+    def self.call
+      unless Date.today.day == 1
+        raise "can't run unless today is the 1st of the month"
+      end
+
+      CardAccount::Query::AnnualFeeDue.().includes(
+        :account, person: :account,
+      ).group_by(&:account).each do |account, cards|
+        card_ids = cards.map(&:id)
+        CardMailer.annual_fee_reminder(
+          account_id: account.id, card_ids: card_ids,
+        ).deliver_later
+      end
+    end
+  end
+end
--- app/mailers/notification_mailer.rb
+++
@@ -1,9 +0,0 @@
-class NotificationMailer < ApplicationMailer
-  def notify_account_for_card_fee(card)
-    @card_holder = card.person
-    @account = @card_holder.account
-    @product = card.card_product
-
-    mail(to: @account.email, subject: "Annual fee notification")
-  end
-end
