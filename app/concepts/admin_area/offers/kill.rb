eb129225d9ca5e927494584d91941c69a90afeea
George Millo <georgejulianmillo@gmail.com>
2017-06-16 07:52:32 -0300

    Offer::Kill can swap in a replacement offer

--- app/concepts/admin_area/offers/kill.rb
+++ app/concepts/admin_area/offers/kill.rb
@@ -1,10 +1,18 @@
 module AdminArea
   module Offers
-    # kill an offer (duh). The only required param is the offer's ID.
+    # kill an offer (duh).
+    #
+    # @!method self.call(params, options = {})
+    #   @option params [Integer] offer_id required
+    #   @option params [Integer] replacement_id optional. If
+    #     replacement_id is specified AND the killed offer has any
+    #     unresolved recs, then the unresolved recs will have their offer
+    #     updated with the replacement
     class Kill < Trailblazer::Operation
       step :setup_model
       step :offer_live?
       failure :log_offer_already_killed
+      success :replace
       step :kill!
 
       private
@@ -19,6 +27,15 @@ module AdminArea
 
       def log_offer_already_killed(opts)
         opts['error'] = 'Offer already killed'
+      end
+
+      def replace(model:, params:, **)
+        unresolved_recs = model.unresolved_recommendations
+        unresolved_recs.size
+        if params[:replacement_id] && unresolved_recs.any?
+          replacement = Offer.find(params[:replacement_id])
+          unresolved_recs.update_all(offer_id: replacement.id)
+        end
       end
 
       def kill!(model:, **)
