79ee5cfaac078ca675e8077aaad934eda017e39e
George Millo <georgejulianmillo@gmail.com>
2017-06-26 12:39:35 -0300

    add custom <title>s to 404 & 500 pages

--- app/concepts/errors/cell/internal_server_error.rb
+++ app/concepts/errors/cell/internal_server_error.rb
@@ -1,6 +1,9 @@
 module Errors
   module Cell
     class InternalServerError < Abroaders::Cell::Base
+      def title
+        'Error'
+      end
     end
   end
 end
--- app/concepts/errors/cell/not_found.rb
+++ app/concepts/errors/cell/not_found.rb
@@ -1,6 +1,9 @@
 module Errors
   module Cell
     class NotFound < Abroaders::Cell::Base
+      def title
+        'Page Not Found'
+      end
     end
   end
 end
