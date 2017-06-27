20370eac722170bbbcab4e6868550ad82a64b2c7
George Millo <georgejulianmillo@gmail.com>
2017-06-27 11:05:35 -0300

    limit account search results to 50 per page

--- app/concepts/admin_area/accounts/cell/search.rb
+++ app/concepts/admin_area/accounts/cell/search.rb
@@ -3,6 +3,7 @@ module AdminArea
     module Cell
       class Search < Abroaders::Cell::Base
         extend Abroaders::Cell::Result
+        include Kaminari::Cells
         include Escaped
 
         skill :collection
@@ -13,12 +14,24 @@ module AdminArea
 
         private
 
+        def page
+          options[:page] || 1
+        end
+
+        def paginated_accounts
+          @paginated_accounts ||= collection.page(page).per_page(50)
+        end
+
+        def paginator
+          paginate(paginated_accounts)
+        end
+
         def query
           escape!(result['query'])
         end
 
         def table_rows
-          cell(AdminArea::Accounts::Cell::Index::TableRow, collection: collection)
+          cell(AdminArea::Accounts::Cell::Index::TableRow, collection: paginated_accounts)
         end
       end
     end
