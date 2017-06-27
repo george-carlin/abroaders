486b1b70006d2b5393c61570ad2450714db5caab
George Millo <georgejulianmillo@gmail.com>
2017-06-27 11:45:06 -0300

    remove unused method current_user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,6 @@
 module ApplicationHelper
   include BootstrapOverrides
 
-  def current_user
-    current_admin || current_account
-  end
-
   def title(provided = '')
     if @cell_title
       @cell_title
