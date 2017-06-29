214ee5fd76f683c6f222efda18fffae3b70604cf
George Millo <georgejulianmillo@gmail.com>
2017-06-29 11:34:30 -0300

    fully extract layout cell

--- app/concepts/abroaders/cell/layout.rb
+++ app/concepts/abroaders/cell/layout.rb
@@ -1,13 +1,28 @@
 module Abroaders
   module Cell
-    # Placeholder class until application.html.erb and basic.html.erb are
-    # fully extracted to cells
+    # The layout. Pass the return value of 'yield' as the model.
+    #
+    # @!method self.call(html, options = {})
+    #   @param html [String] a string of HTML
+    #   @option option [Boolean] basic (false) when true, the sidebar and
+    #     the '#wrapper' div won't be included in the rendered layout.
+    #   @option options [ActionDispatch::Flash] flash
+    #   @option options [String] title the page title
     class Layout < Abroaders::Cell::Base
+      option :basic, default: false
       option :flash
       option :title
 
-      # once the view is fully converted to a cell, everything below this
-      # line should be made private:
+      private
+
+      alias basic? basic
+
+      def body_classes
+        result = []
+        result << 'blank' unless sidebar?
+        result.push('fixed-navbar', 'fixed-sidebar') unless basic?
+        result.join(' ')
+      end
 
       def flash_alerts
         cell(FlashAlerts, flash)
@@ -30,10 +45,12 @@ module Abroaders
       end
 
       def progress_bar
+        return '' if basic?
         cell(Onboarding::Cell::ProgressBar, current_account)
       end
 
       def rec_alert
+        return '' if basic?
         cell(Abroaders::Cell::RecommendationAlert, current_account)
       end
 
@@ -42,14 +59,23 @@ module Abroaders
       end
 
       def sidebar?
-        sidebar.show?
+        !basic? && sidebar.show?
       end
 
       def third_party_scripts
         cell(ThirdPartyScripts)
       end
 
-      private
+      def wrapper(&block)
+        return yield if basic?
+        content_tag(
+          :div,
+          id: 'wrapper',
+          class: "wrapper #{'wrapper-with-sidebar' if sidebar?}",
+        ) do
+          content_tag :div, class: 'content', &block
+        end
+      end
 
       def current_user
         current_account || current_admin
