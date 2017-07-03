a4937654fe2e7fa4e494f6262f0d0780eb459a02
George Millo <georgejulianmillo@gmail.com>
2017-07-03 12:15:13 -0300

    admin/ca#edit - use inheritance instead of Nested

--- app/concepts/admin_area/card_accounts/edit.rb
+++ app/concepts/admin_area/card_accounts/edit.rb
@@ -1,9 +1,7 @@
 module AdminArea
   module CardAccounts
-    class Edit < Trailblazer::Operation
-      step Nested(::CardAccount::Edit, input: -> (opts) do
-        opts.to_hash.merge('card_scope' => Card.accounts)
-      end,)
+    class Edit < ::CardAccount::Edit
+      self['card_scope'] = Card.accounts
     end
   end
 end
