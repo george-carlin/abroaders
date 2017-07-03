0c2bd6e895e94dae3b746592fb7920fe7f63a144
George Millo <georgejulianmillo@gmail.com>
2017-07-03 13:40:38 -0300

    add more specs for admin/card_rec edit op

--- spec/concepts/admin_area/card_recommendations/update_spec.rb
+++ spec/concepts/admin_area/card_recommendations/update_spec.rb
@@ -18,14 +18,57 @@ RSpec.describe AdminArea::CardRecommendations::Update do
 
   example 'updating' do
     rec = create_rec
-    params[:card] = { applied_on: dec_2015, opened_on: jan_2016 }
+    params[:card] = {
+      applied_on: dec_2015,
+      declined_at: jan_2016,
+      decline_reason: 'whatever',
+    }
     params[:id] = rec.id
 
     result = op.(params)
     expect(result.success?).to be true
 
     rec = result['model']
-    expect(rec.applied_on.to_date).to eq dec_2015
-    expect(rec.opened_on.to_date).to eq jan_2016
+    expect(rec.applied_on).to eq dec_2015
+    expect(rec.declined_at.to_date).to eq jan_2016
+    expect(rec.decline_reason).to eq 'whatever'
+  end
+
+  example 'updating to opened' do
+    rec = create_rec
+    params[:card] = { opened_on: dec_2015 }
+    params[:id] = rec.id
+
+    result = op.(params)
+    expect(result.success?).to be true
+
+    expect(result['model'].opened_on).to eq dec_2015
+  end
+
+  example 'updating when already opened' do
+    rec = create_rec(opened_on: Date.today)
+    params[:card] = { applied_on: dec_2015, denied_at: jan_2016 }
+    params[:id] = rec.id
+
+    result = op.(params)
+    expect(result.success?).to be true
+
+    rec = result['model']
+    expect(rec.applied_on).to eq dec_2015
+    expect(rec.denied_at.to_date).to eq jan_2016
+  end
+
+  example 'invalid save' do
+    rec = create_rec
+    # missing decline reason:
+    params[:card] = { applied_on: dec_2015, declined_at: jan_2016 }
+    params[:id] = rec.id
+
+    rec.reload
+    expect do
+      result = op.(params)
+      expect(result.success?).to be false
+      rec.reload
+    end.not_to change { rec.updated_at }
   end
 end
