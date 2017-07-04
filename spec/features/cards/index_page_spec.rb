699139924ba13260578539241f354314d444bd9e
George Millo <georgejulianmillo@gmail.com>
2017-07-04 14:37:34 -0300

    fix CI-specific spec error

--- spec/features/cards/index_page_spec.rb
+++ spec/features/cards/index_page_spec.rb
@@ -57,6 +57,11 @@ RSpec.describe 'as a user viewing my cards' do
     card = create_card_account(person: owner)
     other_persons_rec = create_card_recommendation(:seen, person_id: create_person.id)
 
+    # reload these recs here or you get a weird error on Codeship because
+    # of CS's date precision
+    seen_rec.reload
+    other_persons_rec.reload
+
     expect do
       visit_page
       seen_rec.reload
