5d814720ffb101ed27897a36aafcb43effe07047
George Millo <georgejulianmillo@gmail.com>
2017-07-12 16:04:20 -0500

    extract cell for admin card recs table

--- app/concepts/admin_area/people/cell/show/card_recommendations.rb
+++ app/concepts/admin_area/people/cell/show/card_recommendations.rb
@@ -15,17 +15,10 @@ module AdminArea::People::Cell
 
       private
 
-      def table_rows
-        cell(Row, collection: visible_recommendations)
-      end
-
-      def table_tag(&block)
-        content_tag(
-          :table,
-          class: 'table table-striped tablesorter',
-          id: 'admin_person_card_recommendations_table',
-          &block
-        )
+      def table
+        id = 'admin_person_card_recommendations_table'
+        cell_class = AdminArea::CardRecommendations::Cell::Table
+        cell(cell_class, visible_recommendations, html_id: id)
       end
 
       def wrapper(&block)
@@ -42,112 +35,6 @@ module AdminArea::People::Cell
 
       def visible_recommendations
         @_vr ||= card_recommendations.reject(&:opened?)
-      end
-
-      class Row < Abroaders::Cell::Base
-        include Escaped
-
-        # @param recommendation [Card] must be a recommendation
-        def initialize(rec, options = {})
-          super(CardRecommendation.new(rec), options)
-        end
-
-        property :id
-        property :applied_on
-        property :card_product
-        property :clicked_at
-        property :decline_reason
-        property :declined?
-        property :declined_at
-        property :offer
-        property :recommended_at
-        property :seen_at
-        property :status
-        property :unresolved?
-
-        private
-
-        delegate :bp, to: :card_product, prefix: true
-
-        %i[
-          recommended_at seen_at clicked_at denied_at declined_at applied_on
-        ].each do |date_attr|
-          define_method date_attr do
-            super()&.strftime('%D') || '-' # 12/01/2015
-          end
-        end
-
-        # If the card was opened/closed after being recommended by an admin,
-        # we know the exact date it was opened closed. If they added the card
-        # themselves (e.g. when onboarding), they only provide the month
-        # and year, and we save the date as the 1st of thet month.  So if the
-        # card was added as a recommendation, show the the full date, otherwise
-        # show e.g.  "Jan 2016". If the date is blank, show '-'
-        #
-        # TODO rethink how we know whether a card was added in the survey
-        %i[closed_on opened_on].each do |date_attr|
-          define_method date_attr do
-            # if model.recommended_at.nil? # if card is not a recommendation
-            # super()&.strftime('%b %Y') || '-' # Dec 2015
-            # else
-            super()&.strftime('%D') || '-' # 12/01/2015
-            # end
-          end
-        end
-
-        def card_product_name
-          cell(CardProduct::Cell::FullName, card_product, with_bank: true)
-        end
-
-        def decline_reason_tooltip
-          return '' unless model.declined?
-          link_to('#', 'data-toggle': 'tooltip', title: decline_reason) do
-            fa_icon('question')
-          end
-        end
-
-        def link_to_delete
-          link_to(
-            'Del',
-            admin_card_recommendation_path(model),
-            data: {
-              confirm: 'Are you sure you want to delete this recommendation?',
-              method: :delete,
-            },
-          )
-        end
-
-        def link_to_edit
-          link_to 'Edit', edit_admin_card_recommendation_path(model)
-        end
-
-        def link_to_offer
-          link_to(
-            offer_identifier,
-            admin_offer_path(offer),
-            class: ('unresolved-dead-offer' if offer.dead? && unresolved?),
-          )
-        end
-
-        def offer_identifier
-          cell(AdminArea::Offers::Cell::Identifier, offer, with_partner: true)
-        end
-
-        def status
-          Inflecto.humanize(super)
-        end
-
-        def tr_tag(&block)
-          content_tag(
-            :tr,
-            id: "card_recommendation_#{id}",
-            class: 'card_recommendation',
-            'data-bp':       card_product.bp,
-            'data-bank':     card_product.bank_id,
-            'data-currency': card_product.currency_id,
-            &block
-          )
-        end
       end
     end
   end
