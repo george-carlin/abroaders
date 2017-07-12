george-carlin/abroaders

commit e80643037d0deccfc35cbddc9c676033fef5eabf

fix FB tracking pixel (for real this time)

app/concepts/abroaders/cell/third_party_scripts.rb
@@ -23,6 +23,32 @@ module Abroaders
           post_affiliate_pro
         ].map { |path| render view: "third_party_scripts/#{path}" }.join
       end
+
+      private
+
+      # We need to run a Javascript snippet immediately after a user signs up -
+      # but we need to make sure it doesn't get run again if they e.g. refresh
+      # the page. The hacky solution below will do for now:
+
+      def fb_tracked?
+        !!cookies[fb_tracking_cookie_key]
+      end
+
+      def fb_tracked!
+        cookies[fb_tracking_cookie_key] = true
+      end
+
+      def fb_tracking_cookie_key
+        # Use a meaningless name to obfuscate the cookie's purpose:
+        :cbd50008665cc7269327074d2778d9a6
+      end
+
+      def fb_tracking_pixel_post_sign_up?
+        # If a) we're on the first page after signing up and b) the cookie
+        # isn't present that indicates that this user has been tracked.  then
+        # output the 'CompleteRegistration' FB tracking <script>
+        !fb_tracked? && request.path == survey_home_airports_path
+      end
     end
   end
 end

app/concepts/abroaders/view/third_party_scripts/fb_tracking_pixel.erb
@@ -14,10 +14,10 @@
     /></noscript>
   <!-- End Facebook Pixel Code -->
 
-  <% if @output_fb_signup_code %>
+  <% if fb_tracking_pixel_post_sign_up? %>
     <script>
       fbq('track', 'CompleteRegistration');
-      <% cookies[:cbd50008665cc7269327074d2778d9a6] = true %>
+      <% fb_tracked! %>
     </script>"
   <% end %>
 <% end %>

app/controllers/home_airports_controller.rb
@@ -9,7 +9,6 @@ class HomeAirportsController < AuthenticatedUserController
     # (see also concepts/abroaders/view/third_party_scripts/fb_tracking_pixel)
     # Use a randomly-generated cookie name to obfuscate what it's for if the
     # user looks at their cookies.
-    @output_fb_signup_code = !cookies[:cbd50008665cc7269327074d2778d9a6]
     @survey = HomeAirports::Survey.new(@account)
   end
 

commit acbe171445686a4ecfc9b37d777ca28380338c2f

fix PAP script that was broken by e80643037

app/concepts/abroaders/cell/third_party_scripts.rb
@@ -47,7 +47,11 @@ module Abroaders
         # If a) we're on the first page after signing up and b) the cookie
         # isn't present that indicates that this user has been tracked.  then
         # output the 'CompleteRegistration' FB tracking <script>
-        !fb_tracked? && request.path == survey_home_airports_path
+        !fb_tracked? && just_signed_up?
+      end
+
+      def just_signed_up?
+        request.path == survey_home_airports_path
       end
     end
   end

app/concepts/abroaders/view/third_party_scripts/post_affiliate_pro.erb
@@ -9,4 +9,29 @@
     PostAffTracker.track();
     } catch (err) { }
   </script>
+
+  <% if just_signed_up? %>
+    <div id='papSaleTrackingPlaceholder'></div>
+  <% end %>
+
+  <script type="text/javascript">
+    (function(d,t) {
+      var script = d.createElement(t); script.id= 'pap_x2s6df8d'; script.async = true;
+      script.src = '//abroaders.postaffiliatepro.com/scripts/fvoj8fzu';
+      script.onload = script.onreadystatechange = function() {
+        var rs = this.readyState; if (rs && (rs != 'complete') && (rs != 'loaded')) return;
+
+        PostAffTracker.setAccountId('default1');
+        var sale = PostAffTracker.createAction('app_signup');
+        sale.setData1('<%= current_account.id %>');
+        sale.setData2('<%= current_account.email %>');
+        sale.setData3('<%= current_account.owner_first_name %>');
+
+        try { PostAffTracker.track(); } catch (e) {}
+      }
+      var placeholder = document.getElementById('papSaleTrackingPlaceholder');
+      placeholder.parentNode.insertBefore(script, placeholder);
+      placeholder.parentNode.removeChild(placeholder);
+    })(document, 'script');
+  </script>
 <% end %>

app/views/home_airports/survey.html.erb
@@ -24,33 +24,3 @@
 
   <%= render "home_airports/survey_form", survey: @survey %>
 </div>
-
-<% if Rails.env.production? %>
-  <% if @output_fb_signup_code %>
-    <%# make sure this is only output when the user has first signed up, %>
-    <%# and at no other time: %>
-    <div id='papSaleTrackingPlaceholder'></div>
-  <% end %>
-
-  <script type="text/javascript">
-    (function(d,t) {
-      var script = d.createElement(t); script.id= 'pap_x2s6df8d'; script.async = true;
-      script.src = '//abroaders.postaffiliatepro.com/scripts/fvoj8fzu';
-      script.onload = script.onreadystatechange = function() {
-        var rs = this.readyState; if (rs && (rs != 'complete') && (rs != 'loaded')) return;
-
-        PostAffTracker.setAccountId('default1');
-        var sale = PostAffTracker.createAction('app_signup');
-        sale.setData1('<%= current_account.id %>');
-        sale.setData2('<%= current_account.email %>');
-        sale.setData3('<%= current_account.owner_first_name %>');
-
-        try { PostAffTracker.track(); } catch (e) {}
-
-      }
-      var placeholder = document.getElementById('papSaleTrackingPlaceholder');
-      placeholder.parentNode.insertBefore(script, placeholder);
-      placeholder.parentNode.removeChild(placeholder);
-    })(document, 'script');
-  </script>
-<% end %>

app/views/layouts/application.html.erb
@@ -3,5 +3,4 @@
   yield,
   flash: flash,
   title: title(yield(:title)),
-  output_fb_signup_code: @output_fb_signup_code,
 ) %>

app/views/layouts/basic.html.erb
@@ -4,5 +4,4 @@
   basic: true,
   flash: flash,
   title: title(yield(:title)),
-  output_fb_signup_code: @output_fb_signup_code,
 ) %>