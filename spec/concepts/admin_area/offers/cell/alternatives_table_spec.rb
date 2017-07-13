72668d1a6a53806a2a226b1a76a80611812ec6f9
George Millo <georgejulianmillo@gmail.com>
2017-07-13 15:32:07 -0500

    fix tests for offer AlternativesTable cell

--- spec/concepts/admin_area/offers/cell/alternatives_table_spec.rb
+++ spec/concepts/admin_area/offers/cell/alternatives_table_spec.rb
@@ -1,36 +1,41 @@
 require 'cells_helper'
 
 RSpec.describe AdminArea::Offers::Cell::AlternativesTable do
-  let(:offer) { create_offer(attrs) }
-  let(:card_product) { create(:card_product) }
+  describe '::Section' do
+    let(:cell_class) { described_class::Section }
 
-  let(:attrs) do
-    {
-      card_product: card_product,
-      condition: 'no_bonus',
-      cost: 0,
-      partner: 'card_benefit',
-    }
-  end
+    let(:offer) { create_offer(attrs) }
+    let(:card_product) { create(:card_product) }
 
-  before do # offers that aren't matches
-    create_offer(attrs.except(:card_product))
-    create_offer(attrs.merge(cost: 1))
-    create_offer(card_product: card_product, condition: 'on_minimum_spend')
-  end
+    let(:attrs) do
+      {
+        card_product: card_product,
+        condition: 'no_bonus',
+        cost: 0,
+        partner: 'card_benefit',
+      }
+    end
 
-  example '' do
-    other_0 = create_offer(attrs.merge(value: 10))
-    other_1 = create_offer(attrs.merge(partner: 'credit_cards'))
+    before do # offers that aren't matches
+      create_offer(attrs.except(:card_product))
+      create_offer(attrs.merge(cost: 1))
+      create_offer(card_product: card_product, condition: 'on_minimum_spend')
+    end
 
-    rendered = cell(described_class, offer).()
-    expect(rendered).not_to have_selector "#offer_#{offer.id}"
-    expect(rendered).to have_selector "#offer_#{other_0.id}"
-    expect(rendered).to have_selector "#offer_#{other_1.id}"
-  end
+    example '' do
+      other_0 = create_offer(attrs.merge(value: 10))
+      other_1 = create_offer(attrs.merge(partner: 'credit_cards'))
 
-  example 'offer has no alternatives' do
-    raise if AdminArea::Offers::AlternativesFor.(offer).any? # sanity check
-    expect(raw_cell(described_class, offer)).to eq ''
+      rendered = cell(cell_class, offer).()
+      expect(rendered).not_to have_selector "#offer_#{offer.id}"
+      expect(rendered).to have_selector "#offer_#{other_0.id}"
+      expect(rendered).to have_selector "#offer_#{other_1.id}"
+    end
+
+    example 'offer has no alternatives' do
+      raise if AdminArea::Offers::AlternativesFor.(offer).any? # sanity check
+      expect(raw_cell(cell_class, offer)).to \
+        eq "<h4>Alternatives</h4>No alternatives found"
+    end
   end
 end
