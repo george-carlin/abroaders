b7a18d98a00640eb907f14f70e0d537ef6c96e47
George Millo <georgejulianmillo@gmail.com>
2017-08-11 17:30:39 -0300

    /cards redesign: update rec note display

--- app/concepts/card/cell/index/rec_note.rb
+++ app/concepts/card/cell/index/rec_note.rb
@@ -1,33 +1,53 @@
 module Card::Cell
   # model: a RecommendationNote.
   class Index::RecNote < Abroaders::Cell::Base
+    include Escaped
+
     property :created_at
 
     option :recommended_by # an Admin
 
+    alias admin recommended_by
+
     private
+
+    %w[full_name first_name job_title].each do |method|
+      define_method "admin_#{method}" do
+        escape!(admin.send(method))
+      end
+    end
 
     def content
       cell(RecommendationNote::Cell::FormattedContent, model)
     end
 
+    def headshot
+      cell(Headshot, admin).show(60)
+    end
+
+    def link_to_see_bio
+      return '' if admin.bio.blank?
+      link_to(
+        'see bio',
+        '#',
+        style: 'color: #35a7ff',
+        data: { toggle: 'modal', target: '#rec_recommended_by_bio' },
+      )
+    end
+
+    def bio_modal
+      return '' if admin.bio.blank?
+      cell(BioModal, admin, timestamp: timestamp)
+    end
+
     def timestamp
-      created_at.strftime("%D %l:%M %P EST")
-    end
-
-    def recommended_by_basic
-      cell(RecommendedBy, recommended_by, timestamp: timestamp)
-    end
-
-    def recommended_by_modal
-      cell(RecommendedBy::Modal, recommended_by, timestamp: timestamp)
+      created_at.in_time_zone('EST').strftime("%D %l:%M %P EST")
     end
 
     # model: Admin
-    class RecommendedBy < Abroaders::Cell::Base
-      property :avatar
-      property :bio
-      property :first_name
+    class BioModal < Abroaders::Cell::Base
+      include Escaped
+
       property :full_name
       property :job_title
 
@@ -35,28 +55,40 @@ module Card::Cell
 
       private
 
+      def bio # don't use property :bio because we don't want it to be escaped
+        model.bio
+      end
+
       def headshot
-        headshot_with_size(60)
+        cell(Headshot, model).show(120)
       end
 
-      def headshot_big
-        headshot_with_size(120)
+      def job_title
+        return '' if super.blank?
+        "<p style='color:#ffffff'>#{super}, Abroaders</p>"
       end
+    end
 
-      def headshot_with_size(s)
+    # model: admin
+    class Headshot < Abroaders::Cell::Base
+      include Escaped
+
+      property :avatar
+      property :first_name
+
+      def show(width)
         image_tag(
           avatar.url,
-          size: "#{s}x#{s}",
-          class: 'img-responsive img-circle',
+          size: "#{width}x#{width}",
+          class: 'img-responsive center-block img-circle',
           alt: first_name,
         )
       end
 
-      def job_title_text
-        "#{job_title}, Abroaders"
-      end
+      private
 
-      class Modal < self
+      def headshot
+        cell(Headshot, admin).show(120)
       end
     end
   end
