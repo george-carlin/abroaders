e3d0eab43f9892f283fb90a53673325c060c4560
George Millo <georgejulianmillo@gmail.com>
2017-08-14 15:53:53 -0300

    fix broken refs

--- app/concepts/admin_area/admins/new_form.rb
+++ app/concepts/admin_area/admins/new_form.rb
@@ -1,8 +1,10 @@
 module AdminArea::Admins
   class NewForm < Form
+    PASSWORD_LENGTH = Registration::SignUpForm::PASSWORD_LENGTH.dup
+
     property :password
     validates :password,
               presence: true,
-              length: { in: Admin.password_length, allow_blank: true }
+              length: { in: PASSWORD_LENGTH, allow_blank: true }
   end
 end
