72713d1c9b4337687966f5007e0fdfab984e88d3
George Millo <georgejulianmillo@gmail.com>
2017-08-15 12:20:33 -0300

    convert home airports survey to cell

--- app/controllers/home_airports_controller.rb
+++ app/controllers/home_airports_controller.rb
@@ -2,26 +2,18 @@ class HomeAirportsController < AuthenticatedUserController
   onboard :home_airports, with: [:survey, :save_survey]
 
   def survey
-    @account = current_account
-    # We need to run a Javascript snippet immediately after a user signs up -
-    # but we need to make sure it doesn't get run again if they e.g. refresh
-    # the page. This hacky solution will do for now.
-    # (see also concepts/abroaders/view/third_party_scripts/fb_tracking_pixel)
-    # Use a randomly-generated cookie name to obfuscate what it's for if the
-    # user looks at their cookies.
-    @survey = HomeAirports::Survey.new(@account)
+    form = HomeAirports::Survey.new(current_account)
+    render cell(HomeAirports::Cell::Survey, current_account, form: form)
   end
 
   def save_survey
-    @account = current_account
-    @survey = HomeAirports::Survey.new(@account)
+    form = HomeAirports::Survey.new(current_account)
 
-    if @survey.validate(params[:home_airports_survey])
-      @survey.save
+    if form.validate(params[:home_airports_survey])
+      form.save
       Account::Onboarder.new(current_account).add_home_airports!
       redirect_to onboarding_survey_path
-    else
-      # It's impossible to submit an invalid form through the web interface
+    else # it's impossible to submit an invalid form through the web interface
       raise 'this should never happen!'
     end
   end
@@ -31,9 +23,8 @@ class HomeAirportsController < AuthenticatedUserController
   end
 
   def edit
-    @account = current_account
-    @survey = HomeAirports::Survey.new(@account)
-    render 'survey'
+    form = HomeAirports::Survey.new(current_account)
+    render cell(HomeAirports::Cell::Survey, current_account, form: form, editing: true)
   end
 
   # This is a quick solution to the problem of how users can update their home
@@ -44,22 +35,13 @@ class HomeAirportsController < AuthenticatedUserController
   # This isn't great design, but it's not a very important feature and we
   # needed something built quickly.
   def overwrite
-    @account = current_account
-    @survey = HomeAirports::Survey.new(@account)
+    form = HomeAirports::Survey.new(current_account)
 
-    if @survey.validate(params[:home_airports_survey])
-      @survey.save
+    if form.validate(params[:home_airports_survey])
+      form.save
       redirect_to onboarding_survey_path
-    else
-      # It's impossible to submit an invalid form through the web interface
+    else # it's impossible to submit an invalid form through the web interface
       raise 'this should never happen!'
     end
-  end
-
-  private
-
-  def survey_params
-    survey_params = params.require(:home_airports_survey).permit(airport_ids: [])
-    survey_params.merge(account: @account)
   end
 end
