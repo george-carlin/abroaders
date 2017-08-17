a319f916bedcf50f87451c7c35429548af48fcff
George Millo <georgejulianmillo@gmail.com>
2017-08-17 16:43:48 -0300

    card product - remove duplicate enum types

--- app/concepts/admin_area/card_products/form.rb
+++ app/concepts/admin_area/card_products/form.rb
@@ -9,9 +9,9 @@ module AdminArea
       property :currency_id, type: Types::Form::Int
       property :image
       property :name, type: Types::Form::Int
-      property :network, type: Types::Strict::String.enum('unknown', 'visa', 'mastercard', 'amex')
+      property :network, type: CardProduct::Network
       property :shown_on_survey, type: Types::Form::Bool
-      property :type, type: Types::Strict::String.enum('unknown', 'credit', 'charge', 'debit')
+      property :type, type: CardProduct::Type
 
       validates :annual_fee,
                 numericality: { greater_than_or_equal_to: 0 },
