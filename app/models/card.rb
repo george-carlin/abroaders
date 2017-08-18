659e67795476f104faeb8c24b8a0762e339d9ae3
George Millo <georgejulianmillo@gmail.com>
2017-08-18 15:19:14 -0300

    make card belongs_to assocs optional

--- app/models/card.rb
+++ app/models/card.rb
@@ -125,10 +125,10 @@ class Card < ApplicationRecord
   # Associations
 
   belongs_to :card_product
-  belongs_to :offer, counter_cache: true
+  belongs_to :offer, counter_cache: true, optional: true
   belongs_to :person
-  belongs_to :recommended_by, class_name: 'Admin'
-  belongs_to :recommendation_request
+  belongs_to :recommended_by, class_name: 'Admin', optional: true
+  belongs_to :recommendation_request, optional: true
   has_one :account, through: :person
   has_one :currency, through: :card_product
 
