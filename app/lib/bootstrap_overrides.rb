a3c815b840d11ba76e0afc73cfe57111dd4e11a9
George Millo <georgejulianmillo@gmail.com>
2017-08-22 11:33:02 -0300

    add number_field_tag to BootstrapOverrides

--- app/lib/bootstrap_overrides.rb
+++ app/lib/bootstrap_overrides.rb
@@ -10,7 +10,7 @@ module BootstrapOverrides
   # See stackoverflow.com/a/18844246/1603071
   [
     :text_field, :email_field, :password_field, :text_area,
-    :text_field_tag, :number_field,
+    :text_field_tag, :number_field, :number_field_tag,
   ].each do |meth|
     define_method(meth) do |name, value = nil, options = {}|
       options[:class] = add_class(options[:class], BOOTSTRAP_CLASS)
