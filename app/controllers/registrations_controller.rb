d82c9f34233afdcea176e082839385bedba3d0c8
George Millo <georgejulianmillo@gmail.com>
2017-08-25 14:28:33 -0300

    all basic layout pages use Rio BG

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,7 +7,6 @@ class RegistrationsController < ApplicationController
 
   def new
     run Registration::New
-    @custom_body_class = 'rio-bg'
     render cell(Registration::Cell::New, @model, form: @form)
   end
 
@@ -19,7 +18,6 @@ class RegistrationsController < ApplicationController
     end
 
     @form.clean_up_passwords
-    @custom_body_class = 'rio-bg'
     render cell(Registration::Cell::New, @model, form: @form)
   end
 
