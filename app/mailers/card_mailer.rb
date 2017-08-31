f98cf32f5d105c198ac8aafe44ded6825eda0c42
George Millo <georgejulianmillo@gmail.com>
2017-08-31 13:35:32 -0300

    card mailer symbolize hash keys

--- app/mailers/card_mailer.rb
+++ app/mailers/card_mailer.rb
@@ -1,8 +1,9 @@
 class CardMailer < ApplicationMailer
   # opts: account_id, card_ids
   def annual_fee_reminder(opts = {})
-    @account = Account.find(opts.fetch('account_id'))
-    @cards = Card.accounts.unclosed.where(id: opts.fetch('card_ids'))
+    opts = opts.symbolize_keys
+    @account = Account.find(opts.fetch(:account_id))
+    @cards = Card.accounts.unclosed.where(id: opts.fetch(:card_ids))
     mail to: @account.email, subject: 'Annual Fee Reminder'
   end
 end
